ce29f1411031c21d9d81b8e9eed7b93c1c8584ab
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-08 22:09:55 -0500

    add dash

---
+++ pkgs/dash/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd source || exit 1
+./autogen.sh
+./configure --host="$_TARGET" --bindir=/bin
+make -j4
+mkdir -p "$_PKGROOT"/package/bin
+cp src/dash "$_PKGROOT"/package/bin
+)
+
+(
+cd package || exit 1
+"$_TARGET-strip" -x bin/dash
+ldid -S"$_BSROOT/entitlements.plist" bin/dash
+)
+cp -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package dash-0.5.12.deb
