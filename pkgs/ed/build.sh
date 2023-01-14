4a3648aae6b0281b505802153bc1f2b8600caf32
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-14 06:16:48 -0500

    update ed

--- pkgs/ed/build.sh
+++ pkgs/ed/build.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 (
 cd source || exit 1
-./configure --prefix=/usr CC="$_TARGET-clang"
+./configure --prefix=/usr --bindir=/bin CC="$_TARGET-clang"
 "$_MAKE" -j4
 "$_MAKE" DESTDIR="$_PKGROOT/package" install
 )
 
 (
 cd package || exit 1
-"$_TARGET-strip" -x usr/bin/ed
-ldid -S"$_BSROOT/entitlements.xml" usr/bin/ed
+rm -rf usr
+"$_TARGET-strip" -x bin/ed
+ldid -S"$_BSROOT/entitlements.xml" bin/ed
 )
 
 cp -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package ed-1.18.deb
+dpkg-deb -b --root-owner-group -Zgzip package ed-1.19.deb
--- pkgs/ed/fetch.sh
+++ pkgs/ed/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://ftp.gnu.org/gnu/ed/ed-1.18.tar.lz
+curl -L -# -o source.tar.gz https://ftp.gnu.org/gnu/ed/ed-1.19.tar.lz
 printf "Unpacking source...\n"
 tar -xf source.tar.gz
 rm source.tar.gz
-mv ed-1.18 source
+mv ed-1.19 source
