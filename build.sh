c0089e64e0761d8466f3e7cb105acad62d90720d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-16 23:56:19 -0500

    build.sh is still broken, moving to a lua implementation

--- build.sh
+++ build.sh
@@ -1,170 +1,16 @@
 #!/bin/sh
-#shellcheck disable=SC2185
+# Stop shellcheck from complaining about the find --version not specifying a path
+# shellcheck disable=SC2185
 
-# check target
-case "$*" in
-    *--target=*)
-        _args="$*"
-        _TARGET="${_args#*--target=}" ;;
-    *)
-        _TARGET="arm-apple-darwin9" ;;
-esac
-
-# check for dependencies
-for dep in "$_TARGET-clang" "$_TARGET-clang++" "$_TARGET-gcc" "$_TARGET-g++" "$_TARGET-cc" "$_TARGET-c++" "$_TARGET-strip" "$_TARGET-sdkpath" ldid dpkg-deb mv cp; do
-    if ! command -v "$dep" > /dev/null; then
-        printf "ERROR: Missing dependency %s\n" "$dep"
-        exit 1
-    fi
-done
-
-# check for apple strip
-_strip_version=$("$_TARGET-strip" --version 2> /dev/null)
-case "$_strip_version" in
-    *GNU*) printf "ERROR: GNU strip is not supported\n"; exit 1;;
-esac
-
-# check for gnu make
-if command -v gmake > /dev/null; then
-    _MAKE="gmake"
-elif command -v make > /dev/null; then
-    _make_version="$(make --version)"
-    case "$_make_version" in
-        *GNU*) _MAKE="make" ;;
-        *) printf "ERROR: Non-GNU make detected. Please install GNU make.\n" ;;
-    esac
-else
-    printf "ERROR: No make command detected. Please install GNU make.\n"
+# Error functions
+_red=$(tput setaf 1)
+_end=$(tput sgr0)
+error() {
+    printf "%sERROR:%s %s %s\n" "$_red" "$_end" "$1" "$2"
     exit 1
-fi
-
-# check for gnu patch
-if command -v gpatch > /dev/null; then
-    _PATCH="gpatch"
-elif command -v make > /dev/null; then
-    _patch_version="$(patch --version)"
-    case "$_patch_version" in
-        *GNU*) _PATCH="patch" ;;
-        *) printf "ERROR: Non-GNU patch detected. Please install GNU patch.\n" ;;
-    esac
-else
-    printf "ERROR: No patch command detected. Please install GNU patch.\n"
-    exit 1
-fi
-
-# check for gnu find
-if command -v gfind > /dev/null; then
-    _FIND="gfind"
-elif command -v find > /dev/null; then
-    _find_version="$(find --version)"
-    case "$_find_version" in
-        *GNU*) _FIND="find" ;;
-        *) printf "ERROR: Non-GNU find detected. Please install GNU find.\n" ;;
-    esac
-else
-    printf "ERROR: No find command detected. Please install GNU find.\n"
-    exit 1
-fi
-
-# assign environment variables
-_BSROOT="${0%/*}"
-_BSROOT="$(cd "$_BSROOT" && pwd)"
-_PKGDIR="$_BSROOT/pkgs"
-_SDK="$("$_TARGET-sdkpath")"
-export _PKGDIR _BSROOT _SDK _TARGET _MAKE _FIND
-export TERM="xterm-256color"
-
-# check if a required build dependency is missing
-# we rebuild if the return code is 1
-# rebuild if $_PKGDIR/$1/package/usr/include and $_PKGDIR/$1/package/usr/lib are both missing
-# if $2 = dryrun, then we rebuild the first time this function is run, but not again until the next time the script is run, even if the package is missing
-# check what packages are already built this run by adding them to the _BUILTPKGS variable
-hasbeenbuilt() {
-    if [ "$2" = "dryrun" ]; then
-        if [ -z "$_BUILTPKGS" ]; then
-            _BUILTPKGS="$1"
-            return 1
-        else
-            for pkg in $_BUILTPKGS; do
-                if [ "$pkg" = "$1" ]; then
-                    return 0
-                fi
-            done
-            _BUILTPKGS="$_BUILTPKGS $1"
-            return 1
-        fi
-    fi
-    if [ -d "$_PKGDIR/$1/package/usr/include" ] && [ -d "$_PKGDIR/$1/package/usr/lib" ]; then
-        return 0
-    else
-        return 1
-    fi
 }
 
-applypatches() {
-    if [ -d patches ]; then
-        for patch in patches/*; do
-            printf "Applying patch %s\n" "${patch##*/}"
-            "$_PATCH" -p0 < "$patch"
-        done
-    fi
-}
-
-includedeps() {
-    case "$*" in
-        *--no-tmpfs*) export _SDKPATH="$_BSROOT/sdk" ;;
-        *) export _SDKPATH="/tmp/sdk" ;;
-    esac
-    if ! [ "$2" = "dryrun" ]; then
-        rm -rf "$_SDKPATH"
-        cp -r "$_SDK" "$_SDKPATH"
-    fi
-    if [ -f dependencies.txt ]; then
-        while read -r dep; do
-            if [ -d "$_PKGDIR/$dep" ]; then
-                if [ "$1" = "-r" ] || ! hasbeenbuilt "$dep" "$2"; then
-                    printf "Building dependency %s\n" "$dep"
-                    [ "$2" = "dryrun" ] || mv "$_SDKPATH" "$_SDKPATH.tmp"
-                    build "$dep" "$1" "$2"
-                    [ "$2" = "dryrun" ] || mv "$_SDKPATH.tmp" "$_SDKPATH"
-                    if ! [ "$2" = "dryrun" ]; then
-                        [ -d "$_PKGDIR/$dep/package/usr/include" ] || [ -d "$_PKGDIR/$dep/package/usr/lib" ] || { printf "Failed to build dependency %s\n" "$dep"; exit 1; }
-                    fi
-                fi
-                printf "Including dependency %s\n" "$dep"
-                if ! [ "$2" = "dryrun" ]; then
-                    cp -r "$_PKGDIR/$dep/package/usr/include" "$_SDKPATH/usr"
-                    cp -r "$_PKGDIR/$dep/package/usr/lib" "$_SDKPATH/usr"
-                fi
-            fi
-        done < dependencies.txt
-    fi
-}
-
-build() {
-    (
-    hasbeenbuilt "$1" "$3" && return 0
-    printf "Building %s...\n" "${1##*/}"
-    export _PKGROOT="$_PKGDIR/$1"
-    cd "$_PKGROOT" || exit 1
-    includedeps "$2" "$3"
-    [ -d "$_PKGROOT/sdk" ] && cp -r "$_PKGROOT/sdk"/* "$_SDKPATH"
-    if ! [ "$3" = "dryrun" ]; then
-        ./fetch.sh
-        applypatches
-        ./build.sh
-    fi
-    rm -rf "$_SDKPATH"
-    )
-}
-
-buildall() {
-    for pkg in "$_PKGDIR"/*; do
-        pkg="${pkg##*/}"
-        build "$pkg" -r "$1" || { printf "Failed to build %s\n" "$pkg"; exit 1; }
-    done
-}
-
+# If no arguments are specified, print usage info
 if [ -z "$1" ]; then
     printf "Usage: build.sh <option> [--target=tripple] [--no-tmpfs]
     <package name> [-r]     - Build a single package, specify -r to rebuild dependencies
@@ -174,10 +20,200 @@ if [ -z "$1" ]; then
     dryrun                  - Pretend to build all packages
     listpkgs                - List all packages
     --target                - Specify a target other than arm-apple-darwin9
-    --no-tmpfs              - Do not use tmpfs for temporary sdks (use if you have limited RAM)\n"
+    --no-tmpfs              - Do not use /tmp for anything (use if you have limited RAM)\n"
     exit 1
 fi
 
+# Check target
+case "$*" in
+    *--target=*)
+        _args="$*"
+        _TARGET="${_args#*--target=}" ;;
+    *)
+        _TARGET="arm-apple-darwin9" ;;
+esac
+
+
+# Check for dependencies
+for dep in "$_TARGET-clang" "$_TARGET-clang++" "$_TARGET-gcc" "$_TARGET-g++" "$_TARGET-cc" "$_TARGET-c++" "$_TARGET-strip" "$_TARGET-sdkpath" ldid dpkg-deb mv cp; do
+    if ! command -v "$dep" > /dev/null; then
+        error "Missing dependency:" "$dep"
+    fi
+done
+
+# Check for Apple strip
+_strip_version=$("$_TARGET-strip" --version 2> /dev/null)
+case "$_strip_version" in
+    *GNU*) error "GNU strip is not supported."
+esac
+
+# Check for GNU make
+if command -v gmake > /dev/null; then
+    _MAKE="gmake"
+elif command -v make > /dev/null; then
+    _make_version="$(make --version)"
+    case "$_make_version" in
+        *GNU*) _MAKE="make" ;;
+        *) error "Non-GNU make detected. Please install GNU make." ;;
+    esac
+else
+    error "No make command detected. Please install GNU make."
+fi
+
+# Check for GNU patch
+if command -v gpatch > /dev/null; then
+    _PATCH="gpatch"
+elif command -v make > /dev/null; then
+    _patch_version="$(patch --version)"
+    case "$_patch_version" in
+        *GNU*) _PATCH="patch" ;;
+        *) error "Non-GNU patch detected. Please install GNU patch." ;;
+    esac
+else
+    error "No patch command detected. Please install GNU patch."
+fi
+
+# Check for GNU find
+if command -v gfind > /dev/null; then
+    _FIND="gfind"
+elif command -v find > /dev/null; then
+    _find_version="$(find --version)"
+    case "$_find_version" in
+        *GNU*) _FIND="find" ;;
+        *) error "Non-GNU find detected. Please install GNU find." ;;
+    esac
+else
+    error "No find command detected. Please install GNU find."
+fi
+
+# Assign environment variables
+_BSROOT="${0%/*}"
+_BSROOT="$(cd "$_BSROOT" && pwd)"
+_PKGDIR="$_BSROOT/pkgs"
+_SDK="$("$_TARGET-sdkpath")"
+export _PKGDIR _BSROOT _SDK _TARGET _MAKE _FIND
+export TERM="xterm-256color"
+
+# Decide where to put temporary files
+case "$*" in
+    *--no-tmpfs*)
+        export _TMP="$_BSROOT" ;;
+    *)
+        export _TMP="/tmp" ;;
+esac
+
+# Cleanup temporary files from previous runs
+printf "" > "$_TMP/.builtpkgs"
+rm -rf "$_TMP/sdk"
+rm -rf "$_TMP/sdk.bak"
+
+# Main build function
+# First check if the package is already built, if it has then skip it
+# Second cd to the package directory and export the _PKGROOT variable
+# Third run the includedeps function to build/add any dependencies
+# Fourth run the fetch.sh script to download the source code (skipped during dryrun)
+# Fifth run the applypatches function to apply any patches to the source code (skipped during dryrun)
+# Sixth run the build.sh script to build the package (skipped during dryrun)
+# Lastly remove the temporary sdk directory
+# First argument is the package name
+# Second argument is -r to rebuild dependencies
+# Third argument is dryrun to not actually build
+build() {
+    (
+    if ! [ "$2" = "-r" ] && hasbeenbuilt "$1" "$2" "$3"; then
+        exit 0
+    fi
+    cd "$_PKGDIR/$1" || exit 1
+    export _PKGROOT="$_PKGDIR/$1"
+    includedeps "$2" "$3"
+    [ "$3" = "dryrun" ] || ./fetch.sh
+    [ "$3" = "dryrun" ] || applypatches
+    printf "Building %s\n" "$1"
+    [ "$3" = "dryrun" ] || ./build.sh
+    rm -rf "$_SDKPATH"
+    printf "%s\n" "$1" >> "$_TMP/.builtpkgs"
+    )
+}
+
+# Build all packages
+buildall() {
+    for pkg in "$_PKGDIR"/*; do
+        build "${pkg##*/}" -r "$1"
+    done
+}
+
+# Check if a required build dependency is missing
+# If we need to rebuild then return 1
+# Return 1 if $_PKGDIR/$1/package is missing
+# if $2 = dryrun then we rebuild the first time this function is run, but not again until the next time the script is run, even if the package is missing
+# First argument is the package name
+# Second argument is dryrun to not actually build
+hasbeenbuilt() {
+    while read -r pkg; do
+        if [ "$pkg" = "$1" ] && { [ "$2" = "dryrun" ] || [ -d "$_PKGDIR/$1/package" ]; }; then
+            return 0
+        fi
+    done < "$_TMP/.builtpkgs"
+    return 1
+}
+
+# Apply any patches found in the patches directory
+# Unused durring dryruns
+# This function is always run inside the package directory
+applypatches() {
+    if [ -d patches ]; then
+        for patch in patches/*; do
+            printf "Applying patch %s\n" "${patch##*/}"
+            "$_PATCH" -p0 < "$patch"
+        done
+    fi
+}
+
+# Build an SDK for the build by copying the SDK at $_SDK to /tmp/sdk (or $_BSROOT/sdk if --no-tmpfs is specified) and then adding the /usr/include and /usr/lib directories from any dependencies
+# We run hasbeenbuilt on each dependency to ensure that it has been built before we try to copy it
+# When we do need to rebuild a dependency, we temporarily rename the sdk to sdk.bak, and then rename it back after we have rebuilt the dependency
+# If this is a dryrun, we don't actually build the dependency, or copy the sdk
+# If there is an sdk directory in the package folder, add it to the sdk. This allows us to add additional headers and libraries to the sdk per package
+# First argument is -r to rebuild dependencies
+# Second argument is dryrun to not actually build
+includedeps() {
+    if [ -d "$_SDK" ]; then
+        if ! [ "$2" = "dryrun" ]; then
+            export _SDKPATH="$_TMP/sdk"
+            cp -ar "$_SDK" "$_SDKPATH"
+        fi
+    else
+        error "SDK not found"
+    fi
+
+    if [ -f dependencies.txt ]; then
+        while read -r dep; do
+            if [ -d "$_PKGDIR/$dep" ]; then
+                if ! hasbeenbuilt "$dep" "$2" || [ "$1" = "-r" ]; then
+                    printf "Building dependency %s\n" "$dep"
+                    [ "$2" = "dryrun" ] || mv "$_SDKPATH" "$_SDKPATH.bak"
+                    build "$dep" "$1" "$2"
+                    [ "$2" = "dryrun" ] || mv "$_SDKPATH.bak" "$_SDKPATH"
+                fi
+                printf "Including dependency %s\n" "$dep"
+                if ! [ "$2" = "dryrun" ]; then
+                    [ -d "$_PKGDIR/$dep/package/usr/include" ] && cp -ar "$_PKGDIR/$dep/package/usr/include" "$_SDKPATH/usr"
+                    [ -d "$_PKGDIR/$dep/package/usr/lib" ] && cp -ar "$_PKGDIR/$dep/package/usr/lib" "$_SDKPATH/usr"
+                fi
+            else
+                error "Dependency not found:" "$dep"
+            fi
+        done < dependencies.txt
+    fi
+
+    if [ -d sdk ]; then
+        if ! [ "$2" = "dryrun" ]; then
+            cp -ar sdk/* "$_SDKPATH"
+        fi
+    fi
+}
+
+# Parse arguments
 if [ "$1" = "pkgall" ]; then
     buildall
     "$_FIND" . -iname "*.deb" -exec cp {} "$_BSROOT/debs" \;
@@ -195,6 +231,5 @@ elif [ -d "$_PKGDIR/$1" ]; then
 elif [ "$1" = dryrun ]; then
     buildall dryrun
 else
-    printf "ERROR: Package %s not found!\n" "$1"
-    exit 1
+    error "Package not found:" "$1"
 fi
