b20b8991f1a9c719403315fc42135ebdc5c86da8
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-21 07:36:43 -0500

    add findutils

---
+++ pkgs/findutils/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+(
+cd source || exit 1
+./configure --host="$_TARGET" --prefix=/usr
+"$_MAKE" -j4
+mkdir -p "$_PKGROOT"/package/usr/bin
+"$_CP" find/find "$_PKGROOT"/package/usr/bin
+"$_CP" xargs/xargs "$_PKGROOT"/package/usr/bin
+)
+
+(
+cd package || exit 1
+"$_TARGET-strip" -x usr/bin/find
+"$_TARGET-strip" -x usr/bin/xargs
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/find
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/xargs
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package findutils-4.9.0.deb
