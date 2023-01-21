dd389a847994d9c50618f680db41107c58d00e80
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-21 07:10:13 -0500

    add grep

---
+++ pkgs/grep/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+(
+cd source || exit 1
+./configure --host="$_TARGET" --prefix=/usr
+"$_MAKE" -j4
+mkdir -p "$_PKGROOT"/package/usr/bin
+"$_CP" src/grep "$_PKGROOT"/package/usr/bin
+"$_CP" src/egrep "$_PKGROOT"/package/usr/bin
+"$_CP" src/fgrep "$_PKGROOT"/package/usr/bin
+)
+
+(
+cd package || exit 1
+"$_TARGET-strip" -x usr/bin/grep
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/grep
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package grep-3.8.deb
