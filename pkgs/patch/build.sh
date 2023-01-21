2f318f66adc6f9ea695e1cba44b7f3a562e8af3b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-21 02:20:28 -0500

    add patch

---
+++ pkgs/patch/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd source || exit 1
+./configure --host="$_TARGET" --prefix=/usr
+"$_MAKE" -j4
+mkdir -p "$_PKGROOT"/package/usr/bin
+"$_CP" src/patch "$_PKGROOT"/package/usr/bin/patch
+)
+
+(
+cd package || exit 1
+"$_TARGET-strip" -x usr/bin/patch
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/patch
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package patch-2.7.6.deb
