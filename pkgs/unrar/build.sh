e813bb9a195e92a77e98be3855270c5759f7dbd3
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-22 05:41:22 -0500

    add unrar

---
+++ pkgs/unrar/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+(
+cd source || exit 1
+"$_MAKE" CXX="$_TARGET-clang++" STRIP=true CXXFLAGS=-O2 LIBFLAGS= -j4
+mkdir -p "$_PKGROOT"/package/usr/bin
+"$_CP" unrar "$_PKGROOT"/package/usr/bin
+)
+
+(
+cd package || exit 1
+"$_TARGET-strip" -x usr/bin/unrar
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/unrar
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package unrar-6.20b3.deb
---
+++ pkgs/unrar/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf package source
+printf "Downloading source...\n"
+curl -L -# -o source.tar.gz https://www.rarlab.com/rar/unrarsrc-6.2.3.tar.gz
+printf "Unpacking source...\n"
+tar -xf source.tar.gz
+rm source.tar.gz
+mv unrar source
