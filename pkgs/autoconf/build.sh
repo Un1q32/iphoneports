bb83f4565db361a20ecba9d0cff184411afac47f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-22 05:02:05 -0500

    add autoconf

---
+++ pkgs/autoconf/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+(
+cd source || exit 1
+./configure --host="$_TARGET" --prefix=/usr
+"$_MAKE" -j4
+"$_MAKE" DESTDIR="$_PKGROOT/package" install
+)
+
+(
+cd package || exit 1
+rm -rf usr/share/info usr/share/man
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package autoconf-2.71.deb
