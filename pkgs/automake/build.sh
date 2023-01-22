4cc16fcdc14f6781fdbadfa6eb1bfed8ca76c18b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-22 05:19:49 -0500

    add automake

---
+++ pkgs/automake/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+(
+cd source || exit 1
+./configure --host="$_TARGET" --prefix=/usr
+"$_MAKE" -j4
+"$_MAKE" DESTDIR="$_PKGROOT/package" install
+)
+
+(
+cd package || exit 1
+rm -rf usr/share/info usr/share/man usr/share/doc
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package automake-1.16.5.deb
