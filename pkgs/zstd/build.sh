1459dd34050ebf06ee0b5a2e1256f8d90c0d3675
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-23 10:49:03 -0500

    fix zstd build on linux

--- pkgs/zstd/build.sh
+++ pkgs/zstd/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 (
 cd source || exit 1
-"$_MAKE" CC="$_TARGET-clang" RANLIB="$_TARGET-ranlib" PREFIX=/usr -j4
-"$_MAKE" DESTDIR="$_PKGROOT/package" PREFIX=/usr install
+"$_MAKE" CC="$_TARGET-clang" RANLIB="$_TARGET-ranlib" PREFIX=/usr UNAME=Darwin -j4
+"$_MAKE" DESTDIR="$_PKGROOT/package" PREFIX=/usr UNAME=Darwin install
 )
 
 (
 cd package || exit 1
+rm -rf usr/share
 "$_TARGET-strip" -x usr/bin/zstd
 ldid -S"$_BSROOT/entitlements.xml" usr/bin/zstd
 )
