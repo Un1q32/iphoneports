125b455cb36bafc11ff7587ae31de8623230b561
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-24 01:58:11 -0500

    update unrar

--- pkgs/unrar/build.sh
+++ pkgs/unrar/build.sh
@@ -13,4 +13,4 @@ ldid -S"$_BSROOT/entitlements.xml" usr/bin/unrar
 )
 
 "$_CP" -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package unrar-6.20b3.deb
+dpkg-deb -b --root-owner-group -Zgzip package unrar-6.20.deb
