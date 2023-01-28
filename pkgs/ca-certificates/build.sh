e1c392fd150f0d13049c84cd247400307c9e7b24
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-28 18:08:12 -0500

    switch to libressl

--- pkgs/ca-certificates/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"$_CP" -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package ca-certificates-20221209-0219.deb
--- pkgs/ca-certificates/fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.deb https://humantrafficing.net/ca-certificates-20221209-0219.deb
-printf "Unpacking source...\n"
-dpkg-deb -x source.deb package
-rm source.deb
