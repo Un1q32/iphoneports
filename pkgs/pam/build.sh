f89649485ab08821c41d38b540434345719d4991
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-01-30 09:43:00 -0500

    dont make pam deb

--- pkgs/pam/build.sh
+++ pkgs/pam/build.sh
@@ -3,5 +3,3 @@
 cd package || exit 1
 ln -s pam usr/include/security
 )
-
-dpkg-deb -b --root-owner-group -Zgzip package pam-32.1.deb
--- pkgs/pam/fetch.sh
+++ pkgs/pam/fetch.sh
@@ -3,5 +3,5 @@ rm -rf package source
 printf "Downloading source...\n"
 curl -L -# -o source.deb http://apt.saurik.com/cydia/debs/pam_32.1-4_iphoneos-arm.deb
 printf "Unpacking source...\n"
-dpkg-deb -R source.deb package
+dpkg-deb -x source.deb package
 rm source.deb
