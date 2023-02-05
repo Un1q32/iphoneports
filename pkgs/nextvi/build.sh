d861b0106d3acd7819056d52ceb575347c65e4a0
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-02-05 01:52:52 -0500

    asdf

--- pkgs/nextvi/build.sh
+++ pkgs/nextvi/build.sh
@@ -14,4 +14,4 @@ ldid -S"$_BSROOT/entitlements.xml" usr/bin/vi
 )
 
 "$_CP" -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package nextvi-1.1.deb
+dpkg-deb -b --root-owner-group -Zgzip package nextvi.deb
