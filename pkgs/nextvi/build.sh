aaa12e835711ecef9abc7788d0fd27ee26ccd176
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-02-04 19:51:59 -0500

    update nextvi

--- pkgs/nextvi/build.sh
+++ pkgs/nextvi/build.sh
@@ -2,14 +2,16 @@
 (
 mkdir -p package/usr/bin
 cd source || exit 1
-"$_TARGET-clang" -pedantic -std=c99 -D_POSIX_C_SOURCE=200809L -D_DARWIN_C_SOURCE -O2 vi.c -o "$_PKGROOT/package/usr/bin/vi"
+"$_MAKE" CC="$_TARGET-clang" TARGET_DIR_BIN="$_PKGROOT/package/usr/bin" install -j4
 )
 
 (
 cd package || exit 1
 "$_TARGET-strip" -x usr/bin/vi
+"$_TARGET-strip" -x usr/bin/ex
 ldid -S"$_BSROOT/entitlements.xml" usr/bin/vi
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/ex
 )
 
 "$_CP" -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package nextvi-1.0.deb
+dpkg-deb -b --root-owner-group -Zgzip package nextvi-1.1.deb
