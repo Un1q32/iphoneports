b55a1c5a0414c6d90610dcb5c23e4f2f7f03c93f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-02-15 07:00:19 -0500

    downgrade libressl to latest stable (we were using a dev release before)

--- pkgs/libressl/fetch.sh
+++ pkgs/libressl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.7.0.tar.gz
+curl -L -# -o source.tar.gz https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.6.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf source.tar.gz
 rm source.tar.gz
-mv libressl-3.7.0 source
+mv libressl-3.6.2 source
