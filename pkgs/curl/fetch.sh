fd2ceff60efeac25a8bc59aec11b1c9e58684447
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-02-15 06:48:34 -0500

    update curl and git

--- pkgs/curl/fetch.sh
+++ pkgs/curl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://github.com/curl/curl/releases/download/curl-7_87_0/curl-7.87.0.tar.gz
+curl -L -# -o source.tar.gz https://github.com/curl/curl/releases/download/curl-7_88_0/curl-7.88.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf source.tar.gz
 rm source.tar.gz
-mv curl-7.87.0 source
+mv curl-7.88.0 source
--- pkgs/git/build.sh
+++ pkgs/git/build.sh
@@ -41,4 +41,4 @@ ldid -S"$_BSROOT/entitlements.xml" usr/libexec/git-core/git-daemon
 )
 
 "$_CP" -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package git-2.39.1.deb
+dpkg-deb -b --root-owner-group -Zgzip package git-2.39.2.deb
