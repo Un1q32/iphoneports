351d4d53c0789b223c39af7238e4508f8765f22b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-02-17 06:34:31 -0500

    lol

--- pkgs/nano/fetch.sh
+++ pkgs/nano/fetch.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-rm -rf package source
+rm -rf package source files/syntax
 printf "Downloading source...\n"
 curl -L -# -o source.tar.gz https://ftp.gnu.org/gnu/nano/nano-7.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf source.tar.gz
 rm source.tar.gz
 mv nano-7.2 source
-rm -rf files/syntax
 git clone https://github.com/scopatz/nanorc files/syntax
