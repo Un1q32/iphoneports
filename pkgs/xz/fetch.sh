a06fb749b570cc0f5b00ab615d3e7bc5564a6ded
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-02-18 14:00:57 -0500

    fix xz utils

--- pkgs/xz/fetch.sh
+++ pkgs/xz/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.xz https://tukaani.org/xz/xz-5.4.1.tar.xz
+curl -L -# -o source.tar.xz https://pilotfiber.dl.sourceforge.net/project/lzmautils/xz-5.4.1.tar.xz
 printf "Unpacking source...\n"
 tar -xf source.tar.xz
 rm source.tar.xz
