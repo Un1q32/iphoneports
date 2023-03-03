5f74a688b1b1c8188921eec58d1c9bb7d4629b1d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-03 16:01:28 -0500

    fix bzip2

--- pkgs/bzip2/build.sh
+++ pkgs/bzip2/build.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 (
 cd source || exit 1
-"$_MAKE" CC="$_TARGET-clang" RANLIB="$_TARGET-ranlib" PREFIX="$_PKGROOT/package/usr" libbz2.a bzip2 bzip2recover install -j8
+"$_MAKE" CC="$_TARGET-clang" RANLIB="$_TARGET-ranlib" PREFIX="$_PKGROOT/package/usr" install -j8
 )
 
 (
 cd package || exit 1
 mv usr/bin .
-rm -rf usr/man
+rm -rf usr/man bin/bzcmp bin/bzegrep bin/bzfgrep bin/bzless
+ln -s bzdiff bin/bzcmp
+ln -s bzgrep bin/bzegrep
+ln -s bzgrep bin/bzfgrep
+ln -s bzmore bin/bzless
 "$_TARGET-strip" -x bin/bzip2
+"$_TARGET-strip" -x bin/bunzip2
+"$_TARGET-strip" -x bin/bzcat
 "$_TARGET-strip" -x bin/bzip2recover
 ldid -S"$_BSROOT/entitlements.xml" bin/bzip2
+ldid -S"$_BSROOT/entitlements.xml" bin/bunzip2
+ldid -S"$_BSROOT/entitlements.xml" bin/bzcat
 ldid -S"$_BSROOT/entitlements.xml" bin/bzip2recover
 )
 
