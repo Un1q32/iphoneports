07be1ff2038e29be8cd7f1cf9c6069db6177613c
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-04 01:00:54 -0500

    remove most and its dependencies since most cross compiling is broken

--- pkgs/most/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://www.jedsoft.org/releases/most/most-5.2.0.tar.gz
-printf "Unpacking source...\n"
-tar -xf source.tar.gz
-rm source.tar.gz
-mv most-5.2.0 source
