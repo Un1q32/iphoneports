ff1e8255f505e3e068a42d19c6b1a76dd42d182f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-04 00:59:41 -0500

    remove all broken packages

--- pkgs/mono/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.xz https://download.mono-project.com/sources/mono/mono-6.12.0.182.tar.xz
-printf "Unpacking source...\n"
-tar -xf source.tar.xz
-rm source.tar.xz
-mv mono-6.12.0.182 source
