de65091a7c9dc9accb4238582ab9755965fdacdf
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-06 02:06:57 -0500

    update grep to 3.9, enable pcre2 with grep, make bzip2 use shared library

--- pkgs/bzip2/build.sh
+++ pkgs/bzip2/build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+"$_CP" files/Makefile-libbz2_dylib source
 (
 cd source || exit 1
 "$_MAKE" CC="$_TARGET-clang" RANLIB="$_TARGET-ranlib" PREFIX="$_PKGROOT/package/usr" install -j8
+"$_MAKE" -f Makefile-libbz2_dylib CC="$_TARGET-clang" RANLIB="$_TARGET-ranlib" PREFIX="$_PKGROOT/package/usr/local" -j8
 )
 
 (
 cd package || exit 1
 mv usr/bin .
-rm -rf usr/man bin/bzcmp bin/bzegrep bin/bzfgrep bin/bzless
+rm -rf usr/man bin/bzcmp bin/bzegrep bin/bzfgrep bin/bzless usr/lib
 ln -s bzdiff bin/bzcmp
 ln -s bzgrep bin/bzegrep
 ln -s bzgrep bin/bzfgrep
