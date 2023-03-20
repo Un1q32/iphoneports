b8373d46f8b3e9dc2d2dba206e43b3090c94298e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-20 14:34:22 -0400

    add libunistring, use it in libidn2

---
+++ pkgs/libunistring/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd source || exit 1
+./configure --host="$_TARGET" --prefix=/usr --disable-static
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/package" install
+)
+
+(
+cd package || exit 1
+rm -rf usr/share
+"$_TARGET-strip" -x usr/lib/libunistring.5.dylib
+ldid -S"$_BSROOT/entitlements.xml" usr/lib/libunistring.5.dylib
+)
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package libunistring-1.1.deb
---
+++ pkgs/libunistring/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf package source
+printf "Downloading source...\n"
+curl -L -# -o source.tar.xz https://ftp.gnu.org/gnu/libunistring/libunistring-1.1.tar.xz
+printf "Unpacking source...\n"
+tar -xf source.tar.xz
+rm source.tar.xz
+mv libunistring-1.1 source
