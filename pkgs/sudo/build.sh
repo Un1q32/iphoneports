d517755e67fda28e77c13dc97e99f086b524f1a5
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-22 17:25:24 -0400

    use zlib in sudo

--- pkgs/sudo/build.sh
+++ pkgs/sudo/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd source || exit 1
-./configure --host="$_TARGET" --exec-prefix=/usr --sysconfdir=/etc --includedir=/usr/include --with-passprompt="Password:" --with-env-editor ax_cv_check_cflags___static_libgcc=no
+./configure --host="$_TARGET" --exec-prefix=/usr --sysconfdir=/etc --includedir=/usr/include --with-passprompt="Password:" --enable-zlib --with-env-editor ax_cv_check_cflags___static_libgcc=no
 "$_MAKE" -j8
 fakeroot "$_MAKE" DESTDIR="$_PKGROOT/package" install
 )
