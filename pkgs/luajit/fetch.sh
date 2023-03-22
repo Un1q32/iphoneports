ec2c0f076474c33503e90c63863efd56f48cfa2d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-03-22 12:49:09 -0400

    fix luajit

--- pkgs/luajit/fetch.sh
+++ pkgs/luajit/fetch.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-git clone --depth 1 https://luajit.org/git/luajit.git source
-# curl -L -# -o source.tar.gz https://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
-# printf "Unpacking source...\n"
-# tar -xf source.tar.gz
-# rm source.tar.gz
-# mv LuaJIT-2.1.0-beta3 source
+git clone https://luajit.org/git/luajit.git source
