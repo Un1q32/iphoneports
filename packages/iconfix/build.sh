cfb5786d0af74146a1071d9c9f1496091c2ea62b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-08 09:27:57 -0400

    update iconfix

--- packages/iconfix/build.sh
+++ packages/iconfix/build.sh
@@ -3,4 +3,4 @@ mkdir -p package/usr/bin
 "$_CP" "$_PKGROOT/source/iconfix.sh" "$_PKGROOT/package/usr/bin/iconfix"
 
 "$_CP" -r DEBIAN package
-dpkg-deb -b --root-owner-group -Zgzip package iconfix-5.1.deb
+dpkg-deb -b --root-owner-group -Zgzip package iconfix-5.2.deb
