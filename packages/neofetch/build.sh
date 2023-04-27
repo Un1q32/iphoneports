d55ff452c479d58714093b09beb69f88e424bbac
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-27 00:04:12 -0400

    add neofetch

---
+++ packages/neofetch/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd source || exit 1
+# "$_TARGET-clang" -O2 "$_PKGROOT/files/getmemory.c" -o getmemory
+mkdir -p "$_PKGROOT/package/usr/bin"
+# "$_CP" getmemory "$_PKGROOT/package/usr/bin/getmemory"
+"$_CP" neofetch "$_PKGROOT/package/usr/bin/neofetch"
+)
+
+# (
+# cd package || exit 1
+# "$_TARGET-strip" usr/bin/getmemory > /dev/null 2>&1
+# ldid -S"$_BSROOT/entitlements.xml" usr/bin/getmemory
+# )
+
+"$_CP" -r DEBIAN package
+dpkg-deb -b --root-owner-group -Zgzip package neofetch-7.3.8.deb
