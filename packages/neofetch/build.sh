ce2d7a071704b9e1272cef54cd0dd5dd8095828e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-04-27 20:18:16 -0400

    neofetch memory support

--- packages/neofetch/build.sh
+++ packages/neofetch/build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 (
 cd source || exit 1
-# "$_TARGET-clang" -O2 "$_PKGROOT/files/getmemory.c" -o getmemory
+"$_TARGET-clang" -O2 "$_PKGROOT/files/getusedmem.c" -o getusedmem
 mkdir -p "$_PKGROOT/package/usr/bin"
-# "$_CP" getmemory "$_PKGROOT/package/usr/bin/getmemory"
+"$_CP" getusedmem "$_PKGROOT/package/usr/bin/getusedmem"
 "$_CP" neofetch "$_PKGROOT/package/usr/bin/neofetch"
 )
 
-# (
-# cd package || exit 1
-# "$_TARGET-strip" usr/bin/getmemory > /dev/null 2>&1
-# ldid -S"$_BSROOT/entitlements.xml" usr/bin/getmemory
-# )
+(
+cd package || exit 1
+"$_TARGET-strip" usr/bin/getusedmem > /dev/null 2>&1
+ldid -S"$_BSROOT/entitlements.xml" usr/bin/getusedmem
+)
 
 "$_CP" -r DEBIAN package
 dpkg-deb -b --root-owner-group -Zgzip package neofetch-7.3.8.deb
