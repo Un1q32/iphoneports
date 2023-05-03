0c780745c043d403e9b536dd1e78459806ee5592
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-05-03 19:33:07 -0400

    fix

--- packages/openssl/build.sh
+++ packages/openssl/build.sh
@@ -3,8 +3,8 @@
 cd package || exit 1
 rm -rf etc usr/bin/openssl usr/lib/pkgconfig usr/include
 chmod u+w usr/lib/libcrypto.0.9.8.dylib usr/lib/libssl.0.9.8.dylib
-"$_TARGET-strip" usr/lib/libssl.0.9.8.dylib > /dev/null 2>&1
-"$_TARGET-strip" usr/lib/libcrypto.0.9.8.dylib > /dev/null 2>&1
+"$_TARGET-strip" -x usr/lib/libssl.0.9.8.dylib > /dev/null 2>&1
+"$_TARGET-strip" -x usr/lib/libcrypto.0.9.8.dylib > /dev/null 2>&1
 ldid -S"$_BSROOT/entitlements.xml" usr/lib/libssl.0.9.8.dylib
 ldid -S"$_BSROOT/entitlements.xml" usr/lib/libcrypto.0.9.8.dylib
 chmod u-w usr/lib/libcrypto.0.9.8.dylib usr/lib/libssl.0.9.8.dylib
