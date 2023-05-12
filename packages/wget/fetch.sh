c4821a48fb99ca029cf41647c77a4e5ed10b337e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-05-12 13:56:02 -0400

    update wget

--- packages/wget/fetch.sh
+++ packages/wget/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://ftp.gnu.org/gnu/wget/wget-1.21.3.tar.gz
+curl -L -# -o source.tar.gz https://ftp.gnu.org/gnu/wget/wget-1.21.4.tar.gz
 printf "Unpacking source...\n"
 tar -xf source.tar.gz
 rm source.tar.gz
-mv wget-1.21.3 source
+mv wget-1.21.4 source
