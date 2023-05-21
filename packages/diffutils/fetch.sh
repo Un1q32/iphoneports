8f6cfdb227fc99357bc852c6787e19e79e1e44aa
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-05-21 17:47:53 -0400

    update diffutils

--- packages/diffutils/fetch.sh
+++ packages/diffutils/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.xz https://ftp.gnu.org/gnu/diffutils/diffutils-3.9.tar.xz
+curl -L -# -o source.tar.xz https://ftp.gnu.org/gnu/diffutils/diffutils-3.10.tar.xz
 printf "Unpacking source...\n"
 tar -xf source.tar.xz
 rm source.tar.xz
-mv diffutils-3.9 source
+mv diffutils-3.10 source
