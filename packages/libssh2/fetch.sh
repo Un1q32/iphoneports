e6fcf5ffbe06fdacdbe9b637024fef2704120df2
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-05-30 17:58:40 -0400

    update libssh2, curl

--- packages/libssh2/fetch.sh
+++ packages/libssh2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://www.libssh2.org/download/libssh2-1.10.0.tar.gz
+curl -L -# -o source.tar.gz https://www.libssh2.org/download/libssh2-1.11.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf source.tar.gz
 rm source.tar.gz
-mv libssh2-1.10.0 source
+mv libssh2-1.11.0 source
