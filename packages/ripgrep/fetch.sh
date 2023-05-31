431ef28a7a9b0c000c27f1dd5e4c8e7361f522e5
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-05-31 05:33:27 -0400

    remove ripgrep

--- packages/ripgrep/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://github.com/BurntSushi/ripgrep/archive/refs/tags/13.0.0.tar.gz
-printf "Unpacking source...\n"
-tar -xf source.tar.gz
-rm source.tar.gz
-mv ripgrep-13.0.0 source
