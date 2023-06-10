afd8531847b66f4b8077f4e2ddad37699c2aef5f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-06-09 22:48:32 -0400

    use /usr/local for libressl, remove openssl shell package

--- packages/libressl/build.sh
+++ packages/libressl/build.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 (
 cd source || exit 1
-./configure --host="$_TARGET" --prefix=/usr --sysconfdir=/etc --disable-static
+./configure --host="$_TARGET" --sysconfdir=/etc --disable-static
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/package" install
 )
 
 (
 cd package || exit 1
-rm -rf usr/share
-"$_TARGET-strip" usr/bin/openssl > /dev/null 2>&1
-"$_TARGET-strip" usr/bin/ocspcheck > /dev/null 2>&1
-"$_TARGET-strip" usr/lib/libcrypto.50.dylib > /dev/null 2>&1
-"$_TARGET-strip" usr/lib/libssl.53.dylib > /dev/null 2>&1
-"$_TARGET-strip" usr/lib/libtls.26.dylib > /dev/null 2>&1
-ldid -S"$_BSROOT/ent.xml" usr/bin/openssl
-ldid -S"$_BSROOT/ent.xml" usr/bin/ocspcheck
-ldid -S"$_BSROOT/ent.xml" usr/lib/libcrypto.50.dylib
-ldid -S"$_BSROOT/ent.xml" usr/lib/libssl.53.dylib
-ldid -S"$_BSROOT/ent.xml" usr/lib/libtls.26.dylib
+rm -rf usr/local/share
+"$_TARGET-strip" usr/local/bin/openssl > /dev/null 2>&1
+"$_TARGET-strip" usr/local/bin/ocspcheck > /dev/null 2>&1
+"$_TARGET-strip" usr/local/lib/libcrypto.50.dylib > /dev/null 2>&1
+"$_TARGET-strip" usr/local/lib/libssl.53.dylib > /dev/null 2>&1
+"$_TARGET-strip" usr/local/lib/libtls.26.dylib > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" usr/local/bin/openssl
+ldid -S"$_BSROOT/ent.xml" usr/local/bin/ocspcheck
+ldid -S"$_BSROOT/ent.xml" usr/local/lib/libcrypto.50.dylib
+ldid -S"$_BSROOT/ent.xml" usr/local/lib/libssl.53.dylib
+ldid -S"$_BSROOT/ent.xml" usr/local/lib/libtls.26.dylib
 )
 
 "$_CP" -r DEBIAN package
