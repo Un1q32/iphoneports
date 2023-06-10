51436da173931d63cf66e020647c53dabe645e3d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-06-10 02:52:53 -0400

    update neofetch

--- packages/neofetch/fetch.sh
+++ packages/neofetch/fetch.sh
@@ -2,5 +2,5 @@
 rm -rf package source
 printf "Downloading source...\n"
 mkdir -p source
-curl -L -# -o source/neofetch https://raw.githubusercontent.com/hykilpikonna/hyfetch/1.4.8/neofetch
+curl -L -# -o source/neofetch https://raw.githubusercontent.com/hykilpikonna/hyfetch/1.4.9/neofetch
 chmod 755 source/neofetch
