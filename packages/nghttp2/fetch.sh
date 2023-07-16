2bd8d5e4b57939e216d9b579f6e344b67cbe2f0e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-07-15 23:44:31 -0400

    update nghttp2

--- packages/nghttp2/fetch.sh
+++ packages/nghttp2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf package source
 printf "Downloading source...\n"
-curl -L -# -o source.tar.xz https://github.com/nghttp2/nghttp2/releases/download/v1.55.0/nghttp2-1.55.0.tar.xz
+curl -L -# -o source.tar.xz https://github.com/nghttp2/nghttp2/releases/download/v1.55.1/nghttp2-1.55.1.tar.xz
 printf "Unpacking source...\n"
 tar -xf source.tar.xz
 rm source.tar.xz
-mv nghttp2-1.55.0 source
+mv nghttp2-1.55.1 source
