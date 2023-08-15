d44c21e4d51ec50bf9cc9b68e64da445e62bf003
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-15 07:17:28 -0400

    fix ncdu

--- pkgs/ncdu/build.sh
+++ pkgs/ncdu/build.sh
@@ -8,8 +8,9 @@ cd src || exit 1
 
 (
 cd pkg/var/usr || exit 1
-"$_TARGET-strip" usr/bin/ncdu > /dev/null 2>&1
-ldid -S"$_BSROOT/ent.xml" usr/bin/ncdu
+rm -rf share
+"$_TARGET-strip" bin/ncdu > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" bin/ncdu
 )
 
 "$_CP" -r DEBIAN pkg
