dcdbc304b5e5e307d78a20d78cf28dbbe7349437
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-15 00:55:40 -0400

    add libunistring

--- packages/brotli/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz
-printf "Unpacking source...\n"
-tar -xf source.tar.gz
-rm source.tar.gz
-mv brotli-1.0.9 source
---
+++ pkgs/libunistring/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" lib/libunistring.5.dylib > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" lib/libunistring.5.dylib
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libunistring.deb
