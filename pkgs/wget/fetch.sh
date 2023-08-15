c06668c39578c902865630693e1a41ee1434ff9a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-15 01:17:05 -0400

    add wget

---
+++ pkgs/wget/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/wget/wget-1.21.4.tar.gz
+printf "Unpacking source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv wget-1.21.4 src
