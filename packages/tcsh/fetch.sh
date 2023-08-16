a507f459fab79936b35dcbb6770002a841184832
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 02:30:05 -0400

    add tcsh

--- packages/tcsh/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://astron.com/pub/tcsh/tcsh-6.24.07.tar.gz
-printf "Unpacking source...\n"
-tar -xf source.tar.gz
-rm source.tar.gz
-mv tcsh-6.24.07 source
