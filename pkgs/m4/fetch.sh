fdf9505e91f8c1124c5e85a0b488f06757ca6660
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 07:21:00 -0400

    add m4

---
+++ pkgs/m4/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz
+printf "Unpacking source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv m4-1.4.19 src
