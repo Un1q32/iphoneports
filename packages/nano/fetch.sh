67b311a4ed058b20afdb461b0470d23758b8ca73
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 02:18:42 -0400

    add nano

--- packages/nano/fetch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf package source files/syntax
-printf "Downloading source...\n"
-curl -L -# -o source.tar.gz https://ftp.gnu.org/gnu/nano/nano-7.2.tar.gz
-printf "Unpacking source...\n"
-tar -xf source.tar.gz
-rm source.tar.gz
-mv nano-7.2 source
-git clone --depth 1 https://github.com/scopatz/nanorc.git files/syntax
---
+++ pkgs/nano/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr NCURSESW_LIBS="-lncursesw"
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+# for nanorc in files/syntax/*.nanorc; do
+#     "$_CP" -a "$nanorc" package/usr/share/nano/
+# done
+cd pkg/var/usr || exit 1
+rm -rf share/info share/man share/doc
+"$_TARGET-strip" bin/nano > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" bin/nano
+)
+
+"$_CP" -a files/syntax/*.nanorc pkg/var/usr/share/nano
+mkdir -p pkg/var/usr/etc
+"$_CP" files/nanorc pkg/var/usr/etc
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg nano.deb
