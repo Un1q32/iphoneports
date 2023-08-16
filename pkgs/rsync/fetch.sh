61d55da0e90ec147b8c00a5840983ebad9150b43
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 06:54:29 -0400

    fix link

--- pkgs/rsync/fetch.sh
+++ pkgs/rsync/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/WayneD/rsync/archive/refs/tags/v3.2.7.tar.gz
+curl -L -# -o src.tar.gz https://download.samba.org/pub/rsync/src/rsync-3.2.7.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
