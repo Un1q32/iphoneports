815028e44b37d288137b0fff0941c9a1f01c87ec
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 03:36:37 -0400

    add bc

---
+++ pkgs/bc/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+(
+cd src || exit 1
+autoreconf -fi
+./configure --host="$_TARGET" --prefix=/var/usr
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" bin/bc > /dev/null 2>&1
+"$_TARGET-strip" bin/dc > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" bin/bc
+ldid -S"$_BSROOT/ent.xml" bin/dc
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg bc.deb
