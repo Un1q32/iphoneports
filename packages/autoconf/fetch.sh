2a0145c640893b66be638ad6fe42cda3bdbeba13
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 07:56:47 -0400

    finish overhaul

--- packages/autoconf/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.xz https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.xz
-printf "Unpacking source...\n"
-tar -xf source.tar.xz
-rm source.tar.xz
-mv autoconf-2.71 source
--- packages/automake/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf package source
-printf "Downloading source...\n"
-curl -L -# -o source.tar.xz https://ftp.gnu.org/gnu/automake/automake-1.16.5.tar.xz
-printf "Unpacking source...\n"
-tar -xf source.tar.xz
-rm source.tar.xz
-mv automake-1.16.5 source
