c127e3b68edd324896a68b2e9212185172da8e90
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-16 07:45:13 -0400

    add screen

---
+++ pkgs/screen/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+(
+cd src || exit 1
+./autogen.sh
+./configure --host="$_TARGET" --prefix=/var/usr --enable-colors256 CPPFLAGS="-Wno-implicit-function-declaration"
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share/info share/man
+"$_TARGET-strip" bin/screen-4.9.0 > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" bin/screen-4.9.0
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg screen.deb
