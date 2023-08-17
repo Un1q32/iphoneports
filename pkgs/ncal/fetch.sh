9cf22bf22a2529238c2091006e594e1864110df5
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-17 14:17:18 -0400

    update ncal

--- pkgs/ncal/fetch.sh
+++ pkgs/ncal/fetch.sh
@@ -2,7 +2,7 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/ncal.c https://opensource.apple.com/source/misc_cmds/misc_cmds-34/ncal/ncal.c
-curl -L -# -o src/calendar.c https://opensource.apple.com/source/misc_cmds/misc_cmds-34/ncal/calendar.c
-curl -L -# -o src/calendar.h https://opensource.apple.com/source/misc_cmds/misc_cmds-34/ncal/calendar.h
-curl -L -# -o src/easter.c https://opensource.apple.com/source/misc_cmds/misc_cmds-34/ncal/easter.c
+curl -L -# -o src/ncal.c https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/ncal.c
+curl -L -# -o src/calendar.c https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/calendar.c
+curl -L -# -o src/calendar.h https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/calendar.h
+curl -L -# -o src/easter.c https://github.com/apple-oss-distributions/misc_cmds/raw/misc_cmds-37/ncal/easter.c
