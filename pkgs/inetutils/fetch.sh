4e247376ce8b060b8272b82dab2326dc6115cabe
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-17 17:52:27 -0400

    remove inetutils

--- pkgs/inetutils/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/inetutils/inetutils-2.4.tar.xz
-printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
-mv inetutils-2.4 src
