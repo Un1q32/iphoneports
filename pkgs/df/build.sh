2b9333189dec1640c52c12f292d9db2e5915dc6f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-17 14:32:24 -0400

    add df

---
+++ pkgs/df/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+(
+cd src || exit 1
+"$_TARGET-cc" -I. df.c vfslist.c -o df -lutil
+mkdir -p "$_PKGROOT/pkg/var/usr/bin"
+"$_CP" df "$_PKGROOT/pkg/var/usr/bin"
+)
+
+(
+cd pkg/var/usr/bin || exit 1
+"$_TARGET-strip" df > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" df
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg df.deb
--- pkgs/ncal/build.sh
+++ pkgs/ncal/build.sh
@@ -7,10 +7,10 @@ mkdir -p "$_PKGROOT/pkg/var/usr/bin"
 )
 
 (
-cd pkg/var/usr || exit 1
-"$_TARGET-strip" bin/ncal
-ldid -S"$_BSROOT/ent.xml" bin/ncal
-ln -s ncal bin/cal
+cd pkg/var/usr/bin || exit 1
+"$_TARGET-strip" ncal > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" ncal
+ln -s ncal cal
 )
 
 "$_CP" -r DEBIAN pkg
