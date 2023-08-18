f3fbffa68308470389333256d0d3ce83ae678a48
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-18 10:59:16 -0400

    update openssh

--- pkgs/openssh/build.sh
+++ pkgs/openssh/build.sh
@@ -2,7 +2,7 @@
 rm -f "$_SDKPATH/usr/lib/libsandbox."*
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --sysconfdir=/var/usr/etc/ssh --with-privsep-user=nobody --with-sandbox=no
+./configure --host="$_TARGET" --prefix=/var/usr --sysconfdir=/var/usr/etc/ssh --with-privsep-user="_sshd" --with-sandbox=no
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install-nokeys STRIP_OPT=
 )
