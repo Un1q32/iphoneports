19925ebcb2bbc635b69dd95467e25b0d6dd46b32
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-19 09:42:06 -0400

    update zlib

--- pkgs/zlib/build.sh
+++ pkgs/zlib/build.sh
@@ -9,8 +9,8 @@ CHOST="$_TARGET" ./configure --prefix=/var/usr --shared
 (
 cd pkg/var/usr || exit 1
 rm -rf share lib/libz.a
-"$_TARGET-strip" lib/libz.1.2.13.dylib > /dev/null 2>&1
-ldid -S"$_BSROOT/ent.xml" lib/libz.1.2.13.dylib
+"$_TARGET-strip" lib/libz.1.3.dylib > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" lib/libz.1.3.dylib
 )
 
 "$_CP" -r DEBIAN pkg
