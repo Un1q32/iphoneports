a146bcc668b03de261081b8e508fbe0e640eb916
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-19 15:26:09 -0400

    for loop for fetch here

--- pkgs/ncal/fetch.sh
+++ pkgs/ncal/fetch.sh
@@ -2,7 +2,6 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/ncal.c https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/ncal.c
-curl -L -# -o src/calendar.c https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/calendar.c
-curl -L -# -o src/calendar.h https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/calendar.h
-curl -L -# -o src/easter.c https://github.com/apple-oss-distributions/misc_cmds/raw/misc_cmds-37/ncal/easter.c
+for src in ncal.c calendar.c calendar.h easter.c; do
+    curl -L -# -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/$src"
+done
