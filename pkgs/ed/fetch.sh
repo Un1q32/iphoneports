1374de40a5292723c3716adbb778a41ec332c25e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-19 15:27:16 -0400

    better fetch.sh

--- pkgs/ed/fetch.sh
+++ pkgs/ed/fetch.sh
@@ -2,11 +2,6 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/main.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-154/ed/main.c
-curl -L -# -o src/ed.h https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/ed.h
-curl -L -# -o src/io.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/io.c
-curl -L -# -o src/buf.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/buf.c
-curl -L -# -o src/re.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/re.c
-curl -L -# -o src/glbl.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/glbl.c
-curl -L -# -o src/undo.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/undo.c
-curl -L -# -o src/sub.c https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-138.100.3/ed/sub.c
+for src in main.c ed.h io.c buf.c re.c glbl.c undo.c sub.c; do
+    curl -L -# -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-154/ed/$src"
+done
