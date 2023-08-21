e2269e9ae5e13b637a127595154175620d9732ec
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-21 18:26:42 -0400

    add pax

---
+++ pkgs/pax/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+(
+cd src || exit 1
+mkdir -p "$_PKGROOT/pkg/var/usr/bin"
+"$_TARGET-cc" -O2 -o pax pax.c tar.c cpio.c options.c cache.c ftree.c tables.c getoldopt.c ar_io.c pat_rep.c pax_format.c tty_subs.c ar_subs.c sel_subs.c buf_subs.c file_subs.c gen_subs.c -D__used='// '
+"$_CP" pax "$_PKGROOT/pkg/var/usr/bin"
+)
+
+(
+cd pkg/var/usr/bin || exit 1
+"$_TARGET-strip" pax > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" pax
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg pax.deb
