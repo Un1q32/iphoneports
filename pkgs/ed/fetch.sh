3d7e8c0e2728d82045c291a59258531c557570fa
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-22 09:05:34 -0400

    parallel downloads

--- pkgs/ed/fetch.sh
+++ pkgs/ed/fetch.sh
@@ -3,5 +3,6 @@ rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
 for src in main.c ed.h io.c buf.c re.c glbl.c undo.c sub.c; do
-    curl -L -# -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-154/ed/$src"
+    curl -L -s -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/text_cmds/text_cmds-154/ed/$src" &
 done
+wait
