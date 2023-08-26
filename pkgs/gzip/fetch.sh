dcd1a2320307889aeb68887bf87dc258ba6652ab
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-26 11:39:09 -0400

    multithreaded dl

--- pkgs/gzip/fetch.sh
+++ pkgs/gzip/fetch.sh
@@ -3,5 +3,6 @@ rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
 for file in gzip.c unbzip2.c zuncompress.c unpack.c unxz.c unlz.c futimens.c gzexe zdiff zforce zmore znew; do
-    curl -L -# -o "src/$file" "https://raw.githubusercontent.com/apple-oss-distributions/file_cmds/file_cmds-400/gzip/$file"
+    curl -L -s -o "src/$file" "https://raw.githubusercontent.com/apple-oss-distributions/file_cmds/file_cmds-400/gzip/$file" &
 done
+wait
