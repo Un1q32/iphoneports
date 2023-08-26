a54cb4bf169b122566459b1fb15ada13d8a181dd
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-26 13:39:24 -0400

    multithread

--- pkgs/ncal/fetch.sh
+++ pkgs/ncal/fetch.sh
@@ -3,5 +3,6 @@ rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
 for src in ncal.c calendar.c calendar.h easter.c; do
-    curl -L -# -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/$src"
+    curl -L -s -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/misc_cmds/misc_cmds-37/ncal/$src" &
 done
+wait
