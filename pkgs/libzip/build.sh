867155dc9f65493ab59a466b441b93bd6a38f0aa
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-30 13:54:06 -0400

    add libzip

---
+++ pkgs/libzip/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+mkdir -p src/build
+(
+cd src/build || exit 1
+cmake .. -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr"
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-install_name_tool" -id /var/usr/lib/libzip.5.dylib lib/libzip.5.5.dylib
+"$_TARGET-strip" lib/libzip.5.5.dylib > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" lib/libzip.5.5.dylib
+for bin in bin/*; do
+    lib="$("$_TARGET-otool" -L "$bin" | grep libzip | awk '{print $1}')"
+    if [ -n "$lib" ]; then
+        "$_TARGET-install_name_tool" -change "$lib" /var/usr/lib/libzip.5.dylib "$bin"
+    fi
+
+    "$_TARGET-strip" "$bin" > /dev/null 2>&1
+    ldid -S"$_BSROOT/ent.xml" "$bin"
+done
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libzip.deb
