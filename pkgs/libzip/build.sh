2ebd8074009adfb15a8bdcc9a07389005a5702ca
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-30 14:51:18 -0400

    better

--- pkgs/libzip/build.sh
+++ pkgs/libzip/build.sh
@@ -10,15 +10,12 @@ cmake .. -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INS
 (
 cd pkg/var/usr || exit 1
 rm -rf share
+lib="$("$_TARGET-otool" -D lib/libzip.5.5.dylib | tail -1)"
 "$_TARGET-install_name_tool" -id /var/usr/lib/libzip.5.dylib lib/libzip.5.5.dylib
 "$_TARGET-strip" lib/libzip.5.5.dylib > /dev/null 2>&1
 ldid -S"$_BSROOT/ent.xml" lib/libzip.5.5.dylib
 for bin in bin/*; do
-    lib="$("$_TARGET-otool" -L "$bin" | grep libzip | awk '{print $1}')"
-    if [ -n "$lib" ]; then
-        "$_TARGET-install_name_tool" -change "$lib" /var/usr/lib/libzip.5.dylib "$bin"
-    fi
-
+    "$_TARGET-install_name_tool" -change "$lib" /var/usr/lib/libzip.5.dylib "$bin"
     "$_TARGET-strip" "$bin" > /dev/null 2>&1
     ldid -S"$_BSROOT/ent.xml" "$bin"
 done
