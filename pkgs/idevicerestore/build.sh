23b5897a10c1b04c60f619c156406b94bd9826ca
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-30 13:58:54 -0400

    add idevicerestore

---
+++ pkgs/idevicerestore/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr PKG_CONFIG_PATH="$_SDK/var/usr/lib/pkgconfig" #--without-cython --disable-static 
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" bin/idevicerestore > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" bin/idevicerestore
+)
+
+"$_CP" -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg idevicerestore.deb
