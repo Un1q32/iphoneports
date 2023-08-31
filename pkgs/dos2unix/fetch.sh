8d66a5a676a12e8c059e2b88e687ff87529461a7
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-08-31 14:00:13 -0400

    update dos2unix

--- pkgs/dos2unix/fetch.sh
+++ pkgs/dos2unix/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.5.0.tar.gz
+curl -L -# -o src.tar.gz https://waterlan.home.xs4all.nl/dos2unix/dos2unix-7.5.1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv dos2unix-7.5.0 src
+mv dos2unix-7.5.1 src
