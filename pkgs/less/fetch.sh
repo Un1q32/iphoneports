fba6979f5ff2e3b7e2d3275a92e30a9ec0e660b0
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-01 01:19:25 -0400

    update less

--- pkgs/less/fetch.sh
+++ pkgs/less/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.greenwoodsoftware.com/less/less-633.tar.gz
+curl -L -# -o src.tar.gz https://www.greenwoodsoftware.com/less/less-643.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv less-633 src
+mv less-643 src
