7492913ddc0937b9f44577d00ad2aebee1348845
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-03 18:56:53 -0400

    update brotli

--- pkgs/brotli/build.sh
+++ pkgs/brotli/build.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
+mkdir -p src/build
 (
-cd src || exit 1
-./bootstrap
-./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+cd src/build || exit 1
+cmake .. -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DBROTLI_DISABLE_TESTS=1
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
 
 (
 cd pkg/var/usr || exit 1
-rm -rf share
-for bin in bin/brotli lib/libbrotlicommon.1.dylib lib/libbrotlidec.1.dylib lib/libbrotlienc.1.dylib; do
+for bin in bin/brotli lib/libbrotlicommon.1.1.0.dylib lib/libbrotlidec.1.1.0.dylib lib/libbrotlienc.1.1.0.dylib; do
     "$_TARGET-strip" "$bin" > /dev/null 2>&1
     ldid -S"$_BSROOT/ent.xml" "$bin"
 done
--- pkgs/brotli/fetch.sh
+++ pkgs/brotli/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz
+curl -L -# -o src.tar.gz https://github.com/google/brotli/archive/refs/tags/v1.1.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv brotli-1.0.9 src
+mv brotli-1.1.0 src
