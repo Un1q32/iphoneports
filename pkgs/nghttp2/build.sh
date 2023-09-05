87498d7c2d600d0e530d04f938cee3c295ef47d2
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-04 21:46:30 -0400

    fix nghttp2

--- pkgs/nghttp2/build.sh
+++ pkgs/nghttp2/build.sh
@@ -3,14 +3,13 @@ mkdir -p src/build
 (
 cd src/build || exit 1
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DENABLE_FAILMALLOC=OFF
-"$_MAKE" -j8
-"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install -j8
 )
 
 (
 cd pkg/var/usr || exit 1
 rm -rf share
-"$_TARGET-install_name_tool" -id /var/usr/lib/libnghttp2.14.24.4.dylib lib/libnghttp2.14.24.4.dylib
+"$_TARGET-install_name_tool" -id /var/usr/lib/libnghttp2.14.dylib lib/libnghttp2.14.24.4.dylib
 "$_TARGET-strip" lib/libnghttp2.14.24.4.dylib > /dev/null 2>&1
 ldid -S"$_BSROOT/ent.xml" lib/libnghttp2.14.24.4.dylib
 )
