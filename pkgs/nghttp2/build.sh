8e4629454ef4119a71ba2b04328fa24209307b9e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-04 20:59:15 -0400

    build nghttp2 with cmake

--- pkgs/nghttp2/build.sh
+++ pkgs/nghttp2/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+mkdir -p src/build
 (
-cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --disable-static --disable-examples --with-openssl --with-zlib --with-libxml2 --without-libcares PKG_CONFIG_SYSROOT_DIR="$_SDK" PKG_CONFIG_PATH="$_SDK/var/usr/lib/pkgconfig"
+cd src/build || exit 1
+cmake .. -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DENABLE_FAILMALLOC=OFF
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
 
 (
 cd pkg/var/usr || exit 1
-rm -rf share bin
-"$_TARGET-strip" lib/libnghttp2.14.dylib > /dev/null 2>&1
-ldid -S"$_BSROOT/ent.xml" lib/libnghttp2.14.dylib
+rm -rf share
+"$_TARGET-install_name_tool" -id /var/usr/lib/libnghttp2.14.24.4.dylib lib/libnghttp2.14.24.4.dylib
+"$_TARGET-strip" lib/libnghttp2.14.24.4.dylib > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" lib/libnghttp2.14.24.4.dylib
 )
 
 cp -r DEBIAN pkg
