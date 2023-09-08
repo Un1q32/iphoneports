8da8fbbc64320ad93dad1408216f4825edf3b370
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-08 00:20:33 -0400

    fix

--- pkgs/hostname/fetch.sh
+++ pkgs/hostname/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/hostname.c https://raw.githubusercontent.com/apple-oss-distributions/shell_cmds/main/hostname/hostname.c
+curl -L -# -o src/hostname.c https://raw.githubusercontent.com/apple-oss-distributions/shell_cmds/shell_cmds-240.100.15/hostname/hostname.c
