837aa7e9fec7bc5adbfcd639e7f7e7aaac12091f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-08 00:28:52 -0400

    add killall

---
+++ pkgs/killall/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+(
+cd src || exit 1
+"$_TARGET-cc" -O2 killall.c -o killall -D__FBSDID=__RCSID
+mkdir -p "$_PKGROOT/pkg/var/usr/bin"
+cp killall "$_PKGROOT/pkg/var/usr/bin"
+)
+
+(
+cd pkg/var/usr/bin || exit 1
+"$_TARGET-strip" killall > /dev/null 2>&1
+ldid -S"$_BSROOT/ent.xml" killall
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg killall.deb
