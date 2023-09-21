36689c67d960750ed66ba6da36b3d7cc86a21ab6
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-21 14:07:35 -0400

    update xxd

--- pkgs/xxd/fetch.sh
+++ pkgs/xxd/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -# -o src/xxd.c https://raw.githubusercontent.com/vim/vim/4390d872b6c9498527a43fc7c30a5384f2e1db12/src/xxd/xxd.c
+curl -# -o src/xxd.c https://raw.githubusercontent.com/vim/vim/233f956bd43279db1fb4d017acb4e56a1504addf/src/xxd/xxd.c
