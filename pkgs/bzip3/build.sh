f53d0ddb88af86eabb053a3695fc0dd24962e62c
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-21 21:57:44 -0400

    add bzip3

---
+++ pkgs/bzip3/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" bin/bzip3 > /dev/null 2>&1
+"$_TARGET-strip" lib/libbzip3.0.dylib > /dev/null 2>&1
+ldid -S"$_ENT" bin/bzip3
+ldid -S"$_ENT" lib/libbzip3.0.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg bzip3.deb
---
+++ pkgs/bzip3/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.zst https://github.com/kspalaiologos/bzip3/releases/download/1.3.2/bzip3-1.3.2.tar.zst
+printf "Unpacking source...\n"
+tar -xf src.tar.zst
+rm src.tar.zst
+mv bzip3-1.3.2 src
