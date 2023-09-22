f40e1a7f311d53d3fa3332a3a3d63a267fb25bac
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-09-21 22:15:40 -0400

    update bzip2

--- pkgs/bzip2/build.sh
+++ pkgs/bzip2/build.sh
@@ -8,13 +8,9 @@ cd src || exit 1
 cd pkg/var/usr || exit 1
 rm -rf man
 "$_TARGET-strip" bin/bzip2 > /dev/null 2>&1
-"$_TARGET-strip" bin/bunzip2 > /dev/null 2>&1
-"$_TARGET-strip" bin/bzcat > /dev/null 2>&1
 "$_TARGET-strip" bin/bzip2recover > /dev/null 2>&1
 "$_TARGET-strip" lib/libbz2.1.0.dylib > /dev/null 2>&1
 ldid -S"$_ENT" bin/bzip2
-ldid -S"$_ENT" bin/bunzip2
-ldid -S"$_ENT" bin/bzcat
 ldid -S"$_ENT" bin/bzip2recover
 ldid -S"$_ENT" lib/libbz2.1.0.dylib
 )
