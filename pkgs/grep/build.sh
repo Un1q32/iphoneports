0c1f051602d92bbde252b3d64d40f31c7a756947
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-03 22:40:50 -0400

    replace grep with bsd grep

--- pkgs/grep/build.sh
+++ pkgs/grep/build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr
-"$_MAKE" -j8
+"$_TARGET-cc" -O2 -o grep grep.c util.c queue.c file.c -include "$_PKGROOT/files/compat.h" -D'__FBSDID(x)=' -lbz2 -llzma -lz
 mkdir -p "$_PKGROOT/pkg/var/usr/bin"
-cp src/grep "$_PKGROOT/pkg/var/usr/bin"
-cp src/egrep "$_PKGROOT/pkg/var/usr/bin"
-cp src/fgrep "$_PKGROOT/pkg/var/usr/bin"
+cp grep "$_PKGROOT/pkg/var/usr/bin"
 )
 
 (
 cd pkg/var/usr/bin || exit 1
 "$_TARGET-strip" grep > /dev/null 2>&1
 ldid -S"$_ENT" grep
+for link in egrep fgrep rgrep bzgrep bzegrep bzfgrep zgrep zegrep zfgrep; do
+    ln -s grep "$link"
+done
 )
 
 cp -r DEBIAN pkg
---
+++ pkgs/grep/files/compat.h
@@ -0,0 +1,1 @@
+#define __DECONST(type, var) ((type)(uintptr_t)(const void*)(var))
