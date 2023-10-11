ee16d1fe23aca10184802fc71bce5e6b7995913d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-11 16:12:36 -0400

    add sqlite

---
+++ pkgs/sqlite/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" bin/sqlite3 lib/libsqlite3.0.dylib > /dev/null 2>&1
+ldid -S"$_ENT" bin/sqlite3 lib/libsqlite3.0.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg sqlite.deb
