d539fdb99dfd71c5647b6f96b837daef9433638f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-13 18:53:48 -0400

    update iconfix

--- pkgs/iconfix/build.sh
+++ pkgs/iconfix/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-mkdir -p pkg/usr/bin
+mkdir -p "$_PKGROOT/pkg/usr/bin" "$_PKGROOT/pkg/usr/share/iconfix"
 cp "$_PKGROOT/src/iconfix.sh" "$_PKGROOT/pkg/usr/bin/iconfix"
+cp "$_PKGROOT"/src/*.png "$_PKGROOT/pkg/usr/share/iconfix"
 
 cp -r DEBIAN pkg
 dpkg-deb -b --root-owner-group -Zgzip pkg iconfix.deb
--- pkgs/iconfix/fetch.sh
+++ pkgs/iconfix/fetch.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-mkdir src
-curl -L -# -o src/iconfix.sh https://raw.githubusercontent.com/OldWorldOrdr/icon-fix/master/iconfix.sh
-chmod 755 src/iconfix.sh
+git clone https://github.com/OldWorldOrdr/icon-fix.git src --depth 1
