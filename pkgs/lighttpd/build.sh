4061f9b621691aa084b5f46d17274612e6e660d7
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-12 20:02:12 -0400

    add lighttpd

---
+++ pkgs/lighttpd/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --with-openssl --with-zlib --with-brotli --with-zstd --with-bzip2 --with-webdav-props PKG_CONFIG_PATH="$_SDK/var/usr/lib/pkgconfig"
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" sbin/lighttpd sbin/lighttpd-angel lib/liblightcomp.dylib lib/*.so > /dev/null 2>&1
+ldid -S"$_ENT" sbin/lighttpd sbin/lighttpd-angel lib/liblightcomp.dylib lib/*.so
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg lighttpd.deb
---
+++ pkgs/lighttpd/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.xz https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.72.tar.xz
+printf "Unpacking source...\n"
+tar -xf src.tar.xz
+rm src.tar.xz
+mv lighttpd-1.4.72 src
