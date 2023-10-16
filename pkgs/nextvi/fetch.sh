38f8b10ead3e5ec2b30e6f5ef553ac9b037d1ca3
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-16 13:57:37 -0400

    update nextvi

--- pkgs/nextvi/fetch.sh
+++ pkgs/nextvi/fetch.sh
@@ -3,4 +3,4 @@ rm -rf pkg src
 printf "Downloading source...\n"
 git clone https://github.com/kyx0r/nextvi.git src
 cd src || exit 1
-git -c advice.detachedHead=false checkout 9e9d05a0aa1cd3a13bd3c2b3e3828f82612949de
+git -c advice.detachedHead=false checkout a3c9eb3943576f4aeb4bfffc6693435f721411c9
