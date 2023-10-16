bc91bbd7f476d0333b072b9288eebaa1a538bbd3
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-16 03:11:43 -0400

    update bc

--- pkgs/bc/build.sh
+++ pkgs/bc/build.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 (
 cd src || exit 1
-autoreconf -fi
-./configure --host="$_TARGET" --prefix=/var/usr
+CC="$_TARGET-cc" HOSTCC='cc' ./configure --prefix=/var/usr --enable-readline --disable-nls --disable-strip --disable-man-pages
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
 
 (
-cd pkg/var/usr || exit 1
-rm -rf share
-"$_TARGET-strip" bin/bc bin/dc > /dev/null 2>&1
-ldid -S"$_ENT" bin/bc bin/dc
+cd pkg/var/usr/bin || exit 1
+"$_TARGET-strip" bc > /dev/null 2>&1
+ldid -S"$_ENT" bc
 )
 
 cp -r DEBIAN pkg
--- pkgs/bc/fetch.sh
+++ pkgs/bc/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
+curl -L -# -o src.tar.gz https://git.gavinhoward.com/gavin/bc/archive/6.7.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv bc-1.07.1 src
+mv bc src
