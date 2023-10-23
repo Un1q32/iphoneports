6477d026463ca4fd1c3042592e97ff7ba31109a0
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-23 04:20:18 -0400

    remove one true awk, gawk is default awk again

--- pkgs/awk/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/onetrueawk/awk/archive/refs/tags/2ndEdition.tar.gz
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv awk-2ndEdition src
--- pkgs/gawk/build.sh
+++ pkgs/gawk/build.sh
@@ -8,7 +8,7 @@ cd src || exit 1
 
 (
 cd pkg/var/usr || exit 1
-rm -rf share/info share/man bin/gawk-* bin/gawkbug bin/awk
+rm -rf share/info share/man bin/gawk-* bin/gawkbug
 "$_TARGET-strip" bin/gawk lib/gawk/* libexec/awk/* > /dev/null 2>&1
 ldid -S"$_ENT" bin/gawk lib/gawk/* libexec/awk/*
 )
