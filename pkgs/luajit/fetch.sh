9f26ecaf604edbbb53980af15a4e7f83a6226d3b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-25 07:24:34 -0400

    fix

--- pkgs/luajit/fetch.sh
+++ pkgs/luajit/fetch.sh
@@ -3,4 +3,4 @@ rm -rf pkg src
 printf "Downloading source...\n"
 git clone https://luajit.org/git/luajit.git src
 cd src || exit 1
-git checkout becf5cc65d966a8926466dd43407c48bfea0fa13
+git -c advice.detachedHead=false checkout becf5cc65d966a8926466dd43407c48bfea0fa13
