325349a8c0737d5423dc8b15be1ba1153f23bbc6
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-28 10:07:53 -0400

    fix coreutils, remove df, remove libxo

--- pkgs/df/fetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-mkdir src
-curl -L -# -o src/df.c https://raw.githubusercontent.com/apple-oss-distributions/file_cmds/file_cmds-428/df/df.c
