998a18b02a6ffc35f426636d2908818effc0bd3b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-10-29 12:40:59 -0400

    update nextvi

--- pkgs/nextvi/fetch.sh
+++ pkgs/nextvi/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=13e582d599dda6cfe3dbc2d8a1e061fd7ab608cc
+commit=7812c7950f894e833b1e74c394ab5b188d31ef24
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/kyx0r/nextvi/archive/${commit}.tar.gz"
