18637d2b7d28318b1925181ff519ff43337d6506
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-01 18:21:34 -0400

    update ncdu

--- pkgs/ncdu/build.sh
+++ pkgs/ncdu/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr NCURSES_LIBS="-lncursesw"
+./configure --host="$_TARGET" --prefix=/var/usr --with-shell=/var/usr/bin/sh NCURSES_LIBS="-lncursesw"
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
