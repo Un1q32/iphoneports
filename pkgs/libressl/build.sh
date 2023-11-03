781b670da5fa8d4bdd02e5f6f3fff2b83cca1e55
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-02 20:42:20 -0400

    update libressl

--- pkgs/libressl/build.sh
+++ pkgs/libressl/build.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
+mkdir -p src/build
 (
-cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --disable-static
-"$_MAKE" -j8
-"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+cd src/build || exit 1
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_SYSTEM_PROCESSOR=arm -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DBUILD_SHARED_LIBS=yes -DENABLE_NC=yes
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg" -j8
 )
 
 (
 cd pkg/var/usr || exit 1
 rm -rf share etc/ssl/cert.pem
-"$_TARGET-strip" bin/openssl bin/ocspcheck lib/libcrypto.52.dylib lib/libssl.55.dylib lib/libtls.28.dylib > /dev/null 2>&1
-ldid -S"$_ENT" bin/openssl bin/ocspcheck lib/libcrypto.52.dylib lib/libssl.55.dylib lib/libtls.28.dylib
+cryptoabi=52
+sslabi=55
+tlsabi=28
+mv lib/libcrypto.$cryptoabi.*.dylib lib/libcrypto.$cryptoabi.dylib
+mv lib/libssl.$sslabi.*.dylib lib/libssl.$sslabi.dylib
+mv lib/libtls.$tlsabi.*.dylib lib/libtls.$tlsabi.dylib
+lib1="$("$_OTOOL" -D lib/libcrypto.$cryptoabi.dylib | tail -1)"
+lib2="$("$_OTOOL" -D lib/libssl.$sslabi.dylib | tail -1)"
+lib3="$("$_OTOOL" -D lib/libtls.$tlsabi.dylib | tail -1)"
+"$_INSTALLNAMETOOL" -id /var/usr/lib/libcrypto.$cryptoabi.dylib lib/libcrypto.$cryptoabi.dylib
+"$_INSTALLNAMETOOL" -id /var/usr/lib/libssl.$sslabi.dylib lib/libssl.$sslabi.dylib
+"$_INSTALLNAMETOOL" -id /var/usr/lib/libtls.$tlsabi.dylib lib/libtls.$tlsabi.dylib
+"$_INSTALLNAMETOOL" -change "$lib1" /var/usr/lib/libcrypto.$cryptoabi.dylib bin/openssl
+"$_INSTALLNAMETOOL" -change "$lib2" /var/usr/lib/libssl.$sslabi.dylib bin/openssl
+"$_INSTALLNAMETOOL" -change "$lib1" /var/usr/lib/libcrypto.$cryptoabi.dylib bin/ocspcheck
+"$_INSTALLNAMETOOL" -change "$lib2" /var/usr/lib/libssl.$sslabi.dylib bin/ocspcheck
+"$_INSTALLNAMETOOL" -change "$lib3" /var/usr/lib/libtls.$tlsabi.dylib bin/ocspcheck
+"$_INSTALLNAMETOOL" -change "$lib3" /var/usr/lib/libtls.$tlsabi.dylib bin/nc
+"$_TARGET-strip" bin/openssl bin/ocspcheck bin/nc lib/libcrypto.52.dylib lib/libssl.55.dylib lib/libtls.28.dylib > /dev/null 2>&1
+ldid -S"$_ENT" bin/openssl bin/ocspcheck bin/nc lib/libcrypto.52.dylib lib/libssl.55.dylib lib/libtls.28.dylib
 )
 
 cp -r DEBIAN pkg
