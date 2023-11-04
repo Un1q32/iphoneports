d7c266217ec1ca09fcee6945b37a7b17431358a5
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-04 12:52:34 -0400

    update xz

--- pkgs/xz/fetch.sh
+++ pkgs/xz/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://cytranet.dl.sourceforge.net/project/lzmautils/xz-5.4.4.tar.xz
+curl -L -# -o src.tar.xz https://tukaani.org/xz/xz-5.4.5.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv xz-5.4.4 src
+mv xz-5.4.5 src
