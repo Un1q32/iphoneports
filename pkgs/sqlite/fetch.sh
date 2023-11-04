d29343571836bf2b3d5a1a15765087f117bd1150
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-04 12:51:33 -0400

    update sqlite

--- pkgs/sqlite/fetch.sh
+++ pkgs/sqlite/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://sqlite.org/2023/sqlite-autoconf-3430200.tar.gz
+curl -L -# -o src.tar.gz https://sqlite.org/2023/sqlite-autoconf-3440000.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv sqlite-autoconf-3430200 src
+mv sqlite-autoconf-3440000 src
