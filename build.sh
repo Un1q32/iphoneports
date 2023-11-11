8de586faa4fbbe915c6f60bf94d45ded8ebf8e5f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-11 15:06:50 -0500

    update

--- build.sh
+++ build.sh
@@ -74,7 +74,7 @@ esac
 rm -rf "$_TMP"/iphoneports-sdk*
 
 depcheck() {
-    for dep in "$_TARGET-gcc" "$_TARGET-g++" "$_TARGET-cc" "$_TARGET-c++" "$_TARGET-strip" "$_TARGET-sdkpath" ldid dpkg-deb patch fakeroot automake autoreconf m4; do
+    for dep in "$_TARGET-gcc" "$_TARGET-g++" "$_TARGET-cc" "$_TARGET-c++" "$_TARGET-strip" "$_TARGET-sdkpath" ldid dpkg-deb patch fakeroot automake autoreconf m4 yacc; do
         if ! command -v "$dep" > /dev/null; then
             error "Missing dependency: $dep"
         fi
