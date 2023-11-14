c43e9754ea2c00cb19e7ed3a5bf050af996dff5f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-14 00:30:54 -0500

    update

--- pkgs/nano/build.sh
+++ pkgs/nano/build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr NCURSESW_LIBS="-lncursesw"
+./configure --host="$_TARGET" --prefix=/var/usr
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
 
 (
-# for nanorc in files/syntax/*.nanorc; do
-#     cp -a "$nanorc" package/usr/share/nano/
-# done
 cd pkg/var/usr || exit 1
 rm -rf share/info share/man share/doc
 "$_TARGET-strip" bin/nano > /dev/null 2>&1
