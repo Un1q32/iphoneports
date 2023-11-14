b05d836c7b8041e614c66c42f0f2502358ae48fa
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-14 18:44:00 -0500

    move common code to its own function

--- pkgs/catgirl/files/compat.c
+++ pkgs/catgirl/files/compat.c
@@ -1,7 +1,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <stdarg.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
@@ -12,22 +12,32 @@ void explicit_bzero(void *d, size_t n) {
     __asm__ __volatile__ ("" : : "r"(d) : "memory");
 }
 
-int mkdirat(int fd, const char *pathname, mode_t mode) {
+char* _atfunc(int fd, const char* pathname) {
     if (fd == AT_FDCWD || pathname[0] == '/')
-        return mkdir(pathname, mode);
+        return strdup(pathname);
 
     char fdpath[PATH_MAX];
     if (fcntl(fd, F_GETPATH, fdpath) == -1)
-        return -1;
+        return NULL;
 
     char path[PATH_MAX];
     strcpy(path, fdpath);
     strcat(path, "/");
     strcat(path, pathname);
-    return mkdir(path, mode);
+    return strdup(path);
 }
 
-int openat(int fd, const char *pathname, int flags, ...) {
+int mkdirat(int fd, const char* pathname, mode_t mode) {
+    char* path = _atfunc(fd, pathname);
+    if (path == NULL)
+        return -1;
+
+    int ret = mkdir(path, mode);
+    free(path);
+    return ret;
+}
+
+int openat(int fd, const char* pathname, int flags, ...) {
     mode_t mode = 0;
     if (flags & O_CREAT) {
         va_list ap;
@@ -36,16 +46,11 @@ int openat(int fd, const char *pathname, int flags, ...) {
         va_end(ap);
     }
 
-    if (fd == AT_FDCWD || pathname[0] == '/')
-        return open(pathname, flags, mode);
-
-    char fdpath[PATH_MAX];
-    if (fcntl(fd, F_GETPATH, fdpath) == -1)
+    char* path = _atfunc(fd, pathname);
+    if (path == NULL)
         return -1;
 
-    char path[PATH_MAX];
-    strcpy(path, fdpath);
-    strcat(path, "/");
-    strcat(path, pathname);
-    return open(path, flags, mode);
+    int ret = open(path, flags, mode);
+    free(path);
+    return ret;
 }
