57605b51d847e5ea2c013521c48feadbd0e31e22
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-16 03:36:37 -0500

    update sudo

--- pkgs/sudo/build.sh
+++ pkgs/sudo/build.sh
@@ -2,7 +2,7 @@
 unset SUDO_PROMPT
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --sysconfdir=/var/usr/etc --with-rundir=/var/usr/run/sudo --with-vardir=/var/usr/db/sudo --with-passprompt="Password:" --disable-tmpfiles.d --enable-zlib=system --with-env-editor --with-openssl ax_cv_check_cflags___static_libgcc=no
+./configure --host="$_TARGET" --prefix=/var/usr --sysconfdir=/var/usr/etc --with-rundir=/var/usr/run/sudo --with-vardir=/var/usr/db/sudo --with-passprompt="Password:" --disable-tmpfiles.d --with-env-editor --enable-zlib --enable-openssl --with-pam ax_cv_check_cflags___static_libgcc=no
 "$_MAKE" -j8
 fakeroot "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
@@ -10,10 +10,8 @@ fakeroot "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 (
 cd pkg/var/usr || exit 1
 rm -rf share
-for bin in bin/sudo bin/cvtsudoers bin/sudoreplay sbin/sudo_logsrvd sbin/sudo_sendlog sbin/visudo libexec/sudo/libsudo_util.0.dylib libexec/sudo/*.so; do
-    "$_TARGET-strip" "$bin" > /dev/null 2>&1
-    ldid -S"$_ENT" "$bin"
-done
+"$_TARGET-strip" bin/sudo bin/cvtsudoers bin/sudoreplay sbin/sudo_logsrvd sbin/sudo_sendlog sbin/visudo libexec/sudo/libsudo_util.0.dylib libexec/sudo/*.so > /dev/null 2>&1
+ldid -S"$_ENT" bin/sudo bin/cvtsudoers bin/sudoreplay sbin/sudo_logsrvd sbin/sudo_sendlog sbin/visudo libexec/sudo/libsudo_util.0.dylib libexec/sudo/*.so
 chmod 4755 bin/sudo
 )
 
@@ -22,6 +20,7 @@ mv pkg/var/usr/bin/sudo pkg/usr/libexec/iphoneports
 ln -s ../../../../usr/libexec/iphoneports/sudo pkg/var/usr/bin/sudo
 
 cp files/sudoers pkg/var/usr/etc/sudoers
+chmod 440 pkg/var/usr/etc/sudoers
 
 cp -r DEBIAN pkg
 dpkg-deb -b --root-owner-group -Zgzip pkg sudo.deb
