62653cccbb36d5401d996cd3191fe2890d7a406d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-19 02:59:13 -0500

    fix

--- pkgs/2048/fetch.sh
+++ pkgs/2048/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -# -o src/2048.c https://raw.githubusercontent.com/mevdschee/2048.c/main/2048.c
+curl -# -o src/2048.c https://raw.githubusercontent.com/mevdschee/2048.c/6c04517bb59c28f3831585da338f021bc2ea86d6/2048.c
