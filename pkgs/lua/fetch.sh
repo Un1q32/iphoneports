ef76802ed212a72980aba2b216e19dea9274f67d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-18 21:34:59 -0500

    update lua

--- pkgs/lua/fetch.sh
+++ pkgs/lua/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.lua.org/ftp/lua-5.4.4.tar.gz
+curl -L -# -o src.tar.gz https://www.lua.org/ftp/lua-5.4.6.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv lua-5.4.4 src
+mv lua-5.4.6 src
