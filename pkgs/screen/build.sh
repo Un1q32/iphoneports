8ef99f26c7fdc932ff287334bc64b45901df3d5e
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-19 02:13:54 -0500

    update screen

--- pkgs/screen/build.sh
+++ pkgs/screen/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 (
 cd src || exit 1
-./autogen.sh
 ./configure --host="$_TARGET" --prefix=/var/usr --enable-colors256 CPPFLAGS="-Wno-implicit-function-declaration"
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
@@ -10,8 +9,9 @@ cd src || exit 1
 (
 cd pkg/var/usr || exit 1
 rm -rf share/info share/man
-"$_TARGET-strip" bin/screen-4.9.0 > /dev/null 2>&1
-ldid -S"$_ENT" bin/screen-4.9.0
+mv bin/screen-* bin/screen
+"$_TARGET-strip" bin/screen > /dev/null 2>&1
+ldid -S"$_ENT" bin/screen
 )
 
 cp -r DEBIAN pkg
