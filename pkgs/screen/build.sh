24fca6c236e0f2fc46cf6dc7397ef87cac3458cf
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-11-19 02:54:48 -0500

    update screen

--- pkgs/screen/build.sh
+++ pkgs/screen/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --enable-colors256 CPPFLAGS="-Wno-implicit-function-declaration"
+./configure --host="$_TARGET" --prefix=/var/usr --enable-pam --enable-colors256 --enable-rxvt_osc --enable-telnet CPPFLAGS="-Wno-implicit-function-declaration"
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+mkdir -p "$_PKGROOT/pkg/var/usr/etc" "$_PKGROOT/pkg/etc/pam.d"
+cp etc/etcscreenrc "$_PKGROOT/pkg/var/usr/etc/screenrc"
 )
 
 (
@@ -13,6 +15,8 @@ mv bin/screen-* bin/screen
 "$_TARGET-strip" bin/screen > /dev/null 2>&1
 ldid -S"$_ENT" bin/screen
 )
+
+cp files/screen.pam pkg/etc/pam.d/screen
 
 cp -r DEBIAN pkg
 dpkg-deb -b --root-owner-group -Zgzip pkg screen.deb
