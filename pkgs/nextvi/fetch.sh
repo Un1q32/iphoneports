ea64a649b2330ffd9bc50770cef0a1e7892fe44a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-12-02 19:14:49 -0500

    fix

--- pkgs/nextvi/fetch.sh
+++ pkgs/nextvi/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=82632579ee633084d266088f4a844aefc58e6845
+commit=16ee570e7853bdab1faa4fc35e8c9521d97a0baa
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/OldWorldOrdr/nextvi/archive/${commit}.tar.gz"
