cba9220d67a1cb9bd958edaa8e0e2b328f2710d2
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-12-06 06:01:27 -0500

    update curl

--- pkgs/curl/fetch.sh
+++ pkgs/curl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://curl.se/download/curl-8.4.0.tar.xz
+curl -L -# -o src.tar.xz https://curl.se/download/curl-8.5.0.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv curl-8.4.0 src
+mv curl-8.5.0 src
