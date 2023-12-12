318fd8cb99e2a341b877a4b937c25da9af56e4af
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-12-11 22:33:34 -0500

    update

--- pkgs/bc/fetch.sh
+++ pkgs/bc/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://git.gavinhoward.com/gavin/bc/archive/6.7.4.tar.gz
+curl -L -# -o src.tar.xz https://git.gavinhoward.com/gavin/bc/releases/download/6.7.4/bc-6.7.4.tar.xz
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv bc src
+tar -xf src.tar.xz
+rm src.tar.xz
+mv bc-6.7.4 src
