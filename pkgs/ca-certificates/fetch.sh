0767fc1a021b1b93d756c51f5554bd4faef62db7
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-12-12 04:57:55 -0500

    update ca-certificates

--- pkgs/ca-certificates/fetch.sh
+++ pkgs/ca-certificates/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/cert.pem https://curl.se/ca/cacert-2023-08-22.pem
+curl -L -# -o src/cert.pem https://curl.se/ca/cacert-2023-12-12.pem
