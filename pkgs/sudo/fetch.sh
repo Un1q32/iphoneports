031f89fa6f37c32de9c12eff7b7782d0fae09298
OldWorldOrdr <joey.t.reinhart@gmail.com>
2023-12-16 05:28:11 -0500

    update sudo

--- pkgs/sudo/fetch.sh
+++ pkgs/sudo/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.sudo.ws/dist/sudo-1.9.15p2.tar.gz
+curl -L -# -o src.tar.gz https://www.sudo.ws/dist/sudo-1.9.15p4.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv sudo-1.9.15p2 src
+mv sudo-1.9.15p4 src
