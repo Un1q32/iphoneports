9516afea7c8ffbf0828fabe1caca6afa3a31e540
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-23 23:46:16 -0500

    change variable names

--- pkgs/catgirl/files/compat.c
+++ pkgs/catgirl/files/compat.c
@@ -12,32 +12,32 @@ void explicit_bzero(void *d, size_t n) {
     __asm__ __volatile__ ("" : : "r"(d) : "memory");
 }
 
-char* _atfunc(int fd, const char* pathname) {
-    if (fd == AT_FDCWD || pathname[0] == '/')
-        return strdup(pathname);
+char* _atfunc(int fd, const char* path) {
+    if (fd == AT_FDCWD || path[0] == '/')
+        return strdup(path);
 
     char fdpath[PATH_MAX];
     if (fcntl(fd, F_GETPATH, fdpath) == -1)
         return NULL;
 
-    char path[strlen(fdpath) + strlen(pathname) + 2];
-    strcpy(path, fdpath);
-    strcat(path, "/");
-    strcat(path, pathname);
-    return strdup(path);
+    char new_path[strlen(fdpath) + strlen(path) + 2];
+    strcpy(new_path, fdpath);
+    strcat(new_path, "/");
+    strcat(new_path, path);
+    return strdup(new_path);
 }
 
-int mkdirat(int fd, const char* pathname, mode_t mode) {
-    char* path = _atfunc(fd, pathname);
-    if (path == NULL)
+int mkdirat(int fd, const char* path, mode_t mode) {
+    char* new_path = _atfunc(fd, path);
+    if (new_path == NULL)
         return -1;
 
-    int ret = mkdir(path, mode);
-    free(path);
+    int ret = mkdir(new_path, mode);
+    free(new_path);
     return ret;
 }
 
-int openat(int fd, const char* pathname, int flags, ...) {
+int openat(int fd, const char* path, int flags, ...) {
     mode_t mode = 0;
     if (flags & O_CREAT) {
         va_list ap;
@@ -46,11 +46,11 @@ int openat(int fd, const char* pathname, int flags, ...) {
         va_end(ap);
     }
 
-    char* path = _atfunc(fd, pathname);
-    if (path == NULL)
+    char* new_path = _atfunc(fd, path);
+    if (new_path == NULL)
         return -1;
 
-    int ret = open(path, flags, mode);
-    free(path);
+    int ret = open(new_path, flags, mode);
+    free(new_path);
     return ret;
 }
