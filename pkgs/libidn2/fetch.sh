6ba6e394764bdf8efc03056f27035f8b58f14d78
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-29 11:36:30 -0500

    update libidn2

--- pkgs/libidn2/fetch.sh
+++ pkgs/libidn2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/libidn/libidn2-2.3.5.tar.gz
+curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/libidn/libidn2-2.3.7.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv libidn2-2.3.5 src
+mv libidn2-2.3.7 src
