756ea9813cded3f85fd82b822fd12f4b2dc691d5
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-02-05 03:58:39 -0500

    update base

--- pkgs/base/files/iphoneports-shell.c
+++ pkgs/base/files/iphoneports-shell.c
@@ -3,10 +3,12 @@
 #include <unistd.h>
 
 int main() {
-    char shell[1024] = "/var/usr/bin/sh";
+    char shell[PATH_MAX] = "/var/usr/bin/sh";
     if (access("/var/usr/shell", F_OK) == 0)
-        readlink("/var/usr/shell", shell, 1024);
-    char* shellname = strdup(strrchr(shell, '/'));
+        readlink("/var/usr/shell", shell, PATH_MAX);
+    const char *ptr = strrchr(shell, '/');
+    char shellname[strlen(ptr) + 1];
+    strcpy(shellname, ptr);
     shellname[0] = '-';
     setenv("SHELL", shell, 1);
     return execl(shell, shellname, NULL);
