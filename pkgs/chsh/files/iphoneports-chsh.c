33fe10466c53370650dcd9a5d5eecbcdf9292ec2
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-02-07 21:03:20 -0500

    update chsh

--- pkgs/chsh/files/iphoneports-chsh.c
+++ pkgs/chsh/files/iphoneports-chsh.c
@@ -9,21 +9,17 @@ int main(int argc, const char* argv[]) {
     }
 
     if (argc != 2) {
-        puts("iPhonePorts chsh - change the default iPhonePorts shell\n\nUsage: iphoneports-chsh <SHELL>\n\nSHELL must be an executable in /var/usr/bin");
+        puts("Change the default iPhonePorts shell\n\nUsage: iphoneports-chsh <SHELL>\n\nSHELL must be an executable in /var/usr/bin");
         return 1;
     }
 
-    char shell[strlen(argv[1]) + 15];
+    char shell[strlen(argv[1]) + 14];
     strcpy(shell, "/var/usr/bin/");
     strcat(shell, argv[1]);
 
     if (access(shell, X_OK) == -1) {
-        strcpy(shell, "/var/usr/sbin/");
-        strcat(shell, argv[1]);
-        if (access(shell, X_OK) == -1) {
-            fprintf(stderr, "Error: %s is not a valid shell\n", argv[1]);
-            return 1;
-        }
+        fprintf(stderr, "Error: %s is not a valid shell\n", argv[1]);
+        return 1;
     }
 
     unlink("/var/usr/shell");
