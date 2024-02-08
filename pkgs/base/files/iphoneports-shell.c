d4b0112c1f4444f5f0541222c4d565ec36261bc4
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-02-08 02:23:23 -0500

    update base, chsh

--- pkgs/base/files/iphoneports-shell.c
+++ pkgs/base/files/iphoneports-shell.c
@@ -1,16 +1,47 @@
 #include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+bool checkshell(const char *shell, bool exitonfail) {
+    struct stat st;
+    if (stat(shell, &st) != 0) {
+        printf("Error: %s not found!\n", shell);
+        if (exitonfail)
+            exit(EXIT_FAILURE);
+        return false;
+    }
+    if (!S_ISREG(st.st_mode)) {
+        printf("Error: %s is not a regular file!\n", shell);
+        if (exitonfail)
+            exit(EXIT_FAILURE);
+        return false;
+    }
+    if ((st.st_mode & S_IXUSR) == 0) {
+        printf("Error: %s is not executable!\n", shell);
+        if (exitonfail)
+            exit(EXIT_FAILURE);
+        return false;
+    }
+    return true;
+}
+
 int main() {
     char shell[PATH_MAX] = "/var/usr/bin/sh";
-    if (access("/var/usr/shell", F_OK) == 0)
+    if (access("/var/usr/shell", F_OK) == 0) {
         readlink("/var/usr/shell", shell, PATH_MAX);
+        if (!checkshell(shell, false))
+            shell = "/var/usr/bin/sh";
+    }
+    checkshell(shell, true);
     const char *ptr = strrchr(shell, '/');
     char shellname[strlen(ptr) + 1];
     strcpy(shellname, ptr);
     shellname[0] = '-';
     setenv("SHELL", shell, 1);
-    return execl(shell, shellname, NULL);
+    execl(shell, shellname, NULL);
+    puts("Error: exec failed!");
+    return EXIT_FAILURE;
 }
--- pkgs/chsh/files/iphoneports-chsh.c
+++ pkgs/chsh/files/iphoneports-chsh.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 int main(int argc, const char* argv[]) {
     if (geteuid() != 0) {
         puts("Error: not root, is iphoneports-chsh suid root?");
-        return 1;
+        return EXIT_FAILURE;
     }
 
     if (argc != 2) {
         puts("Change the default iPhonePorts shell\n\nUsage: iphoneports-chsh <SHELL>\n\nSHELL must be an executable in /var/usr/bin");
-        return 1;
+        return EXIT_FAILURE;
     }
 
     char shell[strlen(argv[1]) + 14];
     strcpy(shell, "/var/usr/bin/");
     strcat(shell, argv[1]);
 
-    if (access(shell, X_OK) == -1) {
-        fprintf(stderr, "Error: %s is not a valid shell\n", argv[1]);
-        return 1;
+    struct stat st;
+    if (stat(shell, &st) != 0) {
+        fprintf(stderr, "Error: %s does not exist\n", shell);
+        return EXIT_FAILURE;
+    }
+    if (!S_ISREG(st.st_mode)) {
+        fprintf(stderr, "Error: %s is not a regular file\n", shell);
+        return EXIT_FAILURE;
+    }
+    if ((st.st_mode & S_IXUSR) == 0) {
+        fprintf(stderr, "Error: %s is not executable\n", shell);
+        return EXIT_FAILURE;
     }
 
     unlink("/var/usr/shell");
     symlink(shell, "/var/usr/shell");
     printf("iPhonePorts shell changed to %s\n", argv[1]);
-    return 0;
+    return EXIT_SUCCESS;
 }
