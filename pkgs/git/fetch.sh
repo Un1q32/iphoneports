827804dc41490e5abd2092e3a3584cfaf36a0f11
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-02-15 10:58:17 -0500

    update git

--- pkgs/git/fetch.sh
+++ pkgs/git/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.43.1.tar.xz
+curl -L -# -o src.tar.xz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.43.2.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv git-2.43.1 src
+mv git-2.43.2 src
