595a9747172b774162529357d4129c37b9aed455
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-02-16 22:49:38 -0500

    update pcre2

--- pkgs/pcre2/fetch.sh
+++ pkgs/pcre2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.bz2
+curl -L -# -o src.tar.bz2 https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.43/pcre2-10.43.tar.bz2
 printf "Unpacking source...\n"
 tar -xf src.tar.bz2
 rm src.tar.bz2
-mv pcre2-10.42 src
+mv pcre2-10.43 src
