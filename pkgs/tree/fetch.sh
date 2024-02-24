f40ac9bf13c4deaacd69c9470016d67a8e0da298
Un1q32 <joey.t.reinhart@gmail.com>
2024-02-24 16:41:03 -0500

    fix tree

--- pkgs/tree/fetch.sh
+++ pkgs/tree/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://mama.indstate.edu/users/ice/tree/src/tree-2.1.1.tgz
+curl -L -# -o src.tar.gz https://oldmanprogrammer.net/tar/tree/tree-2.1.1.tgz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
