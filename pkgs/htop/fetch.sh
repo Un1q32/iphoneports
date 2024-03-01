42ed7a4c2c5510835d105452f1158f1b5c1cfba8
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-01 03:32:55 -0500

    update htop

--- pkgs/htop/fetch.sh
+++ pkgs/htop/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/htop-dev/htop/archive/refs/tags/3.0.2.tar.gz
+curl -L -# -o src.tar.gz https://github.com/htop-dev/htop/archive/refs/tags/3.3.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv htop-3.0.2 src
+mv htop-3.3.0 src
