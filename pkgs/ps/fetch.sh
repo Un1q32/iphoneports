bb1fdea9992389160f116132bfd185dcee87126a
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-01 20:43:03 -0500

    downgrade ps to work

--- pkgs/ps/fetch.sh
+++ pkgs/ps/fetch.sh
@@ -3,6 +3,6 @@ rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
 for src in ps.c print.c nlist.c tasks.c keyword.c ps.h extern.h; do
-    curl -L -s -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/adv_cmds/adv_cmds-172/ps/$src" &
+    curl -L -s -o "src/$src" "https://raw.githubusercontent.com/apple-oss-distributions/adv_cmds/adv_cmds-138.1/ps/$src" &
 done
 wait
