10b6aa034e504d11f54e4bd78aee6eba5761db63
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-01 21:30:11 -0500

    remove gdb

--- pkgs/gdb/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p pkg/var/usr/bin
-cp src/gdb pkg/var/usr/bin
-chmod 755 pkg/var/usr/bin/gdb
-
-cp -r DEBIAN pkg
-dpkg-deb -b --root-owner-group -Zgzip pkg gdb.deb
--- pkgs/gdb/fetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-mkdir src
-curl -L -# -o src/gdb https://raw.githubusercontent.com/Un1q32/iphone-gdb/master/gdb
