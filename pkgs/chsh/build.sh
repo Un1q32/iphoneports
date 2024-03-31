ae0f82ae08447906aa431461f064332fc12cbab7
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-31 00:58:31 -0400

    tweak chsh

--- pkgs/chsh/build.sh
+++ pkgs/chsh/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p "$_PKGROOT/pkg/usr/bin"
-"$_TARGET-cc" -std=c89 -O2 "$_PKGROOT/files/iphoneports-chsh.c" -o "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
+"$_TARGET-cc" -Wall -Wextra -Wpedantic -std=c99 -O2 "$_PKGROOT/files/iphoneports-chsh.c" -o "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
 
 llvm-strip "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
 ldid -S"$_ENT" "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
