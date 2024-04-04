499f0b273609a6a5d4d546b996635b81ae8fe1bb
Un1q32 <joey.t.reinhart@gmail.com>
2024-04-04 11:06:17 -0400

    update nextvi

--- pkgs/nextvi/fetch.sh
+++ pkgs/nextvi/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=a61425fe8f936890fc8476792d8d983ae881fa4c
+commit=3183846a7ee934ef9bc33e6c9e416b9d86874154
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/Un1q32/nextvi/archive/${commit}.tar.gz"
