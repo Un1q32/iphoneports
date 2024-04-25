45638e2a97718609b5024577a513a565af0cb820
Un1q32 <joey.t.reinhart@gmail.com>
2024-04-25 17:20:06 -0400

    update ncdu

--- pkgs/ncdu/fetch.sh
+++ pkgs/ncdu/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://dev.yorhel.nl/download/ncdu-1.19.tar.gz
+curl -L -# -o src.tar.gz https://dev.yorhel.nl/download/ncdu-1.20.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv ncdu-1.19 src
+mv ncdu-1.20 src
