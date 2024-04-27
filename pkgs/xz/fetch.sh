55bf9967a2e67ead50c6987b28e3b918a1721fbc
Un1q32 <joey.t.reinhart@gmail.com>
2024-04-26 21:17:57 -0400

    update xz build

--- pkgs/xz/fetch.sh
+++ pkgs/xz/fetch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-git clone https://git.tukaani.org/xz.git -b v5.4.6 src --depth 1
+curl -L -# -o src.tar.zst https://github.com/tukaani-project/xz/releases/download/v5.4.6/xz-5.4.6.tar.zst
+printf "Unpacking source...\n"
+tar -xf src.tar.zst
+rm src.tar.zst
+mv xz-5.4.6 src
