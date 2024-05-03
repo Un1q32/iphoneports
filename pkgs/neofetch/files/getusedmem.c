76ec368606525aeb86aafcd1b890124ff15be443
Un1q32 <joey.t.reinhart@gmail.com>
2024-05-02 23:34:59 -0400

    format

--- pkgs/neofetch/files/getusedmem.c
+++ pkgs/neofetch/files/getusedmem.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <mach/mach.h>
+#include <stdio.h>
 
-int main() {
-    vm_statistics64_data_t vm_stat;
-    mach_msg_type_number_t count = sizeof(vm_stat) / sizeof(natural_t);
-    host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stat, &count);
-    printf("%llu\n", (vm_stat.active_count + vm_stat.inactive_count) * 4ULL);
-    return 0;
+int main(void) {
+  vm_statistics64_data_t vm_stat;
+  mach_msg_type_number_t count = sizeof(vm_stat) / sizeof(natural_t);
+  host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stat,
+                    &count);
+  printf("%llu\n", (vm_stat.active_count + vm_stat.inactive_count) * 4ULL);
+  return 0;
 }
