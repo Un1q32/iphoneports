6556054d369c47b11bb1576a8dd7c2f6ea848c04
Un1q32 <joey.t.reinhart@gmail.com>
2024-05-13 22:21:37 -0400

    update nghttp2

--- pkgs/nghttp2/fetch.sh
+++ pkgs/nghttp2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/nghttp2/nghttp2/releases/download/v1.61.0/nghttp2-1.61.0.tar.xz
+curl -L -# -o src.tar.xz https://github.com/nghttp2/nghttp2/releases/download/v1.62.0/nghttp2-1.62.0.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv nghttp2-1.61.0 src
+mv nghttp2-1.62.0 src
