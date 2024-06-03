eba7eff078d83484e7f6af33acd068a08525576c
Un1q32 <joey.t.reinhart@gmail.com>
2024-06-03 18:21:21 -0400

    update git

--- pkgs/git/fetch.sh
+++ pkgs/git/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.45.1.tar.xz
+curl -L -# -o src.tar.xz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.45.2.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv git-2.45.1 src
+mv git-2.45.2 src
