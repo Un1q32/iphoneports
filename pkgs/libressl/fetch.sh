5fa919385928564a8072ba2638bca16a7c172589
Un1q32 <joey.t.reinhart@gmail.com>
2024-06-04 01:07:43 -0400

    update libressl

--- pkgs/libressl/fetch.sh
+++ pkgs/libressl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.9.1.tar.gz
+curl -L -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.9.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv libressl-3.9.1 src
+mv libressl-3.9.2 src
