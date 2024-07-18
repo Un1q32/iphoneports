8ce6992a276e0b29e6f48b464e5bef553b33366a
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-18 18:35:41 -0400

    update tree

--- pkgs/tree/fetch.sh
+++ pkgs/tree/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://oldmanprogrammer.net/tar/tree/tree-2.1.2.tgz
+curl -L -# -o src.tar.gz https://oldmanprogrammer.net/tar/tree/tree-2.1.3.tgz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv tree-2.1.2 src
+mv tree-2.1.3 src
