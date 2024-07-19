2c1feb7028467196494022e433eefb510ced7b6f
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-18 20:11:46 -0400

    chsh: only in iphoneports path

--- pkgs/chsh/build.sh
+++ pkgs/chsh/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-mkdir -p "$_PKGROOT/pkg/usr/bin"
-"$_TARGET-cc" -Wall -Wextra -Wpedantic -std=c99 -O2 "$_PKGROOT/files/iphoneports-chsh.c" -o "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
+mkdir -p "$_PKGROOT/pkg/usr/libexec/iphoneports" "$_PKGROOT/pkg/var/usr/bin"
+"$_TARGET-cc" -Wall -Wextra -Wpedantic -std=c99 -O2 "$_PKGROOT/files/iphoneports-chsh.c" -o "$_PKGROOT/pkg/usr/libexec/iphoneports/iphoneports-chsh"
 
-llvm-strip "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
-ldid -S"$_ENT" "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
-chmod 4755 "$_PKGROOT/pkg/usr/bin/iphoneports-chsh"
+llvm-strip "$_PKGROOT/pkg/usr/libexec/iphoneports/iphoneports-chsh"
+ldid -S"$_ENT" "$_PKGROOT/pkg/usr/libexec/iphoneports/iphoneports-chsh"
+chmod 4755 "$_PKGROOT/pkg/usr/libexec/iphoneports/iphoneports-chsh"
+
+ln -s /usr/libexec/iphoneports/iphoneports-chsh "$_PKGROOT/pkg/var/usr/bin/chsh"
 
 cp -r DEBIAN pkg
 dpkg-deb -b --root-owner-group -Zgzip pkg chsh.deb
