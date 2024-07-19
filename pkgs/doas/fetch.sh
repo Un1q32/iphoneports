99a28454c71aa56cfee3139f903d5a4c44dc9de4
Un1q32 <joey.t.reinhart@gmail.com>
2024-07-18 22:33:09 -0400

    update doas

--- pkgs/doas/fetch.sh
+++ pkgs/doas/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/slicer69/doas/archive/refs/tags/6.3p9.tar.gz
+curl -L -# -o src.tar.gz https://github.com/slicer69/doas/archive/refs/tags/6.3p12.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv doas-6.3p9 src
+mv doas-6.3p12 src
