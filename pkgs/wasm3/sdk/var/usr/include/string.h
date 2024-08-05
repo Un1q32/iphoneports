083bac5aad7ec56eeaed51c8a7e8b24f724444a6
Un1q32 <joey.t.reinhart@gmail.com>
2024-08-05 16:21:48 -0400

    fix

--- pkgs/wasm3/sdk/var/usr/include/string.h
+++ pkgs/wasm3/sdk/var/usr/include/string.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <stddef.h>
-#include <sys/cdefs.h>
+#include_next <string.h>
 
 __BEGIN_DECLS
 extern size_t strnlen(const char *, size_t);
 __END_DECLS
-
-#include_next <string.h>
