d1a803cd9cd914d0c92890b7296caa0fe917e09f
Un1q32 <joey.t.reinhart@gmail.com>
2024-08-14 18:39:23 -0400

    update sqlite

--- pkgs/sqlite/fetch.sh
+++ pkgs/sqlite/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.sqlite.org/2024/sqlite-autoconf-3460000.tar.gz
+curl -L -# -o src.tar.gz https://www.sqlite.org/2024/sqlite-autoconf-3460100.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv sqlite-autoconf-3460000 src
+mv sqlite-autoconf-3460100 src
