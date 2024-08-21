3b8fa47b895760b8b9a8d90a976110c050521286
Un1q32 <joey.t.reinhart@gmail.com>
2024-08-21 03:38:54 -0400

    add classpath

---
+++ pkgs/classpath/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+(
+cd src || exit 1
+./autogen.sh
+./configure --host="$_TARGET" --prefix=/var/usr --disable-examples --disable-gconf-peer --disable-gtk-peer --disable-plugin
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr/lib/classpath || exit 1
+for lib in lib*.dylib; do
+  if ! [ -h "$lib" ]; then
+    llvm-strip "$lib"
+    ldid -S"$_ENT" "$lib"
+  fi
+done
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg classpath.deb
