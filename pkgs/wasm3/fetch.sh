8282b85d0dc55d247f2b46fad690c788bcbd2f0e
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-01 20:01:10 -0400

    update wasm3

--- pkgs/wasm3/fetch.sh
+++ pkgs/wasm3/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=256cd37bbd654c6b3dc1f7523ebc651ee4a87989
+commit=00b35a122bcc4e694016f281ce28651a7ec527c3
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/wasm3/wasm3/archive/${commit}.tar.gz"
