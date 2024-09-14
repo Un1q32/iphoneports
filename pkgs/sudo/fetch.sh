2778636461260c9a3bab270c0b9d27b20b448d9e
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-13 20:13:00 -0400

    update sudo

--- pkgs/sudo/fetch.sh
+++ pkgs/sudo/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.sudo.ws/dist/sudo-1.9.15p5.tar.gz
+curl -L -# -o src.tar.gz https://www.sudo.ws/dist/sudo-1.9.16.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv sudo-1.9.15p5 src
+mv sudo-1.9.16 src
