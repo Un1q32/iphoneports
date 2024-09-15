01446f31d0748f75c0ebdd6a15cc730dcf208c46
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-14 20:13:39 -0400

    add pfetch

---
+++ pkgs/pfetch/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p pkg/var/usr/bin
+cp src/pfetch pkg/var/usr/bin
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg pfetch.deb
---
+++ pkgs/pfetch/fetch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+mkdir -p src
+curl -L -# -o src/pfetch https://raw.githubusercontent.com/Un1q32/pfetch/1.4.0/pfetch
+chmod 755 src/pfetch
