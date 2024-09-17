f74ca06f92b1ea16e3ae7fa7df70cdd8e7c1d325
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-17 16:29:24 +0000

    update zlib

--- pkgs/zlib/fetch.sh
+++ pkgs/zlib/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.1.tar.gz
+curl -L -# -o src.tar.gz https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv zlib-ng-2.2.1 src
+mv zlib-ng-2.2.2 src
