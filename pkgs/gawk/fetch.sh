24ca33954d784402dcc579aa08a6a0cd88ff3bff
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-18 23:32:41 -0400

    update gawk

--- pkgs/gawk/fetch.sh
+++ pkgs/gawk/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/gawk/gawk-5.3.0.tar.xz
+curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/gawk/gawk-5.3.1.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv gawk-5.3.0 src
+mv gawk-5.3.1 src
