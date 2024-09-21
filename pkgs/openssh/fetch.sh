831f8f4cd930949aa1b830913e65ca8ecf857a8e
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-21 12:48:28 -0400

    update openssh

--- pkgs/openssh/fetch.sh
+++ pkgs/openssh/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.8p1.tar.gz
+curl -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.9p1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv openssh-9.8p1 src
+mv openssh-9.9p1 src
