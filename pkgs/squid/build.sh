b444d0f2f8416d54157357723199183f44bb0b0c
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-24 19:06:19 -0400

    add squid

---
+++ pkgs/squid/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr BUILDCXX=c++ CXXFLAGS='-O2 -Wno-vla-cxx-extension -Wno-gnu-folding-constant' AR="$(command -v "$_TARGET-ar")" enable_arch_native=no
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr/libexec || exit 1
+rm -rf ../share/icons ../share/man
+llvm-strip ../bin/squidclient ../bin/purge ../sbin/squid basic_fake_auth basic_getpwnam_auth basic_ncsa_auth basic_radius_auth basic_smb_auth cachemgr.cgi digest_file_auth diskd ext_file_userip_acl ext_unix_group_acl log_file_daemon negotiate_wrapper_auth ntlm_fake_auth unlinkd url_fake_rewrite
+ldid -S"$_ENT" ../bin/squidclient ../bin/purge ../sbin/squid basic_fake_auth basic_getpwnam_auth basic_ncsa_auth basic_radius_auth basic_smb_auth cachemgr.cgi digest_file_auth diskd ext_file_userip_acl ext_unix_group_acl log_file_daemon negotiate_wrapper_auth ntlm_fake_auth unlinkd url_fake_rewrite
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg squid.deb
