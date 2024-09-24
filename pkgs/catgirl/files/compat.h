15841d86bb29c5299916f3418d694fb21b1a1c61
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-23 20:50:09 -0400

    header guard

--- pkgs/catgirl/files/compat.h
+++ pkgs/catgirl/files/compat.h
@@ -1,3 +1,6 @@
+#ifndef _IPHONEPORTS_COMPAT_H_
+#define _IPHONEPORTS_COMPAT_H_
+
 #include <stdio.h>
 #include <sys/types.h>
 
@@ -9,3 +12,5 @@ extern int openat(int, const char *, int, ...);
 extern void *memmem(const void *, size_t, const void *, size_t);
 extern ssize_t getline(char **, size_t *, FILE *);
 extern ssize_t getdelim(char **, size_t *, int, FILE *);
+
+#endif
