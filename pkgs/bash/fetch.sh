70dee63cc5d9f832eb760ef03e5ad1089aa4fb1b
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-25 15:00:37 -0400

    update bash

--- pkgs/bash/fetch.sh
+++ pkgs/bash/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/bash/bash-5.2.32.tar.gz
+curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/bash/bash-5.2.37.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv bash-5.2.32 src
+mv bash-5.2.37 src
