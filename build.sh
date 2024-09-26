ed323a75cb755d35c96581b5f6c3a8efbf872030
Un1q32 <joey.t.reinhart@gmail.com>
2024-09-25 22:44:05 -0400

    use ninja for libc++ build

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ error() {
 }
 
 depcheck() {
-    for dep in "$_TARGET-gcc" "$_TARGET-g++" "$_TARGET-cc" "$_TARGET-c++" "$_TARGET-sdkpath" llvm-strip ldid dpkg-deb patch fakeroot automake autoreconf m4 yacc ctags tar gzip bzip2 xz zstd ninja; do
+    for dep in "$_TARGET-gcc" "$_TARGET-g++" "$_TARGET-cc" "$_TARGET-c++" "$_TARGET-strip" "$_TARGET-sdkpath" llvm-strip ldid dpkg-deb patch fakeroot automake autoreconf m4 yacc ctags tar gzip bzip2 xz zstd ninja; do
         if ! command -v "$dep" > /dev/null; then
             error "Missing dependency: $dep"
         fi
