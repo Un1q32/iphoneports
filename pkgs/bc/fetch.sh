0237d64a7fb17773bcac9075a67c04671c8c0bc7
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-03 01:10:59 -0400

    update bc

--- pkgs/bc/fetch.sh
+++ pkgs/bc/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://git.gavinhoward.com/gavin/bc/releases/download/7.0.2/bc-7.0.2.tar.xz
+curl -L -# -o src.tar.xz https://git.gavinhoward.com/gavin/bc/releases/download/7.0.3/bc-7.0.3.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv bc-7.0.2 src
+mv bc-* src
