dff6a6d8922abab4275da17dbb60b564a2b1c6f3
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-03 01:04:54 -0400

    update luajit

--- pkgs/luajit/fetch.sh
+++ pkgs/luajit/fetch.sh
@@ -3,4 +3,4 @@ rm -rf pkg src
 printf "Downloading source...\n"
 git clone https://luajit.org/git/luajit.git src
 cd src || exit 1
-git -c advice.detachedHead=false checkout 87ae18af97fd4de790bb6c476b212e047689cc93
+git -c advice.detachedHead=false checkout 97813fb924edf822455f91a5fbbdfdb349e5984f
