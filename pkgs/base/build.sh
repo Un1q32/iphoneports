6262e54f930294eeb53059a278ac8eb0b8b81d6a
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-03 15:05:05 +0000

    update base

--- pkgs/base/build.sh
+++ pkgs/base/build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-mkdir -p pkg/usr/bin pkg/var/usr/bin pkg/var/usr/etc/profile.d
+mkdir -p pkg/usr/bin pkg/usr/libexec/iphoneports pkg/var/usr/bin pkg/var/usr/etc/profile.d
 
 (
 cd pkg || exit 1
 
 "$_TARGET-cc" -Wall -Wextra -Wpedantic -std=c99 -O2 -o usr/bin/iphoneports-shell "$_PKGROOT/files/iphoneports-shell.c"
-"$_TARGET-strip" usr/bin/iphoneports-shell 2>/dev/null
-ldid -S"$_ENT" usr/bin/iphoneports-shell
+"$_TARGET-cc" -Wall -Wextra -Wpedantic -std=c99 -O2 -o usr/libexec/iphoneports/iphoneports-chsh "$_PKGROOT/files/iphoneports-chsh.c"
+
+"$_TARGET-strip" usr/bin/iphoneports-shell usr/libexec/iphoneports/iphoneports-chsh 2>/dev/null
+ldid -S"$_ENT" usr/bin/iphoneports-shell usr/libexec/iphoneports/iphoneports-chsh
+chmod 4755 usr/libexec/iphoneports/iphoneports-chsh
+ln -s /usr/libexec/iphoneports/iphoneports-chsh var/usr/bin/iphoneports-chsh
 
 cp "$_PKGROOT/files/profile" var/usr/etc
 cp "$_PKGROOT/files/path-wrapper" var/usr/bin
