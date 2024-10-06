e4da5e50b536eb32318e6724a44febeeafc25daa
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-06 20:43:04 +0000

    add nghttp3

---
+++ pkgs/nghttp3/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+mkdir -p src/build
+(
+cd src/build || exit 1
+cmake -GNinja .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_INSTALLNAMETOOL" -DCMAKE_INSTALL_NAME_DIR=/var/usr/lib -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DENABLE_STATIC_LIB=OFF -DENABLE_LIB_ONLY=yes
+DESTDIR="$_PKGROOT/pkg" ninja install -j8
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+mv lib/libnghttp3.9.*.dylib lib/libnghttp3.9.dylib
+"$_TARGET-strip" lib/libnghttp3.9.dylib 2>/dev/null
+ldid -S"$_ENT" lib/libnghttp3.9.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg nghttp3.deb
---
+++ pkgs/nghttp3/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.xz https://github.com/ngtcp2/nghttp3/releases/download/v1.6.0/nghttp3-1.6.0.tar.xz
+printf "Unpacking source...\n"
+tar -xf src.tar.xz
+rm src.tar.xz
+mv nghttp3-* src
