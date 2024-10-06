bdd0d70af1c168a1d3116e34b24ee4f35a27286b
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-06 21:43:34 +0000

    fix curl

--- pkgs/curl/build.sh
+++ pkgs/curl/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --with-openssl --with-ca-fallback --disable-static --with-libssh2 --with-nghttp2 --with-nghttp3 --with-ngtcp2 --with-libidn2 --with-ca-bundle=/var/usr/etc/ssl/cert.pem
+./configure --host="$_TARGET" --prefix=/var/usr --with-openssl --with-ca-fallback --disable-static --with-libssh2 --with-nghttp2 --with-nghttp3 --with-ngtcp2 --with-libidn2 --with-ca-bundle=/var/usr/etc/ssl/cert.pem LIBS=-lngtcp2_crypto_quictls
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
