d7bff2a44c4730b8061d74665ab8a17196e0ff5b
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-06 20:24:16 +0000

    nit

--- pkgs/nghttp2/fetch.sh
+++ pkgs/nghttp2/fetch.sh
@@ -5,4 +5,4 @@ curl -L -# -o src.tar.xz https://github.com/nghttp2/nghttp2/releases/download/v1
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv nghttp2-1.63.0 src
+mv nghttp2-* src
