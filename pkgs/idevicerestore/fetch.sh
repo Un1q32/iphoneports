70e130e7c1b68df82e85cb34036d9d3d9da39d32
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-09 11:10:33 -0400

    update idevicerestore

--- pkgs/idevicerestore/fetch.sh
+++ pkgs/idevicerestore/fetch.sh
@@ -3,4 +3,4 @@ rm -rf pkg src
 printf "Downloading source...\n"
 git clone https://github.com/libimobiledevice/idevicerestore.git src
 cd src || exit 1
-git -c advice.detachedHead=false checkout 0548d9f20d2937d1e122d0b405cb60219ec4fa0f
+git -c advice.detachedHead=false checkout 511261e12d23d80cc3c08290022380b8d3411f9c
