e502aa6e385820eee103f1c96e73e0ebe89f12bf
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-09 11:00:30 -0400

    add libtatsu

---
+++ pkgs/libtatsu/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr/lib || exit 1
+"$_TARGET-strip" libtatsu.0.dylib 2>/dev/null
+ldid -S"$_ENT" libtatsu.0.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libtatsu.deb
---
+++ pkgs/libtatsu/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.bz2 https://github.com/libimobiledevice/libtatsu/releases/download/1.0.3/libtatsu-1.0.3.tar.bz2
+printf "Unpacking source...\n"
+tar -xf src.tar.bz2
+rm src.tar.bz2
+mv libtatsu-* src
