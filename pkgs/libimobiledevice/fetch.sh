9bc3721f734286b333886d4936600e19bbb37bb3
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-09 11:03:48 -0400

    update libimobiledevice

--- pkgs/libimobiledevice/fetch.sh
+++ pkgs/libimobiledevice/fetch.sh
@@ -3,4 +3,4 @@ rm -rf pkg src
 printf "Downloading source...\n"
 git clone https://github.com/libimobiledevice/libimobiledevice.git src
 cd src || exit 1
-git -c advice.detachedHead=false checkout 9ccc52222c287b35e41625cc282fb882544676c6
+git -c advice.detachedHead=false checkout 881102944edf36abc98539b10d13e2375fda88cf
