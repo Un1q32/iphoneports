495944ce5730187131118d03919a58fd9440b404
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-09 11:06:38 -0400

    update usbmuxd

--- pkgs/usbmuxd/fetch.sh
+++ pkgs/usbmuxd/fetch.sh
@@ -3,4 +3,4 @@ rm -rf pkg src
 printf "Downloading source...\n"
 git clone https://github.com/libimobiledevice/usbmuxd.git src
 cd src || exit 1
-git -c advice.detachedHead=false checkout 360619c5f721f93f0b9d8af1a2df0b926fbcf281
+git -c advice.detachedHead=false checkout 0b1b233b57d581515978a09e5a4394bfa4ee4962
