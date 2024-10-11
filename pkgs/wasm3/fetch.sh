d6d28309f097836c1ab4509d05e15e03b0d61943
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-11 21:19:35 +0000

    update commit hash for wasm3, no functional changes

--- pkgs/wasm3/fetch.sh
+++ pkgs/wasm3/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=00b35a122bcc4e694016f281ce28651a7ec527c3
+commit=79d412ea5fcf92f0efe658d52827a0e0a96ff442
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/wasm3/wasm3/archive/${commit}.tar.gz"
