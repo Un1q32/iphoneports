a63b90775dc5c6a15c64761732583b1c0a8c5a7a
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-11 19:49:55 +0000

    add libuv

---
+++ pkgs/libuv/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+"$_TARGET-cc" -O2 -c -o src/compat.o files/compat.c
+
+(
+cd src || exit 1
+./autogen.sh
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static CPPFLAGS=-Wno-incompatible-function-pointer-types LIBS="$_PKGROOT/src/compat.o"
+"$_MAKE" -j8
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg"
+)
+
+(
+cd pkg/var/usr/lib || exit 1
+"$_TARGET-strip" libuv.1.dylib 2>/dev/null
+ldid -S"$_ENT" libuv.1.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libuv.deb
---
+++ pkgs/libuv/sdk/var/usr/include/string.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include_next <string.h>
+
+__BEGIN_DECLS
+extern size_t strnlen(const char *, size_t);
+__END_DECLS
