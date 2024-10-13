27997fec210652c7977513def7bdd1bee0a6f480
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-13 19:32:50 -0400

    update squid

--- pkgs/squid/build.sh
+++ pkgs/squid/build.sh
@@ -8,7 +8,7 @@ cd src || exit 1
 
 (
 cd pkg/var/usr/libexec || exit 1
-rm -rf ../share/icons ../share/man
+rm -rf ../share/man
 "$_TARGET-strip" ../bin/squidclient ../bin/purge ../sbin/squid basic_fake_auth basic_getpwnam_auth basic_ncsa_auth basic_radius_auth basic_smb_auth cachemgr.cgi digest_file_auth diskd ext_file_userip_acl ext_unix_group_acl log_file_daemon negotiate_wrapper_auth ntlm_fake_auth unlinkd url_fake_rewrite 2>/dev/null
 ldid -S"$_ENT" ../bin/squidclient ../bin/purge ../sbin/squid basic_fake_auth basic_getpwnam_auth basic_ncsa_auth basic_radius_auth basic_smb_auth cachemgr.cgi digest_file_auth diskd ext_file_userip_acl ext_unix_group_acl log_file_daemon negotiate_wrapper_auth ntlm_fake_auth unlinkd url_fake_rewrite
 )
