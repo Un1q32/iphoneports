57e70d6c813811da30a1a45605de7cb3213ae84f
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-13 20:11:44 -0400

    remove squid and httpiss

--- pkgs/httpiss/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p pkg/var/usr/share/httpiss pkg/Library/LaunchAgents
-cp files/squid.conf pkg/var/usr/share/httpiss
-cp files/com.iphoneports.httpiss.plist pkg/Library/LaunchAgents
-
-cp -r DEBIAN pkg
-dpkg-deb -b --root-owner-group -Zgzip pkg httpiss.deb
--- pkgs/squid/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://www.squid-cache.org/Versions/v6/squid-6.12.tar.xz
-printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
-mv squid-* src
