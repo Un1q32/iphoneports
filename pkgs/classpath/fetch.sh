8570e568bb0ee0d7d23895fcd8599f2f5e1214ec
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-15 15:33:46 +0000

    retire classpath package

--- pkgs/classpath/fetch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-commit=f048aedb5e6360d1617e773b4ea793cb0b6e3fdd
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz "https://github.com/ingelabs/classpath/archive/${commit}.tar.gz"
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv "classpath-${commit}" src
