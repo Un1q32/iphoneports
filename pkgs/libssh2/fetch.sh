f3df2fcb16b3c30e8fbd8a82710f576eb97e8279
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-18 12:06:50 -0400

    update libssh2

--- pkgs/libssh2/fetch.sh
+++ pkgs/libssh2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://libssh2.org/download/libssh2-1.11.0.tar.gz
+curl -L -# -o src.tar.gz https://libssh2.org/download/libssh2-1.11.1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv libssh2-1.11.0 src
+mv libssh2-1.11.1 src
