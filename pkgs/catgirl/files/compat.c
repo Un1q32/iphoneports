4533bfbf1d68233ac3019e322407cd8c8b8a24d0
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-19 00:51:21 -0400

    syntax nit

--- pkgs/catgirl/files/compat.c
+++ pkgs/catgirl/files/compat.c
@@ -57,10 +57,9 @@ void *memmem(const void *haystack, size_t haystacklen, const void *needle,
 
   const char *h = haystack;
   const char *n = needle;
-  for (size_t i = 0; i <= haystacklen - needlelen; i++) {
+  for (size_t i = 0; i <= haystacklen - needlelen; i++)
     if (memcmp(h + i, n, needlelen) == 0)
       return (void *)(h + i);
-  }
   return NULL;
 }
 
