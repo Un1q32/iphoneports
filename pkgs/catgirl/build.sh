e1e46061f7595349a2820e22498d1c17c35c6ed2
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-19 01:02:15 -0400

    enable LTO for catgirl

--- pkgs/catgirl/build.sh
+++ pkgs/catgirl/build.sh
@@ -2,7 +2,7 @@
 (
 cd src || exit 1
 ctags -w ./*.[ch]
-"$_TARGET-cc" -O2 buffer.c chat.c command.c complete.c config.c edit.c filter.c handle.c input.c irc.c log.c ui.c url.c window.c xdg.c "$_PKGROOT/files/compat.c" -include "$_PKGROOT/files/compat.h" -o catgirl -std=c11 -lncurses -ltls
+"$_TARGET-cc" -O3 -flto buffer.c chat.c command.c complete.c config.c edit.c filter.c handle.c input.c irc.c log.c ui.c url.c window.c xdg.c "$_PKGROOT/files/compat.c" -include "$_PKGROOT/files/compat.h" -o catgirl -std=c11 -lncurses -ltls
 mkdir -p "$_PKGROOT/pkg/var/usr/bin"
 cp catgirl "$_PKGROOT/pkg/var/usr/bin"
 )
