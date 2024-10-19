aaf212c759200f76936a6e6f2f8db911ef0b7f90
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-18 20:25:16 -0400

    fix pstree version

--- pkgs/pstree/fetch.sh
+++ pkgs/pstree/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -# -o src/pstree.c https://raw.githubusercontent.com/FredHucht/pstree/main/pstree.c
+curl -# -o src/pstree.c https://raw.githubusercontent.com/FredHucht/pstree/ec7999a748f2ce167a36756f11df0c5d8101170e/pstree.c
