ea89ea1bb8b6ff6f8eb6a6a96390786f71164d1b
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-19 21:25:26 -0400

    add ffmpeg

---
+++ pkgs/ffmpeg/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+(
+cd src || exit 1
+"$_TARGET-cc" -O3 -flto extendhfsf2.c truncsfhf2.c -c
+cpu="${_TARGET%%-*}"
+./configure --prefix=/var/usr --cross-prefix="$_TARGET-" --arch="$cpu" --target-os=darwin --stdc=c23 --disable-stripping --disable-debug --disable-doc --disable-avdevice --extra-libs="$_PKGROOT/src/extendhfsf2.o $_PKGROOT/src/truncsfhf2.o" --disable-static --enable-shared --enable-lto --enable-zlib --enable-lzma --enable-bzlib --enable-libxml2 --enable-openssl --pkg-config="$_PKGROOT/files/pc"
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" bin/ffmpeg bin/ffprobe 2>/dev/null
+ldid -S"$_ENT" bin/ffmpeg bin/ffprobe
+
+cd lib || exit 1
+for lib in pkgconfig/*.pc; do
+  lib="${lib%.pc*}"
+  lib="${lib##*/}"
+
+  "$_TARGET-strip" "$(readlink "$lib.dylib")" 2>/dev/null
+  ldid -S"$_ENT" "$(readlink "$lib.dylib")"
+done
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg ffmpeg.deb
---
+++ pkgs/ffmpeg/fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.xz https://ffmpeg.org/releases/ffmpeg-7.1.tar.xz
+printf "Unpacking source...\n"
+tar -xf src.tar.xz
+rm src.tar.xz
+mv ffmpeg-* src
+
+for src in extendhfsf2.c truncsfhf2.c fp_extend_impl.inc fp_trunc_impl.inc fp_extend.h fp_trunc.h int_lib.h int_types.h int_endianness.h int_util.h; do
+  curl -s -o "src/$src" "https://raw.githubusercontent.com/llvm/llvm-project/refs/tags/llvmorg-19.1.2/compiler-rt/lib/builtins/$src" &
+done
+wait
