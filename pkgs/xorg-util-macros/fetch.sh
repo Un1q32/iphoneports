14fdf98378101b48295385c34ff8973b7d0092b8
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-23 21:52:11 +0000

    update xorg-util-macros

--- pkgs/xorg-util-macros/fetch.sh
+++ pkgs/xorg-util-macros/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://xorg.freedesktop.org/releases/individual/util/util-macros-1.20.0.tar.xz
+curl -L -# -o src.tar.xz https://xorg.freedesktop.org/releases/individual/util/util-macros-1.20.1.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv util-macros-1.20.0 src
+mv util-macros-* src
