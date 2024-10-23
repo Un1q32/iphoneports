b65dfe2f98f60c309fcdee30529183f982035b7a
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-23 17:55:39 +0000

    update sqlite

--- pkgs/sqlite/build.sh
+++ pkgs/sqlite/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --disable-static --enable-static-shell=no
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static --disable-static-shell --enable-silent-rules
 "$_MAKE" -j8
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg"
 )
