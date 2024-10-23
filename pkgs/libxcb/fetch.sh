4a322776e7ced1785321e5b3492918291f648a37
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-23 22:00:07 +0000

    update libxcb

--- pkgs/libxcb/fetch.sh
+++ pkgs/libxcb/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://xorg.freedesktop.org/archive/individual/lib/libxcb-1.16.1.tar.xz
+curl -L -# -o src.tar.xz https://xorg.freedesktop.org/archive/individual/lib/libxcb-1.17.0.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv libxcb-1.16.1 src
+mv libxcb-* src
