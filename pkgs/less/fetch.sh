8571a50664ce8f47dec5fb79378f9b58cd4df19e
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-23 21:38:22 +0000

    update less

--- pkgs/less/fetch.sh
+++ pkgs/less/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.greenwoodsoftware.com/less/less-661.tar.gz
+curl -L -# -o src.tar.gz https://www.greenwoodsoftware.com/less/less-668.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv less-661 src
+mv less-* src
