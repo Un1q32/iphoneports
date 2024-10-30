0681f2cdfa88980bf005025b0ae6e91d27c50571
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-30 16:19:23 -0400

    update clang

--- pkgs/clang/fetch.sh
+++ pkgs/clang/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.2/llvm-project-19.1.2.src.tar.xz
+curl -L -# -o src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.3/llvm-project-19.1.3.src.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
