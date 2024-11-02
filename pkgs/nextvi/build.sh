461a7d6e2a43e7123517009f252b3c22d3113ee1
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-02 01:55:32 +0000

    update nextvi

--- pkgs/nextvi/build.sh
+++ pkgs/nextvi/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-"$_TARGET-cc" -std=c99 -O2 -flto -D_POSIX_C_SOURCE=200809L -D_DARWIN_C_SOURCE vi.c -o vi
+"$_TARGET-cc" -std=c99 -Os -flto -D_POSIX_C_SOURCE=200809L -D_DARWIN_C_SOURCE vi.c -o vi
 mkdir -p "$_PKGROOT/pkg/var/usr/bin"
 cp vi "$_PKGROOT/pkg/var/usr/bin"
 )
