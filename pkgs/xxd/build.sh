ac593f0cd6149955786f600ff5ec5dd41d6cd214
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-02 01:33:45 +0000

    make xxd use -O3 -flto

--- pkgs/xxd/build.sh
+++ pkgs/xxd/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-"$_TARGET-cc" -O2 -DUNIX -o xxd xxd.c
+"$_TARGET-cc" -O3 -flto -DUNIX -o xxd xxd.c
 mkdir -p "$_PKGROOT"/pkg/var/usr/bin
 cp xxd "$_PKGROOT"/pkg/var/usr/bin
 )
