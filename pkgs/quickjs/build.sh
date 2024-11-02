8bf313be6c79d4821c471a402623cf54c545c2c8
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-02 02:16:35 +0000

    update quickjs

--- pkgs/quickjs/build.sh
+++ pkgs/quickjs/build.sh
@@ -4,10 +4,10 @@
 cd src || exit 1
 libqjs_objs='.obj/quickjs.o .obj/libregexp.o .obj/libunicode.o .obj/cutils.o .obj/quickjs-libc.o .obj/libbf.o'
 "$_MAKE" CROSS_PREFIX="$_TARGET-" PREFIX=/var/usr CC="$_TARGET-cc -Wno-implicit-const-int-float-conversion" CONFIG_DARWIN=y CONFIG_M32=y CONFIG_LTO= .obj/repl.o .obj/qjs.o .obj/qjscalc.o $libqjs_objs -j8
-"$_TARGET-cc" -funsigned-char -MMD -MF .obj/qjsc.o.d -fwrapv -D_GNU_SOURCE -DCONFIG_VERSION=\"2024-01-13\" -DCONFIG_BIGNUM -DCONFIG_CC=\"gcc\" -DCONFIG_PREFIX=\"/var/usr\" -O2 -c -o .obj/qjsc.o qjsc.c
-"$_TARGET-cc" -shared -O2 -o libquickjs.dylib $libqjs_objs -install_name /var/usr/lib/quickjs/libquickjs.dylib -compatibility_version 1 -current_version 1
-"$_TARGET-cc" -O2 -o qjs .obj/repl.o .obj/qjs.o .obj/qjscalc.o libquickjs.dylib
-"$_TARGET-cc" -O2 -o qjsc .obj/qjsc.o libquickjs.dylib
+"$_TARGET-cc" -funsigned-char -MMD -MF .obj/qjsc.o.d -fwrapv -D_GNU_SOURCE -DCONFIG_VERSION=\"2024-01-13\" -DCONFIG_BIGNUM -DCONFIG_CC=\"gcc\" -DCONFIG_PREFIX=\"/var/usr\" -O3 -flto -c -o .obj/qjsc.o qjsc.c
+"$_TARGET-cc" -shared -O3 -flto -o libquickjs.dylib $libqjs_objs -install_name /var/usr/lib/quickjs/libquickjs.dylib -compatibility_version 1 -current_version 1
+"$_TARGET-cc" -O3 -flto -o qjs .obj/repl.o .obj/qjs.o .obj/qjscalc.o libquickjs.dylib
+"$_TARGET-cc" -O3 -flto -o qjsc .obj/qjsc.o libquickjs.dylib
 mkdir -p "$_PKGROOT/pkg/var/usr/bin" "$_PKGROOT/pkg/var/usr/lib/quickjs" "$_PKGROOT/pkg/var/usr/include/quickjs"
 cp qjs qjsc "$_PKGROOT/pkg/var/usr/bin"
 cp libquickjs.dylib "$_PKGROOT/pkg/var/usr/lib/quickjs"
