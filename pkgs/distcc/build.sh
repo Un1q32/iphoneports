ca6d1d86d80eb60f4d180409560832ce3c18c113
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-05 16:26:57 +0000

    add distcc

---
+++ pkgs/distcc/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+(
+cd src || exit 1
+./autogen.sh
+./configure --host="$_TARGET" --prefix=/var/usr --without-libiberty --disable-Werror --disable-pump-mode
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share sbin
+for bin in distcc distccd distccmon-text lsdistcc; do
+    "$_TARGET-strip" "bin/$bin" 2>/dev/null
+    ldid -S"$_ENT" "bin/$bin"
+done
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg distcc.deb
