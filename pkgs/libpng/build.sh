b384de57d879a51ebdf928e69a3e914b39adb1f8
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-11 15:18:03 -0500

    add libpng

---
+++ pkgs/libpng/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static --enable-silent-rules
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" bin/pngfix bin/png-fix-itxt lib/libpng16.16.dylib 2>/dev/null
+ldid -S"$_ENT" bin/pngfix bin/png-fix-itxt lib/libpng16.16.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libpng.deb
---
+++ pkgs/libpng/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.xz https://download.sourceforge.net/libpng/libpng-1.6.44.tar.xz
+printf "Unpacking source...\n"
+tar -xf src.tar.xz
+rm src.tar.xz
+mv libpng-* src
