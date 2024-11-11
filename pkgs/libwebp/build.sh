82f1d928bf6e6e6b185a9e0b00efaee16c87e535
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-11 17:54:28 -0500

    update imagemagick, add libwebp

---
+++ pkgs/libwebp/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+mkdir -p src/build
+(
+cd src/build || exit 1
+cmake -GNinja .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_INSTALLNAMETOOL" -DCMAKE_INSTALL_NAME_DIR=/var/usr/lib -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DBUILD_SHARED_LIBS=ON
+DESTDIR="$_PKGROOT/pkg" ninja install -j8
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+for lib in lib/*.dylib; do
+    if ! [ -h "$lib" ]; then
+        "$_TARGET-strip" "$lib" 2>/dev/null
+        ldid -S"$_ENT" "$lib"
+    fi
+done
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libwebp.deb
---
+++ pkgs/libwebp/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.gz https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.4.0.tar.gz
+printf "Unpacking source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv libwebp-* src
