b799215fbd0e12cb2d2765349579cc9992d57a9e
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-11 14:52:33 -0500

    update libzip

--- pkgs/libzip/fetch.sh
+++ pkgs/libzip/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/nih-at/libzip/releases/download/v1.10.1/libzip-1.10.1.tar.xz
+curl -L -# -o src.tar.xz https://github.com/nih-at/libzip/releases/download/v1.11.2/libzip-1.11.2.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv libzip-1.10.1 src
+mv libzip-* src
