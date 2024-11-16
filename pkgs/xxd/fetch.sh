7ec78abdc8484e83d34675ad769bb4c2b1e9052c
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-16 02:10:48 +0000

    update xxd

--- pkgs/xxd/fetch.sh
+++ pkgs/xxd/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -# -o src/xxd.c https://raw.githubusercontent.com/vim/vim/c73fc86bf8fe318aab41900dd92e380143211cda/src/xxd/xxd.c
+curl -# -o src/xxd.c https://raw.githubusercontent.com/vim/vim/4b9fa957125e33951a4a830414ccb70172976397/src/xxd/xxd.c
