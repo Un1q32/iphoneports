f3e71088d1b8618294148d742d7e32fde89fcc19
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-18 12:37:09 -0500

    update lighttpd and screen

--- pkgs/lighttpd/build.sh
+++ pkgs/lighttpd/build.sh
@@ -2,7 +2,7 @@
 (
 cd src || exit 1
 ./autogen.sh
-./configure --host="$_TARGET" --enable-silent-rules --prefix=/var/usr --with-openssl --with-zlib --with-brotli --with-zstd --with-lua --with-sqlite --with-pam --with-webdav-props PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
+./configure --host="$_TARGET" --enable-silent-rules --prefix=/var/usr --with-openssl --with-zlib --with-bzip2 --with-brotli --with-zstd --with-lua --with-sqlite --with-webdav-props --with-xxhash PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
 "$_MAKE" -j8
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg"
 )
--- pkgs/screen/build.sh
+++ pkgs/screen/build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --enable-pam --enable-colors256 --enable-rxvt_osc --enable-telnet CPPFLAGS="-Wno-implicit-function-declaration"
+./configure --host="$_TARGET" --prefix=/var/usr --enable-colors256 --enable-rxvt_osc --enable-telnet CPPFLAGS="-Wno-implicit-function-declaration"
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
-mkdir -p "$_PKGROOT/pkg/var/usr/etc" "$_PKGROOT/pkg/etc/pam.d"
 cp etc/etcscreenrc "$_PKGROOT/pkg/var/usr/etc/screenrc"
 )
 
@@ -15,8 +14,6 @@ mv bin/screen-* bin/screen
 "$_TARGET-strip" bin/screen 2>/dev/null
 ldid -S"$_ENT" bin/screen
 )
-
-cp files/screen.pam pkg/etc/pam.d/screen
 
 cp -r DEBIAN pkg
 dpkg-deb -b --root-owner-group -Zgzip pkg screen.deb
