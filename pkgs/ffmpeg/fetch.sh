5742c038980fe626c55e93c4d1cece24fdd48906
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-21 00:12:13 -0500

    update

--- pkgs/ffmpeg/fetch.sh
+++ pkgs/ffmpeg/fetch.sh
@@ -8,6 +8,6 @@ rm src.tar.xz
 mv ffmpeg-* src
 
 for src in extendhfsf2.c truncsfhf2.c fp_extend_impl.inc fp_trunc_impl.inc fp_extend.h fp_trunc.h int_lib.h int_types.h int_endianness.h int_util.h; do
-  curl -s -o "src/$src" "https://raw.githubusercontent.com/llvm/llvm-project/refs/tags/llvmorg-19.1.3/compiler-rt/lib/builtins/$src" &
+  curl -s -o "src/$src" "https://raw.githubusercontent.com/llvm/llvm-project/refs/tags/llvmorg-19.1.4/compiler-rt/lib/builtins/$src" &
 done
 wait
