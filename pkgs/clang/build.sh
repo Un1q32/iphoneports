3f3f31c76082a695e8d65ab6cf74e09013040352
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-22 15:37:46 -0500

    change dir of fake git in clang build

--- pkgs/clang/build.sh
+++ pkgs/clang/build.sh
@@ -17,10 +17,10 @@ _get_distribution_components() {
 }
 
 # awful hack to make clang not print git info in version string
-mkdir -p "$_TMP/iphoneports-fakebin"
-printf '#!/bin/sh\nexit 1\n' > "$_TMP/iphoneports-fakebin/git"
-chmod +x "$_TMP/iphoneports-fakebin/git"
-export PATH="$_TMP/iphoneports-fakebin:$PATH"
+mkdir -p "$_PKGROOT/src/iphoneports-fakebin"
+printf '#!/bin/sh\nexit 1\n' > "$_PKGROOT/src/iphoneports-fakebin/git"
+chmod +x "$_PKGROOT/src/iphoneports-fakebin/git"
+export PATH="$_PKGROOT/src/iphoneports-fakebin:$PATH"
 
 mkdir -p src/build
 (
@@ -29,8 +29,6 @@ cmake -GNinja ../clang -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-c
 cmake -GNinja ../clang -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_INSTALLNAMETOOL" -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DCLANG_LINK_CLANG_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_INCLUDE_TESTS=OFF -DLLVM_TABLEGEN_EXE="$(command -v llvm-tblgen)" -DLLVM_DISTRIBUTION_COMPONENTS="$(_get_distribution_components | paste -sd ';')"
 DESTDIR="$_PKGROOT/pkg" ninja install-distribution -j8
 )
-
-rm -rf "$_TMP/iphoneports-fakebin"
 
 (
 cd pkg/var/usr || exit 1
