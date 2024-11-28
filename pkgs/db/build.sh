fb7182b5d37f9dcf0a80afa9775a12c80dbe32b2
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-28 16:23:11 -0500

    add berkeley db

---
+++ pkgs/db/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+(
+cd src/build_unix || exit 1
+../dist/configure --host="$_TARGET" --prefix=/var/usr --enable-cxx --enable-compat185 --disable-static --with-mutex=Darwin/_spin_lock_try CPPFLAGS='-Wno-deprecated-non-prototype'
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+"$_TARGET-strip" bin/* lib/libdb-6.2.dylib lib/libdb_cxx-6.2.dylib 2>/dev/null
+ldid -S"$_ENT" bin/* lib/libdb-6.2.dylib lib/libdb_cxx-6.2.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg db.deb
