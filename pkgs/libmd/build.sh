4190a32b942eadba14bae929bd3eec5f2fb4f8a2
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-08 18:04:13 +0000

    add libmd

---
+++ pkgs/libmd/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" -j8
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" lib/libmd.0.dylib 2>/dev/null
+ldid -S"$_ENT" lib/libmd.0.dylib
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg libmd.deb
