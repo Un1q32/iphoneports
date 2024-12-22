e995eadc2378f4f9f39e41a36ffd384f479e00cc
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-22 12:29:44 -0500

    update libwebp

--- pkgs/libwebp/fetch.sh
+++ pkgs/libwebp/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.4.0.tar.gz
+curl -L -# -o src.tar.gz https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.5.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
