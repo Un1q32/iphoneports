c5bd3b160b31007b4216ee4fe07b0d468a6236ae
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-22 02:44:43 -0500

    fix python

--- pkgs/python/build.sh
+++ pkgs/python/build.sh
@@ -9,8 +9,11 @@ export PATH="$_PKGROOT/src/iphoneports-fakebin:$PATH"
 cd src || exit 1
 autoreconf -fi
 
-./configure --prefix="$_PKGROOT/src/buildpython" --without-ensurepip
+(
+mkdir _buildpython && cd _buildpython || exit 1
+../configure --prefix="$_PKGROOT/src/buildpython" --without-ensurepip
 "$_MAKE" install -j8
+)
 
 "$_TARGET-cc" emutls.c atomic.c -O3 -flto -c
 ./configure --host="$_TARGET" --prefix=/var/usr --build="$(cc -dumpmachine)" --with-build-python="$_PKGROOT/src/buildpython/bin/python3" --without-mimalloc --enable-shared --without-ensurepip --with-libs="$_PKGROOT/src/emutls.o $_PKGROOT/src/atomic.o" LDSHARED="$_TARGET-cc -shared -undefined dynamic_lookup" MACHDEP=darwin ac_sys_system=Darwin ac_cv_buggy_getaddrinfo=no ac_cv_file__dev_ptmx=yes ac_cv_file__dev_ptc=no PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
