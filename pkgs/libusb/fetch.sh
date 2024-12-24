68b654c059ee534fe818a53e43fc4030048e2382
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-23 19:08:35 -0500

    bump

--- pkgs/libusb/fetch.sh
+++ pkgs/libusb/fetch.sh
@@ -8,6 +8,6 @@ rm src.tar.bz2
 mv libusb-* src
 
 for src in emutls.c int_lib.h int_types.h int_endianness.h int_util.h; do
-  curl -s -o "src/$src" "https://raw.githubusercontent.com/llvm/llvm-project/refs/tags/llvmorg-19.1.4/compiler-rt/lib/builtins/$src" &
+  curl -s -o "src/$src" "https://raw.githubusercontent.com/llvm/llvm-project/refs/tags/llvmorg-19.1.6/compiler-rt/lib/builtins/$src" &
 done
 wait
