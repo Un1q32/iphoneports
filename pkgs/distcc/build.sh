750d00c7c19ae8afa9bad0518282440c7d92f745
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-26 14:12:21 -0500

    update distcc

--- pkgs/distcc/build.sh
+++ pkgs/distcc/build.sh
@@ -2,7 +2,7 @@
 (
 cd src || exit 1
 ./autogen.sh
-./configure --host="$_TARGET" --prefix=/var/usr --without-libiberty --without-avahi --disable-Werror --disable-pump-mode
+./configure --host="$_TARGET" --prefix=/var/usr --without-libiberty --without-avahi --disable-Werror --enable-rfc2553 --disable-pump-mode
 "$_MAKE" -j8
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
