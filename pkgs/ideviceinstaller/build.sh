6c67f674a4b9d48d4a5079fabc8e3966e671fc9b
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-27 18:23:13 -0500

    fix

--- pkgs/ideviceinstaller/build.sh
+++ pkgs/ideviceinstaller/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-./autogen.sh
+autoreconf -fi
 ./configure --host="$_TARGET" --prefix=/var/usr PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig" ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
 "$_MAKE" -j8
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg"
