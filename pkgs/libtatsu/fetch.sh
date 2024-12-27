41bbeeed4d081995ad47792d904321de4e6d7b2f
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-27 18:07:30 -0500

    update libtatsu

--- pkgs/libtatsu/fetch.sh
+++ pkgs/libtatsu/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://github.com/libimobiledevice/libtatsu/releases/download/1.0.3/libtatsu-1.0.3.tar.bz2
+curl -L -# -o src.tar.bz2 https://github.com/libimobiledevice/libtatsu/releases/download/1.0.4/libtatsu-1.0.4.tar.bz2
 printf "Unpacking source...\n"
 tar -xf src.tar.bz2
 rm src.tar.bz2
