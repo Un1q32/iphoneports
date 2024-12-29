e4a2d690f22f8677e6dc42658160e1963dab6d23
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-29 00:49:03 -0500

    update header

--- pkgs/cctools/sdk/var/usr/include/string.h
+++ pkgs/cctools/sdk/var/usr/include/string.h
@@ -1,11 +1,9 @@
-#ifndef _FAKE_STRING_H_
-#define _FAKE_STRING_H_
+#pragma once
+
+#include_next <string.h>
 
 #include <stdlib.h>
-#include_next <string.h>
-#include <sys/cdefs.h>
 
-__BEGIN_DECLS
 static inline size_t strnlen(const char *str, size_t maxlen) {
   const char *end = (const char *)memchr(str, '\0', maxlen);
   return end ? (size_t)(end - str) : maxlen;
@@ -20,6 +18,3 @@ static inline char *strndup(const char *str, size_t maxlen) {
   newstr[len] = '\0';
   return newstr;
 }
-__END_DECLS
-
-#endif
