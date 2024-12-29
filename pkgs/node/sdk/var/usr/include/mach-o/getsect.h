1b61e9fcf11895c7ea5b8a65717c572aa13df965
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-29 02:28:30 -0500

    non-working node build
    this might be impossible, but its close enough to being possible that i
    want to archive it to possibly work on later

---
+++ pkgs/node/sdk/var/usr/include/mach-o/getsect.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include_next <mach-o/getsect.h>
+
+#include <string.h>
+
+#ifndef __LP64__
+
+static inline uint8_t *getsectiondata(const struct mach_header *mhp,
+                                      const char *segname, const char *sectname,
+                                      unsigned long *size) {
+  struct segment_command *sgp;
+  struct section *sp;
+  uint32_t i, j;
+  intptr_t slide;
+
+  slide = 0;
+  sp = 0;
+  sgp = (struct segment_command *)((char *)mhp + sizeof(struct mach_header));
+  for (i = 0; i < mhp->ncmds; i++) {
+    if (sgp->cmd == LC_SEGMENT) {
+      if (strcmp(sgp->segname, "__TEXT") == 0) {
+        slide = (uintptr_t)mhp - sgp->vmaddr;
+      }
+      if (strncmp(sgp->segname, segname, sizeof(sgp->segname)) == 0) {
+        sp = (struct section *)((char *)sgp + sizeof(struct segment_command));
+        for (j = 0; j < sgp->nsects; j++) {
+          if (strncmp(sp->sectname, sectname, sizeof(sp->sectname)) == 0 &&
+              strncmp(sp->segname, segname, sizeof(sp->segname)) == 0) {
+            *size = sp->size;
+            return ((uint8_t *)(sp->addr) + slide);
+          }
+          sp = (struct section *)((char *)sp + sizeof(struct section));
+        }
+      }
+    }
+    sgp = (struct segment_command *)((char *)sgp + sgp->cmdsize);
+  }
+  return 0;
+}
+
+#else
+
+static inline uint8_t *getsectiondata(const struct mach_header_64 *mhp,
+                                      const char *segname, const char *sectname,
+                                      unsigned long *size) {
+  struct segment_command_64 *sgp;
+  struct section_64 *sp;
+  uint32_t i, j;
+  intptr_t slide;
+
+  slide = 0;
+  sp = 0;
+  sgp = (struct segment_command_64 *)((char *)mhp +
+                                      sizeof(struct mach_header_64));
+  for (i = 0; i < mhp->ncmds; i++) {
+    if (sgp->cmd == LC_SEGMENT_64) {
+      if (strcmp(sgp->segname, "__TEXT") == 0) {
+        slide = (uintptr_t)mhp - sgp->vmaddr;
+      }
+      if (strncmp(sgp->segname, segname, sizeof(sgp->segname)) == 0) {
+        sp = (struct section_64 *)((char *)sgp +
+                                   sizeof(struct segment_command_64));
+        for (j = 0; j < sgp->nsects; j++) {
+          if (strncmp(sp->sectname, sectname, sizeof(sp->sectname)) == 0 &&
+              strncmp(sp->segname, segname, sizeof(sp->segname)) == 0) {
+            *size = sp->size;
+            return ((uint8_t *)(sp->addr) + slide);
+          }
+          sp = (struct section_64 *)((char *)sp + sizeof(struct section_64));
+        }
+      }
+    }
+    sgp = (struct segment_command_64 *)((char *)sgp + sgp->cmdsize);
+  }
+  return 0;
+}
+
+#endif
---
+++ pkgs/node/sdk/var/usr/include/stdlib.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include_next <stdlib.h>
+
+__BEGIN_DECLS
+extern void arc4random_buf(void *, size_t);
+__END_DECLS
