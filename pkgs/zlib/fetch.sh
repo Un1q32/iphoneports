106392d6a2a3670203a4f201ef90ad913b744afc
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-31 20:19:30 -0500

    update zlib

--- pkgs/zlib/fetch.sh
+++ pkgs/zlib/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.2.tar.gz
+curl -L -# -o src.tar.gz https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.3.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
