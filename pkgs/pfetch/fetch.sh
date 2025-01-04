87c33a472a393ac3908067c468341dbde830c884
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-04 12:07:28 -0500

    update pfetch

--- pkgs/pfetch/fetch.sh
+++ pkgs/pfetch/fetch.sh
@@ -2,5 +2,5 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir -p src
-curl -L -# -o src/pfetch https://raw.githubusercontent.com/Un1q32/pfetch/1.7.0/pfetch
+curl -L -# -o src/pfetch https://raw.githubusercontent.com/Un1q32/pfetch/1.7.1/pfetch
 chmod 755 src/pfetch
