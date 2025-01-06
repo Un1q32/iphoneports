093a720377c0075bb36f3c101708b08857984357
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-06 13:06:43 -0500

    update nano

--- pkgs/nano/fetch.sh
+++ pkgs/nano/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src files/syntax
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/nano/nano-8.1.tar.gz
+curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/nano/nano-8.3.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv nano-8.1 src
+mv nano-* src
