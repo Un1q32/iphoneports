597f18c550c7618ad3a783f5a9496842794486b2
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-07 13:26:08 -0500

    update xxhash

--- pkgs/xxhash/fetch.sh
+++ pkgs/xxhash/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/Cyan4973/xxHash/archive/refs/tags/v0.8.2.tar.gz
+curl -L -# -o src.tar.gz https://github.com/Cyan4973/xxHash/archive/refs/tags/v0.8.3.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
