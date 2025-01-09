c30019db2c074909c15a5526cc9b156025878004
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-08 22:24:33 -0500

    remove neon optimizations from zlib til crash is fixed

--- pkgs/zlib/build.sh
+++ pkgs/zlib/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
 cd src || exit 1
-CHOST="$_TARGET" ./configure --prefix=/var/usr --zlib-compat
+CHOST="$_TARGET" ./configure --prefix=/var/usr --zlib-compat --without-neon
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
