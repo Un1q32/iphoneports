90b0c1ad96d51d6fbb7d554b8f76e0574496dd36
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-14 11:17:12 -0500

    update compiler-rt

--- pkgs/compiler-rt/fetch.sh
+++ pkgs/compiler-rt/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.6/compiler-rt-19.1.6.src.tar.xz
+curl -L -# -o src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.7/compiler-rt-19.1.7.src.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
