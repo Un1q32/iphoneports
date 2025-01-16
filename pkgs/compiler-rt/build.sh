d9def45b67fd21527b78ff7b079d0417ac03025f
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-16 00:42:19 -0500

    incomplete ios 2 port
    putting this here so i can revert the revert commit if i ever wanna try
    again with this

--- pkgs/compiler-rt/build.sh
+++ pkgs/compiler-rt/build.sh
@@ -8,7 +8,7 @@ clang="${clang%/*}/../share/iphoneports/bin/clang"
 arm64srcs="emutls.c"
 armv7ssrcs="$arm64srcs atomic.c extendhfsf2.c truncsfhf2.c"
 armv7srcs="$armv7ssrcs"
-armv6srcs="$armv7srcs"
+armv6srcs="$armv7srcs floatundidf.c floatundisf.c"
 
 for src in $armv6srcs; do
     while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
