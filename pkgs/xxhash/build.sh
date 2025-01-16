2cd2c736ce7a8b768218f917a81b162fb7f51e9d
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-15 23:57:44 -0500

    fix xxhash

--- pkgs/xxhash/build.sh
+++ pkgs/xxhash/build.sh
@@ -7,8 +7,8 @@ cd src || exit 1
 (
 cd pkg/var/usr || exit 1
 rm -rf share lib/libxxhash.a
-"$_TARGET-strip" bin/xxhsum lib/libxxhash.0.8.2.dylib 2>/dev/null
-ldid -S"$_ENT" bin/xxhsum lib/libxxhash.0.8.2.dylib
+"$_TARGET-strip" bin/xxhsum lib/libxxhash.0.8.*.dylib 2>/dev/null
+ldid -S"$_ENT" bin/xxhsum lib/libxxhash.0.8.*.dylib
 )
 
 cp -r DEBIAN pkg
