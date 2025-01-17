f14eada244eee4e947d628edf84559c6fbeda272
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-17 12:54:40 -0500

    update rsync

--- pkgs/rsync/fetch.sh
+++ pkgs/rsync/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://download.samba.org/pub/rsync/src/rsync-3.3.0.tar.gz
+curl -L -# -o src.tar.gz https://download.samba.org/pub/rsync/src/rsync-3.4.1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
