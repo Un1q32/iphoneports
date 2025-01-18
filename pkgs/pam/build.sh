bbbf6c88cd5e026ee9ebcdf3cba6db8eeda4e372
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-17 19:16:54 -0500

    remove pam and su

--- pkgs/pam/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp files/*.h pkg/usr/include/pam
-ln -s pam pkg/usr/include/security
-rm -rf pkg/DEBIAN pkg/etc
--- pkgs/pam/fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.deb http://apt.saurik.com/cydia/debs/pam_32.1-4_iphoneos-arm.deb
-printf "Unpacking source...\n"
-dpkg-deb -R src.deb pkg
-rm src.deb
