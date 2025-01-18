4bbfbde42421c08bbee6a7ac32918db9a3b1070e
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-18 13:09:46 -0500

    remove x11 shit

--- pkgs/libxau/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://xorg.freedesktop.org/releases/individual/lib/libXau-1.0.11.tar.xz
-printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
-mv libXau-1.0.11 src
--- pkgs/xorg-util-macros/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://xorg.freedesktop.org/releases/individual/util/util-macros-1.20.1.tar.xz
-printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
-mv util-macros-* src
