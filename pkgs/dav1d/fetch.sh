0079d0f14a8ee811821703a1e4d5907b74d20f5d
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-20 13:09:12 -0500

    update dav1d

--- pkgs/dav1d/fetch.sh
+++ pkgs/dav1d/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://code.videolan.org/videolan/dav1d/-/archive/1.5.0/dav1d-1.5.0.tar.bz2
+curl -L -# -o src.tar.bz2 https://code.videolan.org/videolan/dav1d/-/archive/1.5.1/dav1d-1.5.1.tar.bz2
 printf "Unpacking source...\n"
 tar -xf src.tar.bz2
 rm src.tar.bz2
