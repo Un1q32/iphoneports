5b62ef0bf71cae8ae069d7fa73b8aebd3ee6cf70
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-22 18:14:34 -0500

    fix screen

--- pkgs/screen/build.sh
+++ pkgs/screen/build.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/sh -e
 (
 cd src || exit 1
 ./configure --host="$_TARGET" --prefix=/var/usr --enable-colors256 --enable-rxvt_osc --enable-telnet CPPFLAGS="-Wno-implicit-function-declaration"
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+mkdir -p "$_PKGROOT/pkg/var/usr/etc"
 cp etc/etcscreenrc "$_PKGROOT/pkg/var/usr/etc/screenrc"
 )
 
 (
 cd pkg/var/usr || exit 1
 rm -rf share/info share/man
-mv bin/screen-* bin/screen
-"$_TARGET-strip" bin/screen 2>/dev/null
-ldid -S"$_ENT" bin/screen
+"$_TARGET-strip" bin/screen-* 2>/dev/null || true
+ldid -S"$_ENT" bin/screen-*
 )
 
 cp -r DEBIAN pkg
