4d040eaec6e9ec126f97250471af8a034e2adf5f
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-22 19:30:14 -0500

    fix wget2

--- pkgs/wget2/build.sh
+++ pkgs/wget2/build.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 (
 cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --with-ssl=openssl --with-openssl --disable-static CPPFLAGS='-Wno-unknown-attributes' PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
+autoreconf -f
+./configure --host="$_TARGET" --prefix=/var/usr --with-ssl=openssl --with-openssl --with-lzma --with-bzip2 --without-gpgme --disable-static CPPFLAGS='-Wno-unknown-attributes' PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
@@ -9,7 +10,7 @@ cd src || exit 1
 (
 cd pkg/var/usr || exit 1
 rm -rf share bin/wget2_noinstall
-"$_TARGET-strip" bin/wget2 lib/libwget.3.dylib 2>/dev/null
+"$_TARGET-strip" bin/wget2 lib/libwget.3.dylib 2>/dev/null || true
 ldid -S"$_ENT" bin/wget2 lib/libwget.3.dylib
 )
 
