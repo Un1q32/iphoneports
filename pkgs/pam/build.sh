7daec8792598157e8fb25b1d484444b9609bba17
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-22 22:32:29 -0500

    add pam and su

---
+++ pkgs/pam/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+(
+cd src/pam || exit 1
+autoconf
+ln -s . pam
+./configure --prefix=/var/usr --enable-read-both-confs --enable-giant-libpam --enable-fakeroot="$_PKGROOT/pkg" --enable-sconfigdir=/var/usr/etc/pam --disable-libcrack ac_cv_header_features_h=no
+"$_MAKE" CC="$_TARGET-cc" AR="$_TARGET-ar" RANLIB="$_TARGET-ranlib" LD="$_TARGET-ld"
+"$_MAKE" install
+)
+
+(
+cd pkg || exit 1
+mv usr/include var/usr
+rm -rf usr
+cd var/usr/lib || exit 1
+mv libpam.1.0.dylib libpam.1.dylib
+"$_TARGET-strip" libpam.1.dylib security/*.so 2>/dev/null || true
+ldid -S"$_ENT" libpam.1.dylib security/*.so
+)
+
+(
+cd src/modules || exit 1
+for module in launchd unix uwtmp; do
+    "$_TARGET-cc" -bundle -o "pam_${module}.so" "pam_${module}"/*.c -I"$_PKGROOT/pkg/var/usr/include" "$_PKGROOT/pkg/var/usr/lib/libpam.dylib" -w
+done
+"$_TARGET-strip" ./*.so 2>/dev/null || true
+ldid -S"$_ENT" ./*.so
+cp ./*.so "$_PKGROOT/pkg/var/usr/lib/security"
+)
+
+cp -r DEBIAN pkg
+dpkg-deb -b --root-owner-group -Zgzip pkg pam.deb
---
+++ pkgs/pam/fetch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.gz https://github.com/apple-oss-distributions/pam/archive/refs/tags/pam-32.1.tar.gz
+printf "Unpacking source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv pam-pam-* src
+cd src || exit 1
+printf "Downloading modules source...\n"
+curl -L -# -o src.tar.gz https://github.com/apple-oss-distributions/pam_modules/archive/refs/tags/pam_modules-36.2.tar.gz
+printf "Unpacking modules source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv pam_modules-pam_modules-* modules
