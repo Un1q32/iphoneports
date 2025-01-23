b0fc6caee27fcf75f543cf4c93379a2b204b074d
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-22 22:40:07 -0500

    fix

--- pkgs/doas/build.sh
+++ pkgs/doas/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 (
 cd src || exit 1
 yacc parse.y
@@ -12,7 +12,7 @@ cp doasedit "$_PKGROOT/pkg/var/usr/bin"
 
 (
 cd pkg/var/usr/bin || exit 1
-"$_TARGET-strip" doas 2>/dev/null
+"$_TARGET-strip" doas 2>/dev/null || true
 ldid -S"$_ENT" doas
 chmod 4755 doas
 )
