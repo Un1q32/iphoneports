b9235c3a81b8f29f5d14ea5be4a4b0ab63decbf2
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-27 18:30:53 -0500

    darwin9 fixes again

--- pkgs/getusedmem/files/getusedmem.c
+++ pkgs/getusedmem/files/getusedmem.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 int main(void) {
-  vm_statistics64_data_t vm_stat;
+  vm_statistics_data_t vm_stat;
   mach_msg_type_number_t count = sizeof(vm_stat) / sizeof(natural_t);
-  host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stat,
+  host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)&vm_stat,
                     &count);
   printf("%llu\n", (vm_stat.active_count + vm_stat.inactive_count) * 4ULL);
   return 0;
