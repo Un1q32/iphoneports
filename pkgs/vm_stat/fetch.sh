683f75dade512a7c8b348e79c1a700a2f183ae0c
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-27 17:43:48 -0500

    Revert "update darwin9 patches again"
    This reverts commit 661438deb1c23a9ee71395d9e3e4f968f0c0e5fa.

--- pkgs/vm_stat/fetch.sh
+++ pkgs/vm_stat/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/vm_stat.c https://raw.githubusercontent.com/apple-oss-distributions/system_cmds/system_cmds-431/vm_stat.tproj/vm_stat.c
+curl -L -# -o src/vm_stat.c https://raw.githubusercontent.com/apple-oss-distributions/system_cmds/system_cmds-498.0.10/vm_stat.tproj/vm_stat.c
