be1922b092b795ff98f0d897b8da4bec8f7ad7cc
Un1q32 <joey.t.reinhart@gmail.com>
2025-01-31 01:01:41 -0500

    Revert "update node build (still doesnt work)"
    This reverts commit 95221d606bfea565edd3412a4c281b90255953e0.

--- pkgs/node/sdk/var/usr/include/stdlib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include_next <stdlib.h>
-
-__BEGIN_DECLS
-extern void arc4random_buf(void *, size_t);
-__END_DECLS
--- pkgs/openssl/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.4.0/openssl-3.4.0.tar.gz
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv openssl-* src
