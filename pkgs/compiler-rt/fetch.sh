5ce5b6e1fe236a3dfc1f6281c2eef7b4961188c0
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-02 15:27:47 -0500

    Revert "llvm 20 rc1"
    This reverts commit edd5d570851b88f3fec5427e70e5a357e298c5ab.

--- pkgs/compiler-rt/fetch.sh
+++ pkgs/compiler-rt/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-20.1.0-rc1/compiler-rt-20.1.0-rc1.src.tar.xz
+curl -L -# -o src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.7/compiler-rt-19.1.7.src.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
