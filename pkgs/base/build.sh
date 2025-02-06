082d56bd4165c2af234f4c0f88eda7ddd6a66086
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-06 03:45:25 -0500

    dont include firmware dependency on platforms without cydia

--- pkgs/base/build.sh
+++ pkgs/base/build.sh
@@ -23,4 +23,5 @@ done
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+[ "$_DPKGARCH" = "iphoneos-arm" ] && sed -i '/^Depends:/ s/$/, firmware (>= 3.0)/' pkg/DEBIAN/control
 dpkg-deb -b --root-owner-group -Zgzip pkg "base-$_DPKGARCH.deb"
