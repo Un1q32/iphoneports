f56a7e646f7c89decc665ad19121b245b4008e81
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-06 22:28:39 -0500

    libunwind i386

--- pkgs/libc++/build.sh
+++ pkgs/libc++/build.sh
@@ -3,15 +3,15 @@ mkdir -p src/build
 (
 cd src/build || exit 1
 cpu="${_TARGET%%-*}"
-cmake -GNinja ../runtimes -DLLVM_ENABLE_RUNTIMES='libcxx;libcxxabi' -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_INSTALLNAMETOOL" -DCMAKE_INSTALL_NAME_DIR=/var/usr/lib -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DLLVM_ENABLE_LTO=ON -DLIBCXX_INCLUDE_BENCHMARKS=OFF -DLIBCXXABI_USE_LLVM_UNWINDER=OFF -DLIBCXX_ENABLE_STATIC=OFF -DLIBCXXABI_ENABLE_STATIC=OFF -DCMAKE_OSX_ARCHITECTURES="$cpu"
-DESTDIR="$_PKGROOT/pkg" ninja install-cxx install-cxxabi -j"$_JOBS"
+cmake -GNinja ../runtimes -DLLVM_ENABLE_RUNTIMES='libcxx;libcxxabi;libunwind' -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_INSTALLNAMETOOL" -DCMAKE_INSTALL_NAME_DIR=/var/usr/lib -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DLLVM_ENABLE_LTO=ON -DLIBCXX_INCLUDE_BENCHMARKS=OFF -DLIBCXX_ENABLE_STATIC=OFF -DLIBCXXABI_ENABLE_STATIC=OFF -DLIBUNWIND_ENABLE_STATIC=OFF -DCMAKE_OSX_ARCHITECTURES="$cpu"
+DESTDIR="$_PKGROOT/pkg" ninja install-cxx install-cxxabi install-unwind -j"$_JOBS"
 )
 
 (
 cd pkg/var/usr/lib || exit 1
 rm -rf libc++experimental.a ../include/c++/v1/experimental
-"$_TARGET-strip" libc++.1.0.dylib libc++abi.1.0.dylib 2>/dev/null || true
-ldid -S"$_ENT" libc++.1.0.dylib libc++abi.1.0.dylib
+"$_TARGET-strip" libc++.1.0.dylib libc++abi.1.0.dylib libunwind.1.0.dylib 2>/dev/null || true
+ldid -S"$_ENT" libc++.1.0.dylib libc++abi.1.0.dylib libunwind.1.0.dylib
 )
 
 cp -r DEBIAN pkg
