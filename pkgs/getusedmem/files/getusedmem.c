70a9c22fe48ecac8f35f51872f2c8d50fa04b7ce
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-09 04:36:38 -0500

    getusedmem use 64 bit apis when availible

--- pkgs/getusedmem/files/getusedmem.c
+++ pkgs/getusedmem/files/getusedmem.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 
 int main(void) {
+#ifdef HOST_VM_INFO64
+  vm_statistics64_data_t vm_stat;
+  mach_msg_type_number_t count = sizeof(vm_stat) / sizeof(natural_t);
+  host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stat,
+#else
   vm_statistics_data_t vm_stat;
   mach_msg_type_number_t count = sizeof(vm_stat) / sizeof(natural_t);
   host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)&vm_stat,
-                  &count);
+#endif
+                    &count);
   printf("%llu\n", (vm_stat.active_count + vm_stat.inactive_count) * 4ULL);
   return 0;
 }
