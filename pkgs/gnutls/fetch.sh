58ab66d9ec20bef5b7fc259787398a02b1d535b4
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-10 22:45:39 -0500

    update gnutls

--- pkgs/gnutls/fetch.sh
+++ pkgs/gnutls/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.8.tar.xz
+curl -L -# -o src.tar.xz https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.9.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
