f83cd2d4ae612a02a889823ee605e33fd11beab4
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-11 00:51:35 -0500

    add libsodium and libpcap

---
+++ pkgs/libpcap/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+mkdir -p src/build
+(
+cd src/build || exit 1
+cpu="${_TARGET%%-*}"
+cmake -GNinja .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_INSTALLNAMETOOL" -DCMAKE_INSTALL_NAME_DIR=/var/usr/lib -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DCMAKE_OSX_ARCHITECTURES="$cpu"
+DESTDIR="$_PKGROOT/pkg" ninja install -j"$_JOBS"
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share lib/libpcap.a
+"$_TARGET-strip" "$(realpath lib/libpcap.dylib)" 2>/dev/null || true
+ldid -S"$_ENT" "$(realpath lib/libpcap.dylib)"
+)
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "libpcap-$_DPKGARCH.deb"
---
+++ pkgs/libsodium/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+(
+cd src || exit 1
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" -j"$_JOBS"
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr || exit 1
+rm -rf share
+"$_TARGET-strip" lib/libsodium.26.dylib 2>/dev/null || true
+ldid -S"$_ENT" lib/libsodium.26.dylib
+)
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "libsodium-$_DPKGARCH.deb"
