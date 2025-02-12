4d64262686114b233523d586bd08544ee13cb9a1
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 15:41:51 -0500

    llvm 20

--- pkgs/compiler-rt/build.sh
+++ pkgs/compiler-rt/build.sh
@@ -36,6 +36,7 @@ for src in $arm64srcs; do
     done
     "$clang" -isysroot "$_PKGROOT/iossysroot" -target arm64-apple-ios7 "../lib/builtins/$src" -c -O3 -o "arm64-${src%\.c}.o" &
 done
+"$clang" -isysroot "$_PKGROOT/iossysroot" -target arm64e-apple-ios12 -xc /dev/null -c -O3 -o nothing.o &
 wait
 
 "$_TARGET-libtool" -static -o libclang_rt.ios.a ./*.o 2>/dev/null
@@ -53,7 +54,7 @@ for src in $x64srcs; do
     done
     "$clang" -isysroot "$_PKGROOT/macsysroot" -target x86_64-apple-macos10.4 "../lib/builtins/$src" -c -O3 -o "x86_64-${src%\.c}.o" &
 done
-"$clang" -isysroot "$_PKGROOT/macsysroot" -target arm64-apple-macos11.0 -xc /dev/null -c -O3 -o nothing.o &
+"$clang" -isysroot "$_PKGROOT/macsysroot" -target unknown-apple-macos11.0 -arch arm64 -arch arm64e -xc /dev/null -c -O3 -o nothing.o &
 wait
 
 "$_TARGET-libtool" -static -o libclang_rt.osx.a ./*.o 2>/dev/null
