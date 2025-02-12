4e24c0aae2d84f00234c3b8a36417f963ae47b4f
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 18:43:43 -0500

    fix libplist build on newer macos

--- pkgs/libplist/build.sh
+++ pkgs/libplist/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 (
 cd src || exit 1
+[ -d "$_SDK/usr/include/c++/4.2.1" ] && rm "$_SDK/var/usr/lib/libc++.dylib"
 ./configure --host="$_TARGET" --prefix=/var/usr --without-cython --disable-static CC="$_TARGET-cc"
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg"
@@ -15,4 +16,5 @@ ldid -S"$_ENT" bin/plistutil lib/libplist-2.0.4.dylib lib/libplist++-2.0.4.dylib
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+[ -d "$_SDK/usr/include/c++/4.2.1" ] || sed -i -e '/^Depends:/ s/$/, iphoneports-libc++/' pkg/DEBIAN/control
 dpkg-deb -b --root-owner-group -Zgzip pkg "libplist-$_DPKGARCH.deb"
