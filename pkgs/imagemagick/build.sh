1f1a7ba57299b197cd1f5bbc831ab7cc87f64136
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 18:54:15 -0500

    fix imagemagick on newer macos

--- pkgs/imagemagick/build.sh
+++ pkgs/imagemagick/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 (
-cd src || exit 1
+cd src
+[ -d "$_SDK/usr/include/c++/4.2.1" ] && rm "$_SDK/var/usr/lib/libc++.dylib"
 ./configure --host="$_TARGET" --prefix=/var/usr --disable-static PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
 
 (
-cd pkg/var/usr || exit 1
+cd pkg/var/usr
 rm -rf share/doc share/man
 "$_TARGET-strip" bin/magick lib/libMagick++-7.Q16HDRI.5.dylib lib/libMagickCore-7.Q16HDRI.10.dylib lib/libMagickWand-7.Q16HDRI.10.dylib 2>/dev/null || true
 ldid -S"$_ENT" bin/magick lib/libMagick++-7.Q16HDRI.5.dylib lib/libMagickCore-7.Q16HDRI.10.dylib lib/libMagickWand-7.Q16HDRI.10.dylib
@@ -15,4 +16,5 @@ ldid -S"$_ENT" bin/magick lib/libMagick++-7.Q16HDRI.5.dylib lib/libMagickCore-7.
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+[ -d "$_SDK/usr/include/c++/4.2.1" ] || sed -i -e '/^Depends:/ s/$/, iphoneports-libc++/' pkg/DEBIAN/control
 dpkg-deb -b --root-owner-group -Zgzip pkg "imagemagick-$_DPKGARCH.deb"
