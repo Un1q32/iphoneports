286464804e25565e4534915f43b6dc590382d6c6
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 02:27:28 -0500

    fix libusb build for newer SDKs

--- pkgs/libusb/build.sh
+++ pkgs/libusb/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 (
-cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --disable-static CPPFLAGS='-DkUSBDeviceSpeedSuper=3'
+cd src
+grep -q kUSBDeviceSpeedSuper "$_SDK/System/Library/Frameworks/IOKit.framework/Headers/usb/USB.h" || superspeeddef='CPPFLAGS=-DkUSBDeviceSpeedSuper=3'
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static "$superspeeddef"
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg"
 )
 
 (
-cd pkg/var/usr/lib || exit 1
+cd pkg/var/usr/lib
 "$_TARGET-strip" libusb-1.0.0.dylib 2>/dev/null || true
 ldid -S"$_ENT" libusb-1.0.0.dylib
 )
