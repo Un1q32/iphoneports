502f592bc42b92e81755671528d0f2697d0f30fb
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 20:00:47 -0500

    fix lzo on arm64 macos

--- pkgs/lzo/build.sh
+++ pkgs/lzo/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 (
 cd src || exit 1
+autoreconf -fi
 ./configure --host="$_TARGET" --prefix=/var/usr --disable-static --enable-shared
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
