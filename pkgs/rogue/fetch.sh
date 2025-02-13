46623d83d25412784b95dc725e509712e7fadd06
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 20:47:48 -0500

    fix rogue arm64 macos

--- pkgs/rogue/fetch.sh
+++ pkgs/rogue/fetch.sh
@@ -6,3 +6,6 @@ printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
 mv rogue-5.4.4 src
+curl -L -s -o src/config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=00b15927496058d23e6258a28d8996f87cf1f191' &
+curl -L -s -o src/config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=00b15927496058d23e6258a28d8996f87cf1f191'
+wait
