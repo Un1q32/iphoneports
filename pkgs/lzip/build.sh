14767d7d4807a52f435eaa76a8b72b1ea8bf6106
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 19:57:37 -0500

    fix lzip on newer macos

--- pkgs/lzip/build.sh
+++ pkgs/lzip/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -e
 (
-cd src || exit 1
+cd src
+[ -d "$_SDK/usr/include/c++/4.2.1" ] && rm "$_SDK/var/usr/lib/libc++.dylib"
 ./configure --prefix=/var/usr CXX="$_TARGET-c++"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install -j"$_JOBS"
 )
 
 (
-cd pkg/var/usr || exit 1
+cd pkg/var/usr
 rm -rf share
 "$_TARGET-strip" bin/lzip 2>/dev/null || true
 ldid -S"$_ENT" bin/lzip
@@ -14,4 +15,5 @@ ldid -S"$_ENT" bin/lzip
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+[ -d "$_SDK/usr/include/c++/4.2.1" ] || sed -i -e '/^Depends:/ s/$/, iphoneports-libc++/' pkg/DEBIAN/control
 dpkg-deb -b --root-owner-group -Zgzip pkg "lzip-$_DPKGARCH.deb"
