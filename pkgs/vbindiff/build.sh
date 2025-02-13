a06a4e056bd4413fde06c79047f4d5183de6694d
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-12 22:39:33 -0500

    fix vbindiff on newer macos

--- pkgs/vbindiff/build.sh
+++ pkgs/vbindiff/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
 (
 cd src || exit 1
-autoreconf -i
+autoreconf -fi
+[ -d "$_SDK/usr/include/c++/4.2.1" ] && rm "$_SDK/var/usr/lib/libc++.dylib"
 ./configure --host="$_TARGET" --prefix=/var/usr
 "$_TARGET-c++" -O3 -flto -o vbindiff vbindiff.cpp curses/ConWin.cpp GetOpt/GetOpt.cpp -lncurses -lpanel -Icurses
 mkdir -p "$_PKGROOT/pkg/var/usr/bin"
@@ -16,4 +17,5 @@ ldid -S"$_ENT" vbindiff
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+[ -d "$_SDK/usr/include/c++/4.2.1" ] || sed -i -e '/^Depends:/ s/$/, iphoneports-libc++/' pkg/DEBIAN/control
 dpkg-deb -b --root-owner-group -Zgzip pkg "vbindiff-$_DPKGARCH.deb"
