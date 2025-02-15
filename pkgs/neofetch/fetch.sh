064d4db2f92b572f1fcde61ce60d1a86f7da4fe2
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-15 10:30:37 -0500

    update neofetch

--- pkgs/neofetch/fetch.sh
+++ pkgs/neofetch/fetch.sh
@@ -2,5 +2,5 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir -p src
-curl -L -# -o src/neofetch https://raw.githubusercontent.com/Un1q32/hyfetch/fix-memory/neofetch
+curl -L -# -o src/neofetch https://raw.githubusercontent.com/hykilpikonna/hyfetch/8a536c30f07a1cc599285dc14f6020b0d7d9e44d/neofetch
 chmod 755 src/neofetch
