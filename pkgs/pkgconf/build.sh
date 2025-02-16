552c482118e7f64376050f81a30afc53e033df01
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-16 01:02:43 -0500

    add pkgconf

---
+++ pkgs/pkgconf/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+(
+cd src
+./autogen.sh
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" -j"$_JOBS"
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr
+rm -rf share
+"$_TARGET-strip" bin/* lib/libpkgconf.5.dylib 2>/dev/null || true
+ldid -S"$_ENT" bin/* lib/libpkgconf.5.dylib
+ln -s pkgconf bin/pkg-config
+)
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "pkgconf-$_DPKGARCH.deb"
---
+++ pkgs/pkgconf/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.gz https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-2.3.0.tar.gz
+printf "Unpacking source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv pkgconf-pkgconf-* src
