3c168bf78e423485f91f0d6bbcf19013165d9652
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-17 10:21:06 -0500

    replace O_CLOEXEC patches with open wrappers

--- pkgs/libc++/sdk/var/usr/include/fcntl.h
+++ pkgs/libc++/sdk/var/usr/include/fcntl.h
@@ -44,3 +44,38 @@ static inline int openat(int fd, const char *path, int flags, ...) {
 }
 
 #endif
+
+#ifndef O_CLOEXEC
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define O_CLOEXEC 0x1000000
+
+static inline int __iphoneports_open(const char *path, int flags, ...) {
+  mode_t mode = 0;
+  if (flags & O_CREAT) {
+    va_list va_args;
+    va_start(va_args, flags);
+    mode = va_arg(va_args, int);
+    va_end(va_args);
+  }
+  bool cloexec = false;
+  if (flags & O_CLOEXEC) {
+    flags &= ~O_CLOEXEC;
+    cloexec = true;
+  }
+  int fd = open(path, flags, mode);
+  if (cloexec && fd != -1) {
+    if (fcntl(fd, F_SETFD, FD_CLOEXEC) == -1) {
+      close(fd);
+      return -1;
+    }
+  }
+  return fd;
+}
+
+#define open __iphoneports_open
+
+#endif
---
+++ pkgs/luajit/sdk/var/usr/include/fcntl.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include_next <fcntl.h>
+
+#ifndef O_CLOEXEC
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define O_CLOEXEC 0x1000000
+
+static inline int __iphoneports_open(const char *path, int flags, ...) {
+  mode_t mode = 0;
+  if (flags & O_CREAT) {
+    va_list va_args;
+    va_start(va_args, flags);
+    mode = va_arg(va_args, int);
+    va_end(va_args);
+  }
+  bool cloexec = false;
+  if (flags & O_CLOEXEC) {
+    flags &= ~O_CLOEXEC;
+    cloexec = true;
+  }
+  int fd = open(path, flags, mode);
+  if (cloexec && fd != -1) {
+    if (fcntl(fd, F_SETFD, FD_CLOEXEC) == -1) {
+      close(fd);
+      return -1;
+    }
+  }
+  return fd;
+}
+
+#define open __iphoneports_open
+
+#endif
