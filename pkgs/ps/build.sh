ff4b27aa0ebd3b1f53c0f1f2b3a608b11fd0bcf9
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-17 13:49:32 -0500

    update ps

--- pkgs/ps/build.sh
+++ pkgs/ps/build.sh
@@ -2,7 +2,7 @@
 (
 cd src || exit 1
 for src in ps.c print.c nlist.c tasks.c keyword.c; do
-  "$_TARGET-cc" -Os -flto -c "$src" -D'__FBSDID(x)=' -Wno-deprecated-non-prototype -Wno-#warnings &
+    "$_TARGET-cc" -Os -flto -c "$src" -D'__FBSDID(x)=' -Wno-deprecated-non-prototype -Wno-#warnings &
 done
 wait
 "$_TARGET-cc" -o ps -Os -flto ./*.o
@@ -13,8 +13,16 @@ cp ps "$_PKGROOT/pkg/var/usr/bin"
 (
 cd pkg/var/usr/bin || exit 1
 "$_TARGET-strip" ps 2>/dev/null || true
-ldid -S"$_PKGROOT/files/ent.xml" ps
+case $_DPKGARCH in
+    iphoneos-*) ldid -S"$_PKGROOT/files/ios-entitlements.xml" ps ;;
+    *) ldid -S"$_PKGROOT/files/macos-entitlements.xml" ps ;;
+esac
+chmod 4755 ps
 )
+
+mkdir -p pkg/usr/local/libexec/iphoneports
+mv pkg/var/usr/bin/ps pkg/usr/local/libexec/iphoneports
+ln -s ../../../../usr/local/libexec/iphoneports/ps pkg/var/usr/bin/ps
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
