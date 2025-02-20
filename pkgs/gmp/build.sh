2339a6f2563de2cc00cda3ceff6cd82ab70d91ed
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-19 21:13:54 -0500

    gmp: dont disable asm on archs that can use it

--- pkgs/gmp/build.sh
+++ pkgs/gmp/build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh -e
+# shellcheck disable=2086
 (
-cd src || exit 1
-./configure --host="$_TARGET" --prefix=/var/usr --disable-assembly --disable-static
+cd src
+case ${_TARGET%%-*} in
+    arm64*) ;;
+    arm*) disableasm='--disable-assembly' ;;
+esac
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static $disableasm
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg"
 )
 
 (
-cd pkg/var/usr || exit 1
+cd pkg/var/usr
 rm -rf share
 "$_TARGET-strip" lib/libgmp.10.dylib 2>/dev/null || true
 ldid -S"$_ENT" lib/libgmp.10.dylib
