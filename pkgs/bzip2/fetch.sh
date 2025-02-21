602b8d23f50c329d847f0b783cf0a60cdc59e414
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-20 19:39:53 -0500

    fix bzip2 fetch script

--- pkgs/bzip2/fetch.sh
+++ pkgs/bzip2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://sourceware.org/pub/bzip2/bzip2-latest.tar.gz
+curl -L -# -o src.tar.gz https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv bzip2-1.0.8 src
+mv bzip2-* src
