1e610567209a606d5e26e63df75abc85a9703cf4
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-22 17:30:13 -0500

    update zstd

--- pkgs/zstd/build.sh
+++ pkgs/zstd/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -e
 (
 cd src
+mkdir tmpbin
+ln -s "$(command -v md5sum)" tmpbin/md5
+export PATH="$PATH:$PWD/tmpbin"
 "$_MAKE" CC="$_TARGET-cc" RANLIB="$_TARGET-ranlib" PREFIX=/var/usr UNAME=Darwin -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" PREFIX=/var/usr UNAME=Darwin install
 )
@@ -8,8 +11,8 @@ cd src
 (
 cd pkg/var/usr
 rm -rf share lib/libzstd.a
-"$_TARGET-strip" bin/zstd lib/libzstd.1.5.6.dylib 2>/dev/null || true
-ldid -S"$_ENT" bin/zstd lib/libzstd.1.5.6.dylib
+"$_TARGET-strip" bin/zstd lib/libzstd.1.*.dylib 2>/dev/null || true
+ldid -S"$_ENT" bin/zstd lib/libzstd.1.*.dylib
 )
 
 cp -r DEBIAN pkg
