1934b49946338df77ff1b340bc1474d7127aec6b
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-24 18:39:09 -0500

    fix

--- pkgs/2048/build.sh
+++ pkgs/2048/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 (
 cd src
-"$_TARGET-cc" -Os -flto -o 2048 2048.c
+"$_TARGET-cc" -std=c99 -Os -flto -o 2048 2048.c
 mkdir -p "$_PKGROOT/pkg/var/usr/bin"
 cp 2048 "$_PKGROOT/pkg/var/usr/bin"
 )
