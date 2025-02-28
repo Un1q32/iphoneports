f4296226c76191d64352e5f5ecd617d3e93f363a
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-27 22:10:10 -0500

    update ca-certificates

--- pkgs/ca-certificates/fetch.sh
+++ pkgs/ca-certificates/fetch.sh
@@ -2,4 +2,4 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir src
-curl -L -# -o src/cert.pem https://curl.se/ca/cacert-2024-12-31.pem
+curl -L -# -o src/cert.pem https://curl.se/ca/cacert-2025-02-25.pem
