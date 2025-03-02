ec1199bb67304a9d8b67bb356e67327ef7125c06
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-02 19:04:57 +0000

    update open wrapper functions so the optimizer likes them more

--- pkgs/libc++/sdk/var/usr/include/fcntl.h
+++ pkgs/libc++/sdk/var/usr/include/fcntl.h
@@ -55,18 +55,24 @@ static inline int openat(int fd, const char *path, int flags, ...) {
 
 static inline int __iphoneports_open(const char *path, int flags, ...) {
   mode_t mode = 0;
+  bool passmode = false;
   if (flags & O_CREAT) {
     va_list va_args;
     va_start(va_args, flags);
     mode = va_arg(va_args, int);
     va_end(va_args);
+    passmode = true;
   }
   bool cloexec = false;
   if (flags & O_CLOEXEC) {
     flags &= ~O_CLOEXEC;
     cloexec = true;
   }
-  int fd = open(path, flags, mode);
+  int fd;
+  if (passmode)
+    fd = open(path, flags, mode);
+  else
+    fd = open(path, flags);
   if (cloexec && fd != -1)
     fcntl(fd, F_SETFD, FD_CLOEXEC);
   return fd;
