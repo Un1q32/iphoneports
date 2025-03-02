01d51b4178d14445287c5b78b97efff0da3b100a
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-02 17:26:52 +0000

    update pfetch

--- pkgs/pfetch/fetch.sh
+++ pkgs/pfetch/fetch.sh
@@ -2,7 +2,7 @@
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir -p src
-ver=1.8.0
+ver=1.8.1
 curl -L -s -o src/pfetch "https://raw.githubusercontent.com/Un1q32/pfetch/$ver/pfetch" &
 curl -L -s -o src/LICENSE "https://raw.githubusercontent.com/Un1q32/pfetch/$ver/LICENSE"
 wait
