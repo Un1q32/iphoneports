a248fbe3269061152bf0a86242653e5b6b8eb214
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-05 17:13:05 +0000

    trick to save a few instructions

--- pkgs/libc++/sdk/var/usr/include/fcntl.h
+++ pkgs/libc++/sdk/var/usr/include/fcntl.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 80000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101000)
+
+#define openat __unused_libc_openat
 #include_next <fcntl.h>
+#undef openat
+
+#endif
 
 #ifndef AT_FDCWD
 #define AT_FDCWD -2
@@ -21,20 +30,12 @@
 
 #include <limits.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/stat.h>
 
-#define openat __iphoneports_openat
-
-static inline int openat(int fd, const char *path, int flags, ...) {
-  int mode;
-  if (flags & O_CREAT) {
-    va_list va_args;
-    va_start(va_args, flags);
-    mode = va_arg(va_args, int);
-    va_end(va_args);
-  }
-
+static inline int __iphoneports_openat(int fd, const char *path, int flags,
+                                       mode_t mode, bool passmode) {
 #ifdef __NO_O_CLOEXEC
   bool cloexec = false;
   if (__builtin_expect(flags & O_CLOEXEC, 0)) {
@@ -44,7 +45,11 @@ static inline int openat(int fd, const char *path, int flags, ...) {
 #endif
 
   if (fd == AT_FDCWD || path[0] == '/') {
-    int ret = open(path, flags, mode);
+    int ret;
+    if (passmode)
+      ret = open(path, flags, mode);
+    else
+      ret = open(path, flags);
 #ifdef __NO_O_CLOEXEC
     if (cloexec && __builtin_expect(ret != -1, 1))
       fcntl(ret, F_SETFD, FD_CLOEXEC);
@@ -61,12 +66,22 @@ static inline int openat(int fd, const char *path, int flags, ...) {
 
   strcat(fdpath, "/");
   strcat(fdpath, path);
-  int ret = open(fdpath, flags, mode);
+  int ret;
+  if (passmode)
+    ret = open(fdpath, flags, mode);
+  else
+    ret = open(fdpath, flags);
 #ifdef __NO_O_CLOEXEC
   if (cloexec && __builtin_expect(ret != -1, 1))
     fcntl(ret, F_SETFD, FD_CLOEXEC);
 #endif
   return ret;
 }
+
+#define __ARGSTEST(arg1, arg2, arg3, ...) arg3
+
+#define openat(fd, path, flags, ...)                                           \
+  __iphoneports_openat(fd, path, flags, __ARGSTEST(0, 0, ##__VA_ARGS__, 0),    \
+                       __ARGSTEST(0, ##__VA_ARGS__, true, false))
 
 #endif
