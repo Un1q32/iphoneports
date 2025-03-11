dfd024c11f88636de3f73d851abf5f6753dd8ab7
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-11 18:21:01 +0000

    update dpkg

--- pkgs/dpkg/fetch.sh
+++ pkgs/dpkg/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-dpkgver='1.22.17'
+dpkgver='1.22.18'
 curl -L -# -o src.tar.bz2 "https://salsa.debian.org/dpkg-team/dpkg/-/archive/$dpkgver/dpkg-$dpkgver.tar.bz2"
 printf "Unpacking source...\n"
 tar -xf src.tar.bz2
