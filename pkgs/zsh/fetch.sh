92d9ea4e13d4e6b5ff5aed9c1e8eabda0e43d6db
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-14 03:13:36 +0000

    update zsh fetch script to fix builds on arm64

--- pkgs/zsh/fetch.sh
+++ pkgs/zsh/fetch.sh
@@ -6,3 +6,6 @@ printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
 mv zsh-5.9 src
+curl -L -s -o src/config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=00b15927496058d23e6258a28d8996f87cf1f191' &
+curl -L -s -o src/config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=00b15927496058d23e6258a28d8996f87cf1f191'
+wait
