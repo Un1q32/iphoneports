7ffb8f3972d6930b11c1488a982586a86cc7cb08
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-20 13:24:58 -0400

    update libarchive

--- pkgs/libarchive/fetch.sh
+++ pkgs/libarchive/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://www.libarchive.org/downloads/libarchive-3.7.7.tar.xz
+curl -L -# -o src.tar.xz https://www.libarchive.org/downloads/libarchive-3.7.8.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
