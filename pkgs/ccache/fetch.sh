6f8a4bf8789552080080ebf25e535319777897be
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-22 12:49:36 -0400

    update ccache

--- pkgs/ccache/fetch.sh
+++ pkgs/ccache/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/ccache/ccache/releases/download/v4.11/ccache-4.11.tar.xz
+curl -L -# -o src.tar.xz https://github.com/ccache/ccache/releases/download/v4.11.2/ccache-4.11.2.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
