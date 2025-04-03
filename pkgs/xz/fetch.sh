221206439409803e707e19a6a20c3e916ff2f019
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-03 15:07:50 +0000

    update xz

--- pkgs/xz/fetch.sh
+++ pkgs/xz/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-ver=5.8.0
+ver=5.8.1
 curl -L -# -o src.tar.xz "https://github.com/tukaani-project/xz/releases/download/v$ver/xz-$ver.tar.xz"
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
