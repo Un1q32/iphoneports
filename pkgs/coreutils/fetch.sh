a4ed42904c856fe94f2b3535446e9e6b73ee6762
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-11 15:04:33 -0400

    update coreutils

--- pkgs/coreutils/fetch.sh
+++ pkgs/coreutils/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/coreutils/coreutils-9.6.tar.xz
+curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/coreutils/coreutils-9.7.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
