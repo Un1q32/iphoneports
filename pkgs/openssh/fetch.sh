bf888219929f213118f49124e8ff32b79239ade4
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-12 17:12:09 -0400

    update openssh

--- pkgs/openssh/fetch.sh
+++ pkgs/openssh/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.9p2.tar.gz
+curl -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-10.0p1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
