f32abdcd91f434add3a3f6ef5ad64e9012784ad8
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-13 19:12:31 -0400

    update nano

--- pkgs/nano/build.sh
+++ pkgs/nano/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 (
 cd src
-./configure --host="$_TARGET" --prefix=/var/usr PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
+./configure --host="$_TARGET" --prefix=/var/usr PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig" gl_cv_func_strcasecmp_works=yes
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
