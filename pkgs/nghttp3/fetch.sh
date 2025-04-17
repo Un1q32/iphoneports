7e735bb4edd8808bcae027380917fa27cab302e8
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-16 23:27:27 -0400

    update nghttp3 and ngtcp2

--- pkgs/nghttp3/fetch.sh
+++ pkgs/nghttp3/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/ngtcp2/nghttp3/releases/download/v1.8.0/nghttp3-1.8.0.tar.xz
+curl -L -# -o src.tar.xz https://github.com/ngtcp2/nghttp3/releases/download/v1.9.0/nghttp3-1.9.0.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
