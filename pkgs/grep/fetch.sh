c2a75586c0923565c5412f0fe6ac0f202b0593cc
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-17 12:37:24 -0400

    update grep

--- pkgs/grep/fetch.sh
+++ pkgs/grep/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/grep/grep-3.11.tar.xz
+curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/grep/grep-3.12.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv grep-3.11 src
+mv grep-* src
