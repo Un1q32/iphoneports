cc24383997057072a8741d481bd16953f11724ce
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-30 03:54:45 +0000

    update bzip3

--- pkgs/bzip3/fetch.sh
+++ pkgs/bzip3/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.zst https://github.com/kspalaiologos/bzip3/releases/download/1.5.1/bzip3-1.5.1.tar.zst
+curl -L -# -o src.tar.zst https://github.com/kspalaiologos/bzip3/releases/download/1.5.2/bzip3-1.5.2.tar.zst
 printf "Unpacking source...\n"
 tar -xf src.tar.zst
 rm src.tar.zst
