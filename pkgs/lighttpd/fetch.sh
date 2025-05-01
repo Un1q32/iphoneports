45ca090d1249264471ca91e610b4f3b23e6cf4a2
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-01 04:15:00 +0000

    update lighttpd

--- pkgs/lighttpd/fetch.sh
+++ pkgs/lighttpd/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.77.tar.xz
+curl -L -# -o src.tar.xz https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.79.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
