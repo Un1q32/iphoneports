c3133ac0932c079c2c498c03acb0e678c9e34635
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-13 23:33:42 +0000

    update m4

--- pkgs/m4/fetch.sh
+++ pkgs/m4/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz
+curl -L -# -o src.tar.gz https://ftp.gnu.org/gnu/m4/m4-1.4.20.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv m4-1.4.19 src
+mv m4-* src
