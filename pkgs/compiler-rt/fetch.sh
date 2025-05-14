bc02fed6bebaf2d385e2cb679b7509c36e22f889
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-14 20:44:58 +0000

    update llvm

--- pkgs/compiler-rt/fetch.sh
+++ pkgs/compiler-rt/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-llvmver='20.1.4'
+llvmver='20.1.5'
 curl -L -# -o src.tar.gz "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-$llvmver.tar.gz"
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
