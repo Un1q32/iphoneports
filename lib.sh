d2b517de8991c90c18b7e175e8720514f7206238
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-19 04:16:09 +0000

    fix

--- lib.sh
+++ lib.sh
@@ -7,5 +7,7 @@ fi
 
 strip_and_sign() {
     "$_TARGET-strip" "$@" 2>/dev/null || true
-    [ "$_SUBSYSTEM" != "macos" ] || [ "$_CPU" = "arm64" ] || [ "$_CPU" = "arm64e" ] && ldid -S"$_ENTITLEMENTS" "$@"
+    if [ "$_SUBSYSTEM" != "macos" ] || [ "$_CPU" = "arm64" ] || [ "$_CPU" = "arm64e" ]; then
+        ldid -S"$_ENTITLEMENTS" "$@"
+    fi
 }
