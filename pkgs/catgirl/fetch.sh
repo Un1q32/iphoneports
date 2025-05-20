551900258c1412cd503dee3eb0a02a844049ad09
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-20 07:39:25 +0000

    Revert "Reapply "readd catgirl""
    This reverts commit 802823823e62588afd9b94ce2b8b3ddce1be1973.

--- pkgs/catgirl/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://git.causal.agency/catgirl/snapshot/catgirl-2.2a.tar.gz
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv catgirl-* src
