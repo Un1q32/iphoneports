1e805852ccbfc35bc11dc7cc8219d8609ac5d495
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-20 19:03:50 -0400

    update mpdecimal

--- pkgs/mpdecimal/fetch.sh
+++ pkgs/mpdecimal/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-4.0.0.tar.gz
+curl -L -# -o src.tar.gz https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-4.0.1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
