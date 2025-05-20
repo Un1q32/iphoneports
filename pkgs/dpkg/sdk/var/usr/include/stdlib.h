9c19171e434150a55b258f8b3cb21065bb315d3b
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-20 03:17:03 +0000

    update dpkg back
    the source of the segfaults was diffing behavior in realpath

---
+++ pkgs/dpkg/sdk/var/usr/include/stdlib.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include_next <stdlib.h>
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 30200) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1060)
+
+#include <limits.h>
+#include <string.h>
+
+static inline char *__iphoneports_realpath(const char *restrict path,
+                                           char *restrict resolved_path) {
+  if (!resolved_path) {
+    char buf[PATH_MAX];
+    if (!realpath(path, buf))
+      return NULL;
+    return strdup(buf);
+  }
+  return realpath(path, resolved_path);
+}
+
+#define realpath __iphoneports_realpath
+
+#endif
