7a2bf3475b6d9154192bb7b38293921ed2210873
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-20 03:56:35 +0000

    readd catgirl

---
+++ pkgs/catgirl/sdk/var/usr/include/fcntl.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include_next <fcntl.h>
+
+#ifndef AT_FDCWD
+#define AT_FDCWD -2
+#endif
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 80000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101000)
+
+#include <limits.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define openat __iphoneports_openat
+
+static inline int openat(int fd, const char *path, int flags, ...) {
+  mode_t mode = 0;
+  if (flags & O_CREAT) {
+    va_list va_args;
+    va_start(va_args, flags);
+    mode = va_arg(va_args, int);
+    va_end(va_args);
+  }
+
+  if (fd == AT_FDCWD || path[0] == '/')
+    return open(path, flags, mode);
+
+  char fdpath[PATH_MAX];
+  if (fcntl(fd, F_GETPATH, fdpath) == -1)
+    return -1;
+
+  char new_path[strlen(fdpath) + strlen(path) + 2];
+  strcpy(new_path, fdpath);
+  strcat(new_path, "/");
+  strcat(new_path, path);
+  return open(new_path, flags, mode);
+}
+
+#endif
+
+#ifndef O_CLOEXEC
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define O_CLOEXEC 0x1000000
+
+static inline int __iphoneports_open(const char *path, int flags, ...) {
+  mode_t mode = 0;
+  if (flags & O_CREAT) {
+    va_list va_args;
+    va_start(va_args, flags);
+    mode = va_arg(va_args, int);
+    va_end(va_args);
+  }
+  bool cloexec = false;
+  if (flags & O_CLOEXEC) {
+    flags &= ~O_CLOEXEC;
+    cloexec = true;
+  }
+  int fd = open(path, flags, mode);
+  if (cloexec && fd != -1)
+    fcntl(fd, F_SETFD, FD_CLOEXEC);
+  return fd;
+}
+
+#define open __iphoneports_open
+
+#endif
