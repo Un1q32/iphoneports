95b5122c70a6122783b29e74b34b375d96c8ec27
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-28 02:56:46 -0400

    update curl

--- pkgs/curl/fetch.sh
+++ pkgs/curl/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://curl.se/download/curl-8.13.0.tar.xz
+curl -L -# -o src.tar.xz https://curl.se/download/curl-8.14.0.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
