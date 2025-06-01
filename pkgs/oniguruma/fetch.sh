79f2025df4c732b9f09ce2bdbd8f9287fd424ebc
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-01 18:59:27 -0400

    add oniguruma

---
+++ pkgs/oniguruma/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.gz https://github.com/kkos/oniguruma/releases/download/v6.9.10/onig-6.9.10.tar.gz
+printf "Unpacking source...\n"
+tar -xf src.tar.gz
+rm src.tar.gz
+mv onig-* src
