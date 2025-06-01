a53474cd043f7230ba9766241879a6fc87b2c5e9
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-01 05:33:57 -0400

    add jansson

---
+++ pkgs/jansson/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+. ../../lib.sh
+(
+cd src
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static --enable-silent-rules
+"$_MAKE" -j"$_JOBS"
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+strip_and_sign pkg/var/usr/lib/libjansson.*.dylib
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/LICENSE "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
