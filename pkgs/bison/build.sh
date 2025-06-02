a2c985464d1f5fc4ac9453db4e224f58c149b941
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-02 04:36:11 -0400

    add bison

---
+++ pkgs/bison/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+. ../../lib.sh
+(
+cd src
+./configure --host="$_TARGET" --prefix=/var/usr
+"$_MAKE" -j"$_JOBS"
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr
+rm -rf share/doc share/info share/man
+strip_and_sign bin/bison
+)
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/COPYING "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
