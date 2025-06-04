1bee545f60821bb475e1684ef37fd7fceb935928
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-04 08:54:34 -0400

    update libimobiledevice-glue

--- pkgs/libimobiledevice-glue/fetch.sh
+++ pkgs/libimobiledevice-glue/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/1.3.1/libimobiledevice-glue-1.3.1.tar.bz2
+curl -L -# -o src.tar.bz2 https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/1.3.2/libimobiledevice-glue-1.3.2.tar.bz2
 printf "Unpacking source...\n"
 tar -xf src.tar.bz2
 rm src.tar.bz2
