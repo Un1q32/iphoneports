18d089a8d4ba41898b927a84940572a3c6469a8b
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-10 09:51:40 -0400

    minor bash tweaks

--- pkgs/bash/build.sh
+++ pkgs/bash/build.sh
@@ -3,18 +3,14 @@ set -e
 . ../../lib.sh
 (
 cd src
-./configure --host="$_TARGET" --prefix=/var/usr --with-installed-readline CFLAGS="-Wno-parentheses -Wno-format-security -Wno-deprecated-non-prototype -O3" CC_FOR_BUILD='clang'
+./configure --host="$_TARGET" --prefix=/var/usr --with-installed-readline CFLAGS="-Wno-parentheses -Wno-format-security -Wno-deprecated-non-prototype -O3" CC_FOR_BUILD='clang' CFLAGS_FOR_BUILD='-std=c99 -O2'
 "$_MAKE" -j"$_JOBS"
 mkdir -p "$_PKGROOT/pkg/var/usr/bin"
 cp bash "$_PKGROOT/pkg/var/usr/bin"
 )
 
-(
-cd pkg/var/usr
-strip_and_sign bin/bash
-mkdir -p etc/bash/bashrc.d
-)
-
+strip_and_sign pkg/var/usr/bin/bash
+mkdir -p pkg/var/usr/etc/bash/bashrc.d
 cp files/bashrc pkg/var/usr/etc/bash
 
 mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
