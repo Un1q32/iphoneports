a530f9ba9042c8ec151c9082b68605aa794843e5
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-12 15:52:58 -0400

    remove libssh2 support from curl

--- pkgs/curl/build.sh
+++ pkgs/curl/build.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/sh -e
 set -e
 . ../../lib.sh
+
 (
 cd src
-./configure --host="$_TARGET" --prefix=/var/usr --with-openssl --with-ca-fallback --disable-static --with-libssh2 --with-nghttp2 --with-nghttp3 --with-ngtcp2 --with-libidn2 --with-ca-bundle=/var/usr/etc/ssl/cert.pem LIBS=-lngtcp2_crypto_quictls PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
+./configure --host="$_TARGET" --prefix=/var/usr --with-openssl --with-ca-fallback --disable-static --with-nghttp2 --with-nghttp3 --with-ngtcp2 --with-libidn2 --with-ca-bundle=/var/usr/etc/ssl/cert.pem PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
