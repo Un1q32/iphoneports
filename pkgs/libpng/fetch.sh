0b6327eef458f5040b071a1fbb73cbc195018f9c
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-15 14:38:28 -0400

    update libpng

--- pkgs/libpng/fetch.sh
+++ pkgs/libpng/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://download.sourceforge.net/libpng/libpng-1.6.48.tar.xz
+curl -L -# -o src.tar.xz https://download.sourceforge.net/libpng/libpng-1.6.49.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
