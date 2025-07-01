ebc83eb1e75633fd93f30c76fdda7cd023fbe5b3
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-01 15:22:31 -0400

    add lynx

---
+++ pkgs/lynx/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+. ../../lib.sh
+(
+cd src
+./configure --host="$_TARGET" --prefix=/var/usr --enable-ipv6 --with-ssl --with-brotli --with-zlib
+"$_MAKE" -j"$_JOBS"
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+rm -rf pkg/var/usr/share
+strip_and_sign pkg/var/usr/bin/lynx
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/COPYING "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
