2eb28ddcfb349f5123a5fdfda4053766c9c8d98b
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-04 22:16:57 -0400

    fix cppcheck build

--- pkgs/cppcheck/build.sh
+++ pkgs/cppcheck/build.sh
@@ -3,7 +3,7 @@ set -e
 . ../../lib.sh
 (
 cd src
-"$_MAKE" install CXX="$_TARGET-c++" PREFIX=/var/usr FILESDIR=/var/usr/share/cppcheck DESTDIR="$_PKGROOT/pkg" HAVE_RULES=yes uname_S=Darwin -j"$_JOBS"
+"$_MAKE" install CXX="$_TARGET-c++" PREFIX=/var/usr FILESDIR=/var/usr/share/cppcheck DESTDIR="$_PKGROOT/pkg" HAVE_RULES=yes uname_S=Darwin -j"$_JOBS" PCRE_CONFIG="$_SDK/var/usr/bin/pcre-config"
 )
 
 (
