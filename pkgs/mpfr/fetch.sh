83c29e4c4eaf757785b5a6595896afe390c2dadd
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-05 12:28:00 -0400

    update mpfr

--- pkgs/mpfr/fetch.sh
+++ pkgs/mpfr/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.xz
+curl -L -# -o src.tar.xz https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.2.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
-mv mpfr-4.2.1 src
+mv mpfr-* src
