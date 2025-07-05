c6d27dc20376583b6e4917440e20542b142f97c9
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-05 18:20:31 -0400

    update pkgconf

--- pkgs/pkgconf/fetch.sh
+++ pkgs/pkgconf/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-2.5.0.tar.gz
+curl -L -# -o src.tar.gz https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-2.5.1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
