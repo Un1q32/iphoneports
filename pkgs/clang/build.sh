5c20808a3e019fe040a076059fc2964ab92f5540
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-04 21:39:50 -0400

    fix llvm and clang builds on armv7

--- pkgs/clang/build.sh
+++ pkgs/clang/build.sh
@@ -26,6 +26,11 @@ tblgendir="$(command -v "$_TARGET-sdkpath")"
 tblgendir="${tblgendir%/*}/../share/iphoneports/bin"
 case $_CPU in
     arm64*|x86_64*) ltoopt='ON' ;;
+    arm*)
+        ltoopt='OFF'
+        export CFLAGS="-marm"
+        export CXXFLAGS="$CFLAGS"
+    ;;
     *) ltoopt='OFF' ;;
 esac
 cmake -GNinja ../clang -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_C_COMPILER="$_TARGET-cc" -DCMAKE_RANLIB="$(command -v "$_TARGET-ranlib")" -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_INSTALL_PREFIX=/var/usr -DCMAKE_INSTALL_NAME_TOOL="$_TARGET-install_name_tool" -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" -DCLANG_LINK_CLANG_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON -DLLVM_INCLUDE_TESTS=OFF -DLLVM_TABLEGEN_EXE="$tblgendir/llvm-tblgen" -DCLANG_TABLEGEN_EXE="$tblgendir/clang-tblgen" -DLLVM_ENABLE_LTO="$ltoopt" -DLLVM_ENABLE_LIBCXX=ON
