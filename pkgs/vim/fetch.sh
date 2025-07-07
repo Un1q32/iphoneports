fe95f242d400ad7b268881c1dcba4d2b2d5034e7
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-07 01:45:20 -0400

    update vim

--- pkgs/vim/fetch.sh
+++ pkgs/vim/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=de8f8f732ac1bcf69899df6ffd27dca9a4e66f3c
+commit=e2c0f81dd014b03a40af6b2c42463b7a0d92f5d6
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/vim/vim/archive/${commit}.tar.gz"
