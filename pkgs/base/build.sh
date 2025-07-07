ea404240bbb03f177b6c7c8923c3da91408cf0d8
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-07 01:43:12 -0400

    update base build script

--- pkgs/base/build.sh
+++ pkgs/base/build.sh
@@ -7,8 +7,8 @@ mkdir -p pkg/usr/local/bin pkg/usr/local/libexec/iphoneports pkg/var/usr/bin pkg
 (
 cd pkg
 
-"$_TARGET-cc" -Wall -Wextra -pedantic -std=c99 -Os -flto -o usr/local/bin/iphoneports-shell "$_PKGROOT/files/iphoneports-shell.c"
-"$_TARGET-cc" -Wall -Wextra -pedantic -std=c99 -Os -flto -o usr/local/libexec/iphoneports/iphoneports-chsh "$_PKGROOT/files/iphoneports-chsh.c"
+"$_TARGET-cc" -Wall -Wextra -pedantic -std=c99 -Os -o usr/local/bin/iphoneports-shell "$_PKGROOT/files/iphoneports-shell.c"
+"$_TARGET-cc" -Wall -Wextra -pedantic -std=c99 -Os -o usr/local/libexec/iphoneports/iphoneports-chsh "$_PKGROOT/files/iphoneports-chsh.c"
 
 strip_and_sign usr/local/bin/iphoneports-shell usr/local/libexec/iphoneports/iphoneports-chsh
 chmod 4755 usr/local/libexec/iphoneports/iphoneports-chsh
@@ -26,8 +26,10 @@ cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
 if [ "$_SUBSYSTEM" = "ios" ]; then
     case $_CPU in
-        (armv[67]) minver=3.0 ;;
-        (armv7s)   minver=6.0 ;;
+        (armv[67])       minver=3.0  ;;
+        (armv7s)         minver=6.0  ;;
+        (arm64|aarch64*) minver=7.0  ;;
+        (arm64e)         minver=12.0 ;;
     esac
     sed -i "/^Depends:/ s/$/, firmware (>= $minver)/" pkg/DEBIAN/control
 fi
