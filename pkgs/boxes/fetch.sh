930549b544ee055e205d3a01925ef076e8bd91c8
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-14 03:12:20 -0400

    remove boxes

--- pkgs/boxes/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/ascii-boxes/boxes/archive/refs/tags/v2.3.1.tar.gz
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv boxes-* src
