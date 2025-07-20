828c30147826542c203e65c2aff7fdc771216b3b
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-20 16:55:38 -0400

    update cppcheck

--- pkgs/cppcheck/fetch.sh
+++ pkgs/cppcheck/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/danmar/cppcheck/archive/refs/tags/2.17.1.tar.gz
+curl -L -# -o src.tar.gz https://github.com/danmar/cppcheck/archive/refs/tags/2.18.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
