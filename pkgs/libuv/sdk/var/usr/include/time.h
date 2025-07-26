2fc2f865644e42bf29aac4650e679be18aaedd5b
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-26 08:58:00 -0400

    add CPUTIME_ID clocks to clock_gettime

--- pkgs/libuv/sdk/var/usr/include/time.h
+++ pkgs/libuv/sdk/var/usr/include/time.h
@@ -35,6 +35,12 @@
 #ifndef CLOCK_UPTIME_RAW_APROX
 #define CLOCK_UPTIME_RAW_APROX 9
 #endif
+#ifndef CLOCK_PROCESS_CPUTIME_ID
+#define CLOCK_PROCESS_CPUTIME_ID 12
+#endif
+#ifndef CLOCK_THREAD_CPUTIME_ID
+#define CLOCK_THREAD_CPUTIME_ID 16
+#endif
 
 #define clock_gettime __iphoneports_clock_gettime
 
@@ -62,16 +68,55 @@ static int clock_gettime(int clockid, struct timespec *ts) {
     ts->tv_nsec = tv.tv_usec * 1000;
     return 0;
   }
-  case CLOCK_MONOTONIC_RAW_APROX:
-  case CLOCK_UPTIME_RAW_APROX:
+  case CLOCK_PROCESS_CPUTIME_ID: {
+    struct rusage ru;
+    int ret = getrusage(RUSAGE_SELF, &ru);
+    timeradd(&ru.ru_utime, &ru.ru_stime, &ru.ru_utime);
+    ts->tv_sec = ru.ru_utime.tv_sec;
+    ts->tv_nsec = ru.ru_utime.tv_usec * 1000;
+    return ret;
+  }
 #if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 80000) ||               \
     (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
      __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 101000) ||               \
     defined(__ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__) ||                     \
     defined(__ENVIRONMENT_WATCH_OS_VERSION_MIN_REQUIRED__)
+  case CLOCK_THREAD_CPUTIME_ID: {
+    static uint64_t (*syscall64)(int, ...) = NULL;
+    if (!syscall64)
+      syscall64 = dlfcn(RTLD_NEXT, "syscall");
+    mach_time = syscall64(SYS_thread_selfusage);
+  } break;
+  case CLOCK_MONOTONIC_RAW_APROX:
+  case CLOCK_UPTIME_RAW_APROX:
     mach_time = mach_approximate_time();
     break;
+#else
+  case CLOCK_THREAD_CPUTIME_ID: {
+    thread_basic_info_data_t info;
+    mach_msg_type_number_t count = THREAD_BASIC_INFO_COUNT;
+    thread_port_t thread = mach_thread_self();
+    int thread_info_ret =
+        thread_info(thread, THREAD_BASIC_INFO, (thread_info_t)(&info), &count);
+    mach_port_deallocate(mach_task_self(), thread);
+    if (thread_info_ret != 0)
+      return -1;
+
+    ts->tv_sec = info.user_time.seconds + info.system_time.seconds;
+    ts->tv_nsec =
+        (info.user_time.microseconds + info.system_time.microseconds) * 1000;
+    if (ts->tv_nsec >= 1000000000) {
+      ts->tv_sec += 1;
+      ts->tv_nsec -= 1000000000;
+    }
+    if (ts->tv_sec == 0 && ts->tv_nsec == 0)
+      ts->tv_nsec = 1;
+
+    return 0;
+  }
+  case CLOCK_MONOTONIC_RAW_APROX:
+  case CLOCK_UPTIME_RAW_APROX:
 #endif
   case CLOCK_MONOTONIC:
   case CLOCK_MONOTONIC_RAW:
