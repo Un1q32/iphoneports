4ce3efdab9e4e8843289e74a5f16780ae3d40cfa
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-26 21:01:03 -0400

    update sudo

--- pkgs/sudo/fetch.sh
+++ pkgs/sudo/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.sudo.ws/dist/sudo-1.9.17p1.tar.gz
+curl -L -# -o src.tar.gz https://www.sudo.ws/dist/sudo-1.9.17p2.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
