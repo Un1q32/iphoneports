d64a50bfe6efc5709d688ec627496a409cc59370
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-27 07:51:46 -0400

    jit segfaults on iOS

--- pkgs/luajit/build.sh
+++ pkgs/luajit/build.sh
@@ -4,7 +4,12 @@ set -e
 (
 cd src
 "$_TARGET-cc" -dM -E - < /dev/null | grep -q __LP64__ && arg=-m64
-"$_MAKE" amalg TARGET_SYS=Darwin HOST_CC="clang ${arg:--m32}" CROSS="$_TARGET-" BUILDMODE=dynamic CCOPT=-O3 PREFIX=/var/usr DESTDIR="$_PKGROOT/pkg" TARGET_CFLAGS="-DLUAJIT_ENABLE_JIT" MACOSX_DEPLOYMENT_TARGET="$_MACVER" -j"$_JOBS"
+if [ "$_SUBSYSTEM" = "ios" ]; then
+    sys='iOS'
+else
+    sys='Darwin'
+fi
+"$_MAKE" amalg TARGET_SYS="$sys" HOST_CC="clang ${arg:--m32}" CROSS="$_TARGET-" BUILDMODE=dynamic CCOPT=-O3 PREFIX=/var/usr DESTDIR="$_PKGROOT/pkg" MACOSX_DEPLOYMENT_TARGET="$_MACVER" -j"$_JOBS"
 "$_MAKE" install PREFIX=/var/usr DESTDIR="$_PKGROOT/pkg" TARGET_SYS=Darwin
 )
 
