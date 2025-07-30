c169d6b50cd01422c5228255a492f3c0036cace2
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-30 09:39:48 -0400

    nit

--- lib.sh
+++ lib.sh
@@ -7,8 +7,8 @@ fi
 
 strip_and_sign() {
     "$_TARGET-strip" "$@" 2>/dev/null || true
-    if { [ "$_SUBSYSTEM" != "macos" ] && [ "$_TRUEOSVER" -ge 20000 ]; } || 
-        [ "$_CPU" = "arm64" ] || [ "$_CPU" = "arm64e" ] || 
+    if { [ "$_SUBSYSTEM" != "macos" ] && [ "$_TRUEOSVER" -ge 20000 ]; } ||
+        [ "$_CPU" = "arm64" ] || [ "$_CPU" = "arm64e" ] ||
         [ "$_ALWAYSSIGN" = 1 ]; then
         ldid -S"$_ENTITLEMENTS" "$@"
     fi
