dc030782080bc6136a54960636777024856e3eb7
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-30 03:24:11 -0400

    git fix

--- pkgs/git/build.sh
+++ pkgs/git/build.sh
@@ -3,7 +3,7 @@ set -e
 . ../../lib.sh
 (
 cd src
-./configure --host="$_TARGET" --prefix=/var/usr --with-libpcre2 --with-curl --with-openssl --with-zlib CURL_CONFIG="$_SDK/var/usr/bin/curl-config" ac_cv_snprintf_returns_bogus=y ac_cv_iconv_omits_bom=y ac_cv_fread_reads_directories=y
+./configure --host="$_TARGET" --prefix=/var/usr --with-libpcre2 --with-curl --with-openssl --with-zlib CURL_CONFIG="$_SDK/var/usr/bin/curl-config" ac_cv_snprintf_returns_bogus=no ac_cv_iconv_omits_bom=no ac_cv_fread_reads_directories=yes
 "$_MAKE" install DESTDIR="$_PKGROOT/pkg" INSTALL_SYMLINKS=y CSPRNG_METHOD=openssl uname_M="$_CPU" uname_R= uname_S= uname_O= uname_V= -j"$_JOBS"
 )
 
