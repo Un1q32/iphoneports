f15bedea9266de166f7ca3461b2e475c64b9bfd5
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-01 14:31:25 -0400

    update tcsh

--- pkgs/tcsh/fetch.sh
+++ pkgs/tcsh/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://astron.com/pub/tcsh/tcsh-6.24.15.tar.gz
+curl -L -# -o src.tar.gz https://astron.com/pub/tcsh/tcsh-6.24.16.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
