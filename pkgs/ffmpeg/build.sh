6bc323ba2d4f4e7c09267d6de8c7f60f9aa09eb2
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-04 08:19:22 -0400

    fix in ffmpeg

--- pkgs/ffmpeg/build.sh
+++ pkgs/ffmpeg/build.sh
@@ -4,7 +4,7 @@ set -e
 (
 cd src
 export PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig"
-./configure --prefix=/var/usr --cross-prefix="$_TARGET-" --arch="$_CPU" --target-os=darwin --stdc=c23 --disable-stripping --disable-debug --disable-doc --disable-avdevice --disable-static --enable-shared --enable-lto --enable-zlib --enable-lzma --enable-bzlib --enable-libxml2 --enable-openssl --enable-libdav1d --enable-libwebp --pkg-config='pkg-config'
+./configure --prefix=/var/usr --cross-prefix="$_TARGET-" --arch="$_CPU" --target-os=darwin --stdc=c23 --disable-stripping --disable-debug --disable-doc --disable-avdevice --disable-static --enable-shared --enable-lto --enable-zlib --enable-lzma --enable-bzlib --enable-libxml2 --enable-openssl --enable-libdav1d --enable-libwebp --pkg-config='pkg-config' --enable-gpl --enable-version3
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
@@ -19,6 +19,9 @@ for lib in lib/*.dylib; do
     fi
 done
 )
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/COPYING.GPLv3 "pkg/var/usr/share/licenses/$_PKGNAME"
 
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
