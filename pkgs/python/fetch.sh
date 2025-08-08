7a1259425b3ebf86e3bb68ff1d9ffe7ebe1ea18c
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-07 20:50:33 -0400

    update python

--- pkgs/python/fetch.sh
+++ pkgs/python/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://www.python.org/ftp/python/3.13.5/Python-3.13.5.tar.xz
+curl -L -# -o src.tar.xz https://www.python.org/ftp/python/3.13.6/Python-3.13.6.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
