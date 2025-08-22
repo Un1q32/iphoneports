8b035fe0b4fd3b23517dc3eda08646f2cc9ff40b
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-21 23:57:11 -0400

    update cctools

--- pkgs/cctools/fetch.sh
+++ pkgs/cctools/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-commit=4eaecbf2028af167cb66671a2decd7a0bc55997a
+commit=82b2c73842cffe49d7a756b82bafb2c60a62e29d
 rm -rf pkg src
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz "https://github.com/Un1q32/cctools-port/archive/${commit}.tar.gz"
