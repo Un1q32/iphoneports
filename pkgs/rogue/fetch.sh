e15ab2f756dd12b6d26a0ff385f1b2c494162b90
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-22 22:54:01 -0400

    update config.guess and config.sub things

--- pkgs/rogue/fetch.sh
+++ pkgs/rogue/fetch.sh
@@ -6,6 +6,6 @@ printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
 mv rogue-5.4.4 src
-curl -L -s -o src/config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=00b15927496058d23e6258a28d8996f87cf1f191' &
-curl -L -s -o src/config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=00b15927496058d23e6258a28d8996f87cf1f191'
+curl -L -s -o src/config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=a2287c3041a3f2a204eb942e09c015eab00dc7dd' &
+curl -L -s -o src/config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=a2287c3041a3f2a204eb942e09c015eab00dc7dd'
 wait
