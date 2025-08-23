3ba9bcf34a4d833901b3fe037d93ced7d28b1f0e
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-22 21:29:25 -0400

    revert

--- pkgs/openssl/fetch.sh
+++ pkgs/openssl/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.0.17/openssl-3.0.17.tar.gz
+curl -L -# -o src.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.5.2/openssl-3.5.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
