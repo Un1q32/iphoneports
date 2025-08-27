5e13e32991de0fae165496c637bb4681a951a377
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-27 15:01:59 -0400

    update pcre2

--- pkgs/pcre2/fetch.sh
+++ pkgs/pcre2/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.45/pcre2-10.45.tar.bz2
+curl -L -# -o src.tar.bz2 https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.46/pcre2-10.46.tar.bz2
 printf "Unpacking source...\n"
 tar -xf src.tar.bz2
 rm src.tar.bz2
