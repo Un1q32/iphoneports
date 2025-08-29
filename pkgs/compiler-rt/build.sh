953397b8650030bcee0bda82a513629feab17aa4
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-29 17:23:19 -0400

    update compiler-rt

--- pkgs/compiler-rt/build.sh
+++ pkgs/compiler-rt/build.sh
@@ -14,49 +14,37 @@ armv7ssrcs="$arm64srcs atomic.c extendhfsf2.c truncsfhf2.c"
 armv7srcs="$armv7ssrcs"
 armv6srcs="$armv7srcs floatundidf.c floatundisf.c"
 
+clang() {
+    printf " \033[1;32mCC\033[0m %s\n" "$2"
+    command -v ccache >/dev/null && ccache=ccache
+    command $ccache clang "$@"
+}
+
 for src in $armv6srcs; do
-    while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
-        sleep 0.1
-    done
-    clang -isysroot "$_PKGROOT/sysroot" -target armv6-apple-ios2 "../lib/builtins/$src" -c -O3 -o "armv6-${src%\.c}.o" &
+    clang -o "armv6-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target armv6-apple-ios2 "../lib/builtins/$src" -c -O3 &
 done
 for src in $armv7srcs; do
-    while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
-        sleep 0.1
-    done
-    clang -isysroot "$_PKGROOT/sysroot" -target armv7-apple-ios3 "../lib/builtins/$src" -c -O3 -o "armv7-${src%\.c}.o" &
+    clang -o "armv7-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target armv7-apple-ios3 "../lib/builtins/$src" -c -O3 &
 done
 for src in $armv7ssrcs; do
-    while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
-        sleep 0.1
-    done
-    clang -isysroot "$_PKGROOT/sysroot" -target armv7s-apple-ios6 "../lib/builtins/$src" -c -O3 -o "armv7s-${src%\.c}.o" &
+    clang -o "armv7s-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target armv7s-apple-ios6 "../lib/builtins/$src" -c -O3 &
 done
 for src in $arm64srcs; do
-    while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
-        sleep 0.1
-    done
-    clang -isysroot "$_PKGROOT/sysroot" -target arm64-apple-ios7 "../lib/builtins/$src" -c -O3 -o "arm64-${src%\.c}.o" &
+    clang -o "arm64-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target arm64-apple-ios7 "../lib/builtins/$src" -c -O3 &
 done
-clang -target arm64e-apple-ios14 -xc /dev/null -c -o nothing.o &
+clang -o nothing.o -target arm64e-apple-ios14 -xc /dev/null -c &
 wait
 
 "$_TARGET-libtool" -static -o libclang_rt.ios.a ./*.o 2>/dev/null
 rm ./*.o
 
 for src in $x32srcs; do
-    while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
-        sleep 0.1
-    done
-    clang -isysroot "$_PKGROOT/sysroot" -target i386-apple-macos10.4 "../lib/builtins/$src" -c -O3 -o "i386-${src%\.c}.o" &
+    clang -o "i386-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target i386-apple-macos10.4 "../lib/builtins/$src" -c -O3 &
 done
 for src in $x64srcs; do
-    while [ "$(pgrep clang | wc -l)" -ge "$_JOBS" ]; do
-        sleep 0.1
-    done
-    clang -isysroot "$_PKGROOT/sysroot" -target x86_64-apple-macos10.4 "../lib/builtins/$src" -c -O3 -o "x86_64-${src%\.c}.o" &
+    clang -o "x86_64-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target x86_64-apple-macos10.4 "../lib/builtins/$src" -c -O3 &
 done
-clang -target arm64-apple-macos11.0 -arch arm64 -arch arm64e -xc /dev/null -c -o nothing.o &
+clang -o nothing.o -target arm64-apple-macos11.0 -arch arm64 -arch arm64e -xc /dev/null -c &
 wait
 
 "$_TARGET-libtool" -static -o libclang_rt.osx.a ./*.o 2>/dev/null
