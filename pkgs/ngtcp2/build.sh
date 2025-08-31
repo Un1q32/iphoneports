fa6c3d7de771d6c8658cfc4cc2e3ca0ed4095fc8
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-31 02:16:41 -0400

    go back to ngtcp2 in curl
    http3 doesnt work on leopard and iOS 2-3.1 with ossl-quic

---
+++ pkgs/ngtcp2/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+. ../../lib.sh
+
+(
+cd src
+./configure --host="$_TARGET" --prefix=/var/usr --disable-static
+"$_MAKE" install DESTDIR="$_PKGROOT/pkg" -j"$_JOBS"
+)
+
+(
+cd pkg/var/usr
+rm -rf share
+for lib in lib/*.dylib; do
+    [ -h "$lib" ] || strip_and_sign "$lib"
+done
+)
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/COPYING "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
---
+++ pkgs/ngtcp2/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.xz https://github.com/ngtcp2/ngtcp2/releases/download/v1.15.0/ngtcp2-1.15.0.tar.xz
+printf "Unpacking source...\n"
+tar -xf src.tar.xz
+rm src.tar.xz
+mv ngtcp2-* src
