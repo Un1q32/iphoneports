f4e29e68b13274e3492db9fa81a64f8fed4a3ca9
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-03 14:10:49 -0400

    update libjpeg-turbo

--- pkgs/libjpeg-turbo/fetch.sh
+++ pkgs/libjpeg-turbo/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/3.1.1.tar.gz
+curl -L -# -o src.tar.gz https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/3.1.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
