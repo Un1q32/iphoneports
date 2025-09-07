2465554029ac3840fdccab205cc8b6f8d931b7cc
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-07 19:59:53 -0400

    update imagemagick

--- pkgs/imagemagick/fetch.sh
+++ pkgs/imagemagick/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://imagemagick.org/archive/releases/ImageMagick-7.1.1-47.tar.xz
+curl -L -# -o src.tar.xz https://imagemagick.org/archive/releases/ImageMagick-7.1.2-3.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
