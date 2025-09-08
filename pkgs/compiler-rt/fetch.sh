7015563d6e4121d3319dbd61626111417e533d9d
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-07 20:08:39 -0400

    fix

--- pkgs/compiler-rt/fetch.sh
+++ pkgs/compiler-rt/fetch.sh
@@ -5,7 +5,7 @@ ver='21.1.0'
 if [ ! -f "$_DLCACHE/llvm-$ver.tar.gz" ] ||
     [ "$(sha256sum "$_DLCACHE/llvm-$ver.tar.gz" | awk '{print $1}')" != "fba0618cf8de48ec05880c446edd756a2669157eab9d29949e971c77da10275f" ]; then
     printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/llvm-$ver.tar.gz" https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-21.1.0.tar.gz
+    curl -L -# -o "$_DLCACHE/llvm-$ver.tar.gz" "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-$ver.tar.gz"
 fi
 printf "Unpacking source...\n"
 tar -xf "$_DLCACHE/llvm-$ver.tar.gz"
