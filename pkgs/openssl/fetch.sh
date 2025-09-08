2292c1d9a6a1aca1ad95501d0732fc76da2c37f5
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-07 20:11:43 -0400

    cache openssl tarballs

--- pkgs/openssl/fetch.sh
+++ pkgs/openssl/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.5.2/openssl-3.5.2.tar.gz
+ver='3.5.2'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/openssl-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/openssl-$ver.tar.gz" | awk '{print $1}')" != "c53a47e5e441c930c3928cf7bf6fb00e5d129b630e0aa873b08258656e7345ec" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/openssl-$ver.tar.gz" "https://github.com/openssl/openssl/releases/download/openssl-$ver/openssl-$ver.tar.gz"
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/openssl-$ver.tar.gz"
 mv openssl-* src
