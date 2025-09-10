cb68e5456c592a3facdb4d58a0ff1d49a106d894
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-10 14:42:59 -0400

    update llvm

--- pkgs/compiler-rt/fetch.sh
+++ pkgs/compiler-rt/fetch.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-rm -rf pkg src
-ver='21.1.0'
-[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
-if [ ! -f "$_DLCACHE/llvm-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/llvm-$ver.tar.gz" | awk '{print $1}')" != "fba0618cf8de48ec05880c446edd756a2669157eab9d29949e971c77da10275f" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/llvm-$ver.tar.gz" "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-$ver.tar.gz"
-fi
-printf "Unpacking source...\n"
-tar -xf "$_DLCACHE/llvm-$ver.tar.gz"
-mv llvm-project-llvmorg-* src
-printf '%s\n' "${ver%%.*}" > src/iphoneports-llvmversion.txt
+../llvm/fetch.sh
--- pkgs/llvm/fetch.sh
+++ pkgs/llvm/fetch.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 rm -rf pkg src
-ver='21.1.0'
+ver='21.1.1'
 [ -z "$_DLCACHE" ] && _DLCACHE=/tmp
 if [ ! -f "$_DLCACHE/llvm-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/llvm-$ver.tar.gz" | awk '{print $1}')" != "fba0618cf8de48ec05880c446edd756a2669157eab9d29949e971c77da10275f" ]; then
+    [ "$(sha256sum "$_DLCACHE/llvm-$ver.tar.gz" | awk '{print $1}')" != "5f048351ee63050d7fa45b6a1160768fb222a8d306a89e1344515ef7a4bcd278" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/llvm-$ver.tar.gz" "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-$ver.tar.gz"
 fi
 printf "Unpacking source...\n"
 tar -xf "$_DLCACHE/llvm-$ver.tar.gz"
 mv llvm-project-llvmorg-* src
+printf '%s\n' "${ver%%.*}" > src/iphoneports-llvmversion.txt
