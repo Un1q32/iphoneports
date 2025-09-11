4811cb5a5960e604513432695964d690663ed7b4
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-11 19:07:02 -0400

    add gnupg

---
+++ pkgs/gnupg/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+. ../../lib.sh
+
+(
+cd src
+./configure \
+    --host="$_TARGET" \
+    --prefix=/var/usr \
+    --disable-static \
+    --disable-doc \
+    --enable-silent-rules \
+    SYSROOT="$_SDK" \
+    PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig" \
+    PKG_CONFIG_SYSROOT_DIR="$_SDK/var/usr/lib/pkgconfig" \
+    GPGRT_CONFIG="$_SDK/var/usr/bin/gpgrt-config"
+"$_MAKE" -j"$_JOBS"
+"$_MAKE" DESTDIR="$_PKGROOT/pkg" install
+)
+
+(
+cd pkg/var/usr
+rm -rf share/doc
+strip_and_sign bin/*
+for bin in libexec/*; do
+    [ "$bin" != "libexec/gpg-wks-client" ] && strip_and_sign "$bin"
+done
+)
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/COPYING "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
---
+++ pkgs/gnupg/fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf pkg src
+printf "Downloading source...\n"
+curl -L -# -o src.tar.bz2 https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.8.tar.bz2
+printf "Unpacking source...\n"
+tar -xf src.tar.bz2
+rm src.tar.bz2
+mv gnupg-* src
