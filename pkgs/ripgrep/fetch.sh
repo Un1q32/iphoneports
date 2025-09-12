a9201b9f18c4f2517c59b29984328fd785209c3b
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-12 14:04:07 -0400

    remove ripgrep
    rust is being removed until i can figure out how to fix the cc crate

--- pkgs/ripgrep/fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/BurntSushi/ripgrep/archive/refs/tags/14.1.1.tar.gz
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv ripgrep-* src
