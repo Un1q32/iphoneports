f4bc95fa60aff045588efe52097b203df2cb1f23
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-14 20:31:57 -0400

    fix

--- pkgs/ccache/fetch.sh
+++ pkgs/ccache/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/ccache/ccache/archive/refs/tags/v4.12.tar.gz
+curl -L -# -o src.tar.gz https://github.com/ccache/ccache/archive/refs/tags/v4.12.tar.gz
 printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
+tar -xf src.tar.gz
+rm src.tar.gz
 mv ccache-* src
