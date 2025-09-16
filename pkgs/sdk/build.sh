12cc6e22427c9d1ab0e3bee0be2a00d72c4b2c01
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-16 18:17:07 -0400

    trick to make clang use the /var/usr prefix stuff

--- pkgs/sdk/build.sh
+++ pkgs/sdk/build.sh
@@ -6,6 +6,10 @@ mkdir -p pkg/var/usr/etc/profile.d
 cp "$_PKGROOT/files/sdkroot.sh" pkg/var/usr/etc/profile.d
 cp -a "$("$_TARGET-sdkpath")" pkg/var/usr/sdk
 
+mkdir pkg/var/usr/sdk/usr/local
+ln -s /var/usr/include pkg/var/usr/sdk/usr/local
+ln -s /var/usr/lib pkg/var/usr/sdk/usr/local
+
 cp -r DEBIAN pkg
 sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
 dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
