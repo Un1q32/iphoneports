98ceca5c1128bf857b405d769017362e20fc4114
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-17 14:08:02 -0400

    update fmt

--- pkgs/fmt/fetch.sh
+++ pkgs/fmt/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/fmtlib/fmt/archive/refs/tags/11.2.0.tar.gz
+curl -L -# -o src.tar.gz https://github.com/fmtlib/fmt/archive/refs/tags/12.0.0.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
