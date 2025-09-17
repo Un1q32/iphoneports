933083c1a2b424027c5039ad5af17b7cbb663497
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-16 23:35:47 -0400

    add lld

---
+++ pkgs/lld/build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+. ../../lib.sh
+
+(
+mkdir -p src/build
+cd src/build
+cmake -GNinja ../lld \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER="$_TARGET-cc" \
+    -DCMAKE_RANLIB="$(command -v "$_TARGET-ranlib")" \
+    -DCMAKE_SYSTEM_NAME=Darwin \
+    -DCMAKE_INSTALL_PREFIX=/var/usr \
+    -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+    -DCMAKE_FIND_ROOT_PATH="$_SDK/var/usr" \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_TABLEGEN_EXE="$(command -v llvm-tblgen)" \
+    -DLLVM_ENABLE_LTO=Thin
+DESTDIR="$_PKGROOT/pkg" ninja -j"$_JOBS" install
+)
+
+(
+cd pkg/var/usr
+rm -rf include lib
+strip_and_sign bin/lld
+for link in ld.lld ld64.lld lld-link wasm-ld; do
+    ln -sf lld "bin/$link"
+done
+)
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/lld/LICENSE.TXT "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
---
+++ pkgs/lld/fetch.sh
@@ -0,0 +1,1 @@
+../llvm/fetch.sh
