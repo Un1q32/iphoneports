e8133187ad386d6301068cfc7e463431062f3503
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-19 15:55:18 -0400

    update neofetch

--- pkgs/neofetch/fetch.sh
+++ pkgs/neofetch/fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-version=2.0.2
+version=ac81ef3551a8070e4ec118ee56379c263a48469e
 rm -rf pkg src
 printf "Downloading source...\n"
 mkdir -p src
-curl -L -s -o src/neofetch "https://raw.githubusercontent.com/hykilpikonna/hyfetch/refs/tags/$version/neofetch" &
-curl -L -s -o src/LICENSE.md "https://raw.githubusercontent.com/hykilpikonna/hyfetch/refs/tags/$version/LICENSE.md"
+curl -L -s -o src/neofetch "https://raw.githubusercontent.com/Un1q32/hyfetch/$version/neofetch" &
+curl -L -s -o src/LICENSE.md "https://raw.githubusercontent.com/Un1q32/hyfetch/$version/LICENSE.md"
 wait
 chmod 755 src/neofetch
