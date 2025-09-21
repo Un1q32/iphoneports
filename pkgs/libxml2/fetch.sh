5571e07a6b8a56fcd5c31c3890dc546832622af1
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-21 17:45:45 -0400

    update libxml2

--- pkgs/libxml2/fetch.sh
+++ pkgs/libxml2/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://download.gnome.org/sources/libxml2/2.14/libxml2-2.14.6.tar.xz
+curl -L -# -o src.tar.xz https://download.gnome.org/sources/libxml2/2.15/libxml2-2.15.0.tar.xz
 printf "Unpacking source...\n"
 tar -xf src.tar.xz
 rm src.tar.xz
