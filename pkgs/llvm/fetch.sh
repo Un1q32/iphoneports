e2c95c4a3e116966cb23331493da29ef60cabf20
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-23 17:36:47 -0400

    update llvm

--- pkgs/llvm/fetch.sh
+++ pkgs/llvm/fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 rm -rf pkg src
-ver='21.1.1'
+ver='21.1.2'
 [ -z "$_DLCACHE" ] && _DLCACHE=/tmp
 if [ ! -f "$_DLCACHE/llvm-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/llvm-$ver.tar.gz" | awk '{print $1}')" != "5f048351ee63050d7fa45b6a1160768fb222a8d306a89e1344515ef7a4bcd278" ]; then
+    [ "$(sha256sum "$_DLCACHE/llvm-$ver.tar.gz" | awk '{print $1}')" != "eced3dd78186621f4df8a1accbcd1ecf2ee399571e62d052c21e9bf363af2166" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/llvm-$ver.tar.gz" "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-$ver.tar.gz"
 fi
