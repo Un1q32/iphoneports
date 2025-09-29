c4a0211bcdf714e41e9f7b72e54ecee3780b5929
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-28 22:07:56 -0400

    add fd

---
+++ pkgs/fd/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+. ../../files/lib.sh
+
+(
+cd src
+SDKROOT="$_SDK" cargo build --target "$_RUSTTARGET" --release -j "$_JOBS"
+mkdir -p "$_PKGROOT/pkg/var/usr/bin"
+cp "target/$_RUSTTARGET/release/fd" "$_PKGROOT/pkg/var/usr/bin"
+)
+
+strip_and_sign pkg/var/usr/bin/fd
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/LICENSE-MIT "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
--- pkgs/libsyscompat/files/libsyscompat.c
+++ pkgs/libsyscompat/files/libsyscompat.c
@@ -146,6 +146,85 @@ int clock_gettime(int clockid, struct timespec *ts) {
     (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
      __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101000)
 
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#ifndef AT_FDCWD
+#define AT_FDCWD -2
+#endif
+#ifndef AT_EACCESS
+#define AT_EACCESS 0x0010
+#endif
+
+static inline int pthread_fchdir_np(int fd) {
+  return syscall(SYS___pthread_fchdir, fd);
+}
+
+int faccessat(int fd, const char *path, int mode, int flags) {
+  static bool init = false;
+  static int (*func)(int, const char *, int, int);
+
+  if (!init) {
+    func = (int (*)(int, const char *, int, int))dlsym(RTLD_NEXT, "faccessat");
+    init = true;
+  }
+
+  if (func)
+    return func(fd, path, mode, flags);
+
+  if (flags & ~AT_EACCESS) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  uid_t ruid = getuid(), euid = geteuid();
+  gid_t rgid = getgid(), egid = getegid();
+  int check_euid = ruid != euid && (flags & AT_EACCESS);
+  int check_egid = rgid != egid && (flags & AT_EACCESS);
+  if (check_euid)
+    setreuid(euid, ruid);
+  if (check_egid)
+    setregid(egid, rgid);
+
+  if (fd == AT_FDCWD || path[0] == '/') {
+    int ret = access(path, mode);
+    int errnobak = errno;
+    if (check_euid)
+      setreuid(ruid, euid);
+    if (check_egid)
+      setregid(rgid, egid);
+    errno = errnobak;
+    return ret;
+  }
+
+  int cwd = open(".", O_RDONLY);
+  if (pthread_fchdir_np(-1) < 0 && cwd != -1) {
+    close(cwd);
+    cwd = -1;
+  }
+  if (pthread_fchdir_np(fd) < 0) {
+    pthread_fchdir_np(cwd);
+    if (cwd != -1)
+      close(cwd);
+    return -1;
+  }
+
+  int ret = access(path, mode);
+  int errnobak = errno;
+  if (check_euid)
+    setreuid(ruid, euid);
+  if (check_egid)
+    setregid(rgid, egid);
+  errno = errnobak;
+
+  pthread_fchdir_np(cwd);
+  if (cwd != -1)
+    close(cwd);
+
+  return ret;
+}
+
 void arc4random_buf(void *, size_t);
 
 int CCRandomGenerateBytes(void *buf, size_t size) {
