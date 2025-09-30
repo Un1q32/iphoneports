d69f8881009b4a237849436fc5d9dad86291c5ca
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-30 16:56:15 -0400

    update cmake

--- pkgs/cmake/fetch.sh
+++ pkgs/cmake/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/Kitware/CMake/archive/refs/tags/v4.1.1.tar.gz
+curl -L -# -o src.tar.gz https://github.com/Kitware/CMake/archive/refs/tags/v4.1.2.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
