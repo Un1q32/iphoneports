07051df59a857aafccf599c6704a526c0ee23947
Un1q32 <joey.t.reinhart@gmail.com>
2025-09-30 17:01:11 -0400

    cache cmake downloads

--- pkgs/cmake/fetch.sh
+++ pkgs/cmake/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/Kitware/CMake/archive/refs/tags/v4.1.2.tar.gz
+ver='4.1.2'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/cmake-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/cmake-$ver.tar.gz" | awk '{print $1}')" != "ccf48cd50c3441c5eee23351ffbf1264bc246e2ce94c953e4440747d7b74877e" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/cmake-$ver.tar.gz" "https://github.com/Kitware/CMake/archive/refs/tags/v$ver.tar.gz"
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/cmake-$ver.tar.gz"
 mv CMake-* src
