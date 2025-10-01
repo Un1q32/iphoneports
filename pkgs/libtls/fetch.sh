8b1b4e0298b9c97d942ce86fefffed08c391274b
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-01 00:16:42 -0400

    update libtls and nc

--- pkgs/libtls/fetch.sh
+++ pkgs/libtls/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-4.1.0.tar.gz
+curl -L -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-4.1.1.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
