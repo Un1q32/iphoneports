11116751902d944fa50b389e336cce6ed0feaf26
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-01 00:21:54 -0400

    cache libressl

--- pkgs/libtls/fetch.sh
+++ pkgs/libtls/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-4.1.1.tar.gz
+ver='4.1.1'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/libressl-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/libressl-$ver.tar.gz" | awk '{print $1}')" != "c7ff7a7d675d5f57730940e5ccff1dbe2dcd5b7405b5397e0f7ffd66a5ed5679" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/libressl-$ver.tar.gz" "https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$ver.tar.gz"
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/libressl-$ver.tar.gz"
 mv libressl-* src
