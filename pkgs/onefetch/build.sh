7a61ef818f2ff2bed80cc68fbcf082f40e80beb6
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-02 03:30:04 -0400

    add onefetch

---
+++ pkgs/onefetch/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+. ../../files/lib.sh
+
+(
+cd src
+SDKROOT="$_SDK" \
+    TARGET_PKG_CONFIG=pkg-config \
+    PKG_CONFIG_LIBDIR="$_SDK/var/usr/lib/pkgconfig" \
+    PKG_CONFIG_SYSROOT_DIR="$_SDK" \
+    ZSTD_SYS_USE_PKG_CONFIG=1 \
+    cargo build --target "$_RUSTTARGET" --release -j "$_JOBS"
+mkdir -p "$_PKGROOT/pkg/var/usr/bin"
+cp "target/$_RUSTTARGET/release/onefetch" "$_PKGROOT/pkg/var/usr/bin"
+)
+
+strip_and_sign pkg/var/usr/bin/onefetch
+
+mkdir -p "pkg/var/usr/share/licenses/$_PKGNAME"
+cp src/LICENSE.md "pkg/var/usr/share/licenses/$_PKGNAME"
+
+cp -r DEBIAN pkg
+sed -e "s|@DPKGARCH@|$_DPKGARCH|" DEBIAN/control > pkg/DEBIAN/control
+dpkg-deb -b --root-owner-group -Zgzip pkg "$_PKGNAME.deb"
