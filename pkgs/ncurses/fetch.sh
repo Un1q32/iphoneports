d216cbcd800788180ddcb6ec3d96465e81ee6169
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-04 17:38:19 -0400

    cache ncurses

--- pkgs/ncurses/fetch.sh
+++ pkgs/ncurses/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftpmirror.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz
+ver='6.5'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/ncurses-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/ncurses-$ver.tar.gz" | awk '{print $1}')" != "136d91bc269a9a5785e5f9e980bc76ab57428f604ce3e5a5a90cebc767971cc6" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/ncurses-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/ncurses/ncurses-$ver.tar.gz"
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv ncurses-6.5 src
+tar -xf "$_DLCACHE/ncurses-$ver.tar.gz"
+mv ncurses-* src
