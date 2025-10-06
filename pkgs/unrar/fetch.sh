f407228a0e02e9a1b4bcf0599218fc634d00df77
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-06 00:58:07 -0400

    cache unrar

--- pkgs/unrar/fetch.sh
+++ pkgs/unrar/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.rarlab.com/rar/unrarsrc-7.1.10.tar.gz
+ver='7.1.10'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/unrar-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/unrar-$ver.tar.gz" | awk '{print $1}')" != "72a9ccca146174f41876e8b21ab27e973f039c6d10b13aabcb320e7055b9bb98" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/unrar-$ver.tar.gz" "https://www.rarlab.com/rar/unrarsrc-$ver.tar.gz"
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/unrar-$ver.tar.gz"
 mv unrar src
