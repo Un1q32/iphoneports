b34d46d9a99a6253af516f4c086b3f11fe5cdeac
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-06 02:21:31 -0400

    update nano

--- pkgs/nano/fetch.sh
+++ pkgs/nano/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf pkg src
-ver='8.4'
+ver='8.6'
 [ -z "$_DLCACHE" ] && _DLCACHE=/tmp
 if [ ! -f "$_DLCACHE/nano-$ver.tar.gz" ] ||
     [ "$(sha256sum "$_DLCACHE/nano-$ver.tar.gz" | awk '{print $1}')" != "35acc088bc190943382b4da2752563d28cf31351c1478419c4c82908fba94456" ]; then
