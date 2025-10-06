19be4bc32ef527c723c1c4f971bc333cd82faa23
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-06 01:32:25 -0400

    cache git

--- pkgs/git/fetch.sh
+++ pkgs/git/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.51.0.tar.xz
+ver='2.51.0'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/git-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/git-$ver.tar.xz" | awk '{print $1}')" != "60a7c2251cc2e588d5cd87bae567260617c6de0c22dca9cdbfc4c7d2b8990b62" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/git-$ver.tar.xz" "https://mirrors.edge.kernel.org/pub/software/scm/git/git-$ver.tar.xz"
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
+tar -xf "$_DLCACHE/git-$ver.tar.xz"
 mv git-* src
