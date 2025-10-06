4ab4c1c156f2a148fb93e3f5679fb1c989f31247
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-06 02:23:10 -0400

    fix hash

--- pkgs/nano/fetch.sh
+++ pkgs/nano/fetch.sh
@@ -3,7 +3,7 @@ rm -rf pkg src
 ver='8.6'
 [ -z "$_DLCACHE" ] && _DLCACHE=/tmp
 if [ ! -f "$_DLCACHE/nano-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/nano-$ver.tar.gz" | awk '{print $1}')" != "35acc088bc190943382b4da2752563d28cf31351c1478419c4c82908fba94456" ]; then
+    [ "$(sha256sum "$_DLCACHE/nano-$ver.tar.gz" | awk '{print $1}')" != "86608f72b77787bf15dde922077c62a679aa4536192942991f58a92062838187" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/nano-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/nano/nano-$ver.tar.gz" || exit 1
 fi
