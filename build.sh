6d2ca95bcdd81863308eaed28c18e045e4e2aadc
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-08 05:04:09 -0400

    shorten line

--- build.sh
+++ build.sh
@@ -86,7 +86,34 @@ error() {
 export ZERO_AR_DATE=1
 
 depcheck() {
-    for dep in "$_TARGET-cc" "$_TARGET-sdkpath" sha256sum awk ldid dpkg-deb fakeroot automake autoreconf m4 yacc ctags tar gzip bzip2 xz zstd ninja sed pgrep meson cmake curl git pkg-config perl; do
+    for dep in \
+        "$_TARGET-cc" \
+        "$_TARGET-sdkpath" \
+        sha256sum \
+        awk \
+        ldid \
+        dpkg-deb \
+        fakeroot \
+        automake \
+        autoconf \
+        autoreconf \
+        m4 \
+        yacc \
+        ctags \
+        tar \
+        gzip \
+        bzip2 \
+        xz \
+        zstd \
+        ninja \
+        sed \
+        pgrep \
+        meson \
+        cmake \
+        curl \
+        git \
+        pkg-config \
+        perl; do
         if ! command -v "$dep" > /dev/null; then
             error "Missing dependency: $dep"
         fi
