c2ddfb34444b1b422d97d7c305362e470fe8b1b7
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-07 22:45:11 -0400

    update python

--- pkgs/python/fetch.sh
+++ pkgs/python/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://www.python.org/ftp/python/3.13.7/Python-3.13.7.tar.xz
+ver='3.14.0'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/python-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/python-$ver.tar.xz" | awk '{print $1}')" != "2299dae542d395ce3883aca00d3c910307cd68e0b2f7336098c8e7b7eee9f3e9" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/python-$ver.tar.xz" "https://www.python.org/ftp/python/$ver/Python-$ver.tar.xz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
+tar -xf "$_DLCACHE/python-$ver.tar.xz"
 mv Python-* src
