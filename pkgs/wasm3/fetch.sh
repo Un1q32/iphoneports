6d58465d8e719b2369eebd32bfbfa24e8ea6efdf
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-10 14:01:25 -0400

    cache wasm3

--- pkgs/wasm3/fetch.sh
+++ pkgs/wasm3/fetch.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-commit=79d412ea5fcf92f0efe658d52827a0e0a96ff442
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz "https://github.com/wasm3/wasm3/archive/${commit}.tar.gz"
+ver='79d412ea5fcf92f0efe658d52827a0e0a96ff442'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/wasm3-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/wasm3-$ver.tar.gz" | awk '{print $1}')" != "adc76b13fc4d2192428dbe7f18617e94a7b3e464b0ae65f282ad50ac1c5f5088" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/wasm3-$ver.tar.gz" "https://github.com/wasm3/wasm3/archive/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv "wasm3-${commit}" src
+tar -xf "$_DLCACHE/wasm3-$ver.tar.gz"
+mv wasm3-* src
