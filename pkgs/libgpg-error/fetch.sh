623412521373bcc3a351d3a4c168b37457bb4ddd
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-09 20:32:22 -0400

    update libgpg-error

--- pkgs/libgpg-error/fetch.sh
+++ pkgs/libgpg-error/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.55.tar.bz2
+ver='1.56'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/libgpg-error-$ver.tar.bz2" ] ||
+    [ "$(sha256sum "$_DLCACHE/libgpg-error-$ver.tar.bz2" | awk '{print $1}')" != "82c3d2deb4ad96ad3925d6f9f124fe7205716055ab50e291116ef27975d169c0" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/libgpg-error-$ver.tar.bz2" "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$ver.tar.bz2" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.bz2
-rm src.tar.bz2
+tar -xf "$_DLCACHE/libgpg-error-$ver.tar.bz2"
 mv libgpg-error-* src
