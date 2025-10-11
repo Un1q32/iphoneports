ae86c323b8a8d4498d8521b02cbbaba04ea72799
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-11 14:27:13 -0400

    disable ipv6 on wget too to save code size

--- pkgs/wget/build.sh
+++ pkgs/wget/build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+# shellcheck disable=2086
 set -e
 . ../../files/lib.sh
+
 (
 cd src
-./configure --host="$_TARGET" --prefix=/var/usr --with-ssl=openssl --disable-iri
+if [ "$_SUBSYSTEM" = "ios" ] && [ "$_TRUEOSVER" -lt 20000 ]; then
+    ipv6='--disable-ipv6'
+fi
+./configure --host="$_TARGET" --prefix=/var/usr --with-ssl=openssl --disable-iri $ipv6
 "$_MAKE" -j"$_JOBS"
 "$_MAKE" DESTDIR="$_PKGROOT/pkg" install
 )
