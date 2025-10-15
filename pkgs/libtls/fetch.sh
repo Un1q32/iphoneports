876d3b394a2db9c6e80a58ba7cb2c29bc1818a75
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-15 00:58:08 -0400

    update libtls and nc

--- pkgs/libtls/fetch.sh
+++ pkgs/libtls/fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 rm -rf pkg src
-ver='4.1.1'
+ver='4.2.0'
 [ -z "$_DLCACHE" ] && _DLCACHE=/tmp
 if [ ! -f "$_DLCACHE/libressl-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libressl-$ver.tar.gz" | awk '{print $1}')" != "c7ff7a7d675d5f57730940e5ccff1dbe2dcd5b7405b5397e0f7ffd66a5ed5679" ]; then
+    [ "$(sha256sum "$_DLCACHE/libressl-$ver.tar.gz" | awk '{print $1}')" != "0f7dba44d7cb8df8d53f2cfbf1955254bc128e0089595f1aba2facfaee8408b2" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libressl-$ver.tar.gz" "https://cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$ver.tar.gz" || exit 1
 fi
