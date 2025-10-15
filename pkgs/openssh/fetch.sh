426ac9a980fee7533eb2d2956ad1388687d493c0
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-15 06:00:24 -0400

    update openssh

--- pkgs/openssh/fetch.sh
+++ pkgs/openssh/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -# -o src.tar.gz https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-10.0p2.tar.gz
+ver='10.2p1'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/openssh-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/openssh-$ver.tar.gz" | awk '{print $1}')" != "ccc42c0419937959263fa1dbd16dafc18c56b984c03562d2937ce56a60f798b2" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/openssh-$ver.tar.gz" "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/openssh-$ver.tar.gz"
 mv openssh-* src
