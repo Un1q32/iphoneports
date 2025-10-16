1ad7787660348af5435ef0a6af2e462936244a60
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-16 03:09:34 -0400

    update ripgrep

--- pkgs/ripgrep/fetch.sh
+++ pkgs/ripgrep/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/BurntSushi/ripgrep/archive/refs/tags/14.1.1.tar.gz
+ver='15.0.0'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/ripgrep-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/ripgrep-$ver.tar.gz" | awk '{print $1}')" != "e6b2d35ff79c3327edc0c92a29dc4bb74e82d8ee4b8156fb98e767678716be7a" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/ripgrep-$ver.tar.gz" "https://github.com/BurntSushi/ripgrep/archive/refs/tags/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/ripgrep-$ver.tar.gz"
 mv ripgrep-* src
