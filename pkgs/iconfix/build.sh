f7f3052559b7131cc51eef838c01893ffd2ba0fb
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-19 17:49:04 -0400

    prevent building iconfix for incompatible platforms

--- pkgs/iconfix/build.sh
+++ pkgs/iconfix/build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 . ../../files/lib.sh
+
+if [ "$_SUBSYSTEM" != "ios" ] ||
+    { [ "$_SUBSYSTEM" = "ios" ] &&
+    { [ "$_TRUEOSVER" -ge 70000 ] || [ "$_TRUEOSVER" -lt 20000 ]; }; }; then
+    printf 'iconfix is for iOS 2 through 6\n'
+    mkdir pkg
+    exit 0
+fi
+
 mkdir -p "$_PKGROOT/pkg/usr/bin" "$_PKGROOT/pkg/usr/share/iconfix"
 cp "$_PKGROOT/src/iconfix.sh" "$_PKGROOT/pkg/usr/bin/iconfix"
 cp "$_PKGROOT"/src/*.png "$_PKGROOT/pkg/usr/share/iconfix"
