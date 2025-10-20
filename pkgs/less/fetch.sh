a877454188aed6acbd4df68296773df6d9a0a074
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-20 08:28:35 -0400

    update less and libxml2

--- pkgs/less/fetch.sh
+++ pkgs/less/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://www.greenwoodsoftware.com/less/less-679.tar.gz
+curl -L -# -o src.tar.gz https://www.greenwoodsoftware.com/less/less-685.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
