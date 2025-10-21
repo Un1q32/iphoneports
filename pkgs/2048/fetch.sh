70ea275de3a254f872411bdb38924109ebfcbe2e
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-21 12:42:48 -0400

    better fetch script

--- pkgs/2048/fetch.sh
+++ pkgs/2048/fetch.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 rm -rf pkg src
 printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/mevdschee/2048.c/archive/refs/tags/v1.0.3.tar.gz
-printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv 2048.c-* src
+mkdir src
+curl -L -s -o src/2048.c https://raw.githubusercontent.com/mevdschee/2048.c/refs/tags/v1.0.3/2048.c &
+curl -L -s -o src/LICENSE https://raw.githubusercontent.com/mevdschee/2048.c/refs/tags/v1.0.3/LICENSE
+wait
