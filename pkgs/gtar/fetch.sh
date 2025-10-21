710ac1e20bcb960ac1b9b7d15a66bb0cd7fc4055
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-21 07:09:12 -0400

    cache gtar

--- pkgs/gtar/fetch.sh
+++ pkgs/gtar/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://ftpmirror.gnu.org/gnu/tar/tar-1.35.tar.xz
+ver='1.35'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/gtar-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/gtar-$ver.tar.xz" | awk '{print $1}')" != "4d62ff37342ec7aed748535323930c7cf94acf71c3591882b26a7ea50f3edc16" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/gtar-$ver.tar.xz" "https://ftpmirror.gnu.org/gnu/tar/tar-$ver.tar.xz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
-mv tar-1.35 src
+tar -xf "$_DLCACHE/gtar-$ver.tar.xz"
+mv tar-* src
