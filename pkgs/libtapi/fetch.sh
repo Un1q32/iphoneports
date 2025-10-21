b8d2913c9d8a0d6e5c2b98d335fb7117ed379548
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-21 18:24:25 -0400

    cache libtapi

--- pkgs/libtapi/fetch.sh
+++ pkgs/libtapi/fetch.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-commit=aa37c11ad1a817248c9d1578ac99e133875b4eb5
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz "https://github.com/tpoechtrager/apple-libtapi/archive/${commit}.tar.gz"
+ver='aa37c11ad1a817248c9d1578ac99e133875b4eb5'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/libtapi-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/libtapi-$ver.tar.gz" | awk '{print $1}')" != "3ea293da90789055e847af48582a46ff296fc6dfa4ef8ee8db3167599dd93da3" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/libtapi-$ver.tar.gz" "https://github.com/tpoechtrager/apple-libtapi/archive/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv "apple-libtapi-${commit}" src
+tar -xf "$_DLCACHE/libtapi-$ver.tar.gz"
+mv apple-libtapi-* src
