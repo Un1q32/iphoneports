4b20846a20cfff11c36f7490e2a45168b51c23df
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-21 07:32:10 -0400

    update pcre2

--- pkgs/pcre2/fetch.sh
+++ pkgs/pcre2/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.46/pcre2-10.46.tar.bz2
+ver='10.47'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/pcre2-$ver.tar.bz2" ] ||
+    [ "$(sha256sum "$_DLCACHE/pcre2-$ver.tar.bz2" | awk '{print $1}')" != "47fe8c99461250d42f89e6e8fdaeba9da057855d06eb7fc08d9ca03fd08d7bc7" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/pcre2-$ver.tar.bz2" "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-$ver/pcre2-$ver.tar.bz2" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.bz2
-rm src.tar.bz2
+tar -xf "$_DLCACHE/pcre2-$ver.tar.bz2"
 mv pcre2-* src
