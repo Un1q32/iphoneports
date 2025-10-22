2cbda6577a67f795b68cf8c9af3d2523679e34de
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-22 15:59:22 -0400

    update cctools

--- pkgs/cctools/fetch.sh
+++ pkgs/cctools/fetch.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-commit=59375f9eb8a0642e1c3dfa4b1753f1b636ed5d34
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz "https://github.com/Un1q32/cctools-port/archive/${commit}.tar.gz"
+ver='38fcee34eb502c63482dba1a1b3246bf2f051fdb'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/cctools-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/cctools-$ver.tar.gz" | awk '{print $1}')" != "6e56188f5ada0e12f9464eb08fb93abd16943536390025d5ac3506207b0ab750" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/cctools-$ver.tar.gz" "https://github.com/Un1q32/cctools-port/archive/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv "cctools-port-${commit}" src
+tar -xf "$_DLCACHE/cctools-$ver.tar.gz"
+mv cctools-port-* src
