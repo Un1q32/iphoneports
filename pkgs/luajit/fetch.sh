b98e1282a145ce805255fae0ec14680670f05d45
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-23 10:36:34 -0400

    update luajit

--- pkgs/luajit/fetch.sh
+++ pkgs/luajit/fetch.sh
@@ -1,9 +1,12 @@
-#!/bin/sh -e
-commit=871db2c84ecefd70a850e03a6c340214a81739f0
+#!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz "https://github.com/LuaJIT/LuaJIT/archive/${commit}.tar.gz"
+ver='25a61a182166fec06f1a1a025eb8fabbb6cf483e'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/luajit-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/luajit-$ver.tar.gz" | awk '{print $1}')" != "3fca2bb5068d7150d324a34eaac555757b8ce94f15565e0a0552373f7534081e" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/luajit-$ver.tar.gz" "https://github.com/LuaJIT/LuaJIT/archive/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv "LuaJIT-${commit}" src
+tar -xf "$_DLCACHE/luajit-$ver.tar.gz"
+mv LuaJIT-* src
