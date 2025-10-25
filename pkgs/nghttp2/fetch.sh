ebb63f5e18266787ec9631a4118fa88ccb8d4508
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-25 09:24:38 -0400

    update nghttp2

--- pkgs/nghttp2/fetch.sh
+++ pkgs/nghttp2/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/nghttp2/nghttp2/releases/download/v1.67.1/nghttp2-1.67.1.tar.xz
+ver='1.68.0'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/nghttp2-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/nghttp2-$ver.tar.xz" | awk '{print $1}')" != "5511d3128850e01b5b26ec92bf39df15381c767a63441438b25ad6235def902c" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/nghttp2-$ver.tar.xz" "https://github.com/nghttp2/nghttp2/releases/download/v$ver/nghttp2-$ver.tar.xz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
+tar -xf "$_DLCACHE/nghttp2-$ver.tar.xz"
 mv nghttp2-* src
