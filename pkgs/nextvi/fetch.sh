a563f7e09ef9429adefe0224ac93c71f97f669f2
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-26 14:20:12 -0400

    update nextvi

--- pkgs/nextvi/fetch.sh
+++ pkgs/nextvi/fetch.sh
@@ -1,9 +1,12 @@
-#!/bin/sh -e
-commit=b56362546f0a89971e2078a0e1ebfd2c2b497905
+#!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz "https://github.com/Un1q32/nextvi/archive/${commit}.tar.gz"
+ver='ffc0710084719443b27fddad8d8c4fb9c1046b50'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/nextvi-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/nextvi-$ver.tar.gz" | awk '{print $1}')" != "84bd575d2e077d0e706ea3c6c1f36667f7b7b1a4b0597a679b7e8e0a7b5383ab" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/nextvi-$ver.tar.gz" "https://github.com/Un1q32/nextvi/archive/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
-mv "nextvi-${commit}" src
+tar -xf "$_DLCACHE/nextvi-$ver.tar.gz"
+mv nextvi-* src
