dd5baf20ee883936bfff2be4454cb68b634f6097
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-29 16:14:30 -0400

    update fmt

--- pkgs/fmt/fetch.sh
+++ pkgs/fmt/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/fmtlib/fmt/archive/refs/tags/12.0.0.tar.gz
+ver='12.1.0'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/fmt-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/fmt-$ver.tar.gz" | awk '{print $1}')" != "ea7de4299689e12b6dddd392f9896f08fb0777ac7168897a244a6d6085043fea" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/fmt-$ver.tar.gz" "https://github.com/fmtlib/fmt/archive/refs/tags/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.gz
-rm src.tar.gz
+tar -xf "$_DLCACHE/fmt-$ver.tar.gz"
 mv fmt-* src
