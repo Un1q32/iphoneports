0c1bba2506f9d77489df8d315755417f94884f17
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-31 12:40:29 -0400

    update bash-completion

--- pkgs/bash-completion/fetch.sh
+++ pkgs/bash-completion/fetch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm -rf pkg src
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://github.com/scop/bash-completion/releases/download/2.16.0/bash-completion-2.16.0.tar.xz
+ver='2.17.0'
+[ -z "$_DLCACHE" ] && _DLCACHE=/tmp
+if [ ! -f "$_DLCACHE/bash-completion-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/bash-completion-$ver.tar.xz" | awk '{print $1}')" != "dd9d825e496435fb3beba3ae7bea9f77e821e894667d07431d1d4c8c570b9e58" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/bash-completion-$ver.tar.xz" "https://github.com/scop/bash-completion/releases/download/$ver/bash-completion-$ver.tar.xz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -xf src.tar.xz
-rm src.tar.xz
+tar -xf "$_DLCACHE/bash-completion-$ver.tar.xz"
 mv bash-completion-* src
