c62c8d295127a16c6ad4b7690f71b6ac6d42ae8d
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-01 16:56:06 -0400

    nit

--- pkgs/lzop/fetch.sh
+++ pkgs/lzop/fetch.sh
@@ -5,4 +5,4 @@ curl -L -# -o src.tar.gz https://www.lzop.org/download/lzop-1.04.tar.gz
 printf "Unpacking source...\n"
 tar -xf src.tar.gz
 rm src.tar.gz
-mv lzop-1.04 "$_SRCDIR"
+mv lzop-* "$_SRCDIR"
