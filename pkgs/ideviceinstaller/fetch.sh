1d6febbbed4365a8a121bde18a1484837d5c96cd
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-01 13:18:55 -0400

    fix

--- pkgs/ideviceinstaller/fetch.sh
+++ pkgs/ideviceinstaller/fetch.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 rm -rf "$_DESTDIR" "$_SRCDIR"
 printf "Downloading source...\n"
-git clone https://github.com/libimobiledevice/ideviceinstaller.git src
-cd src || exit 1
+git clone https://github.com/libimobiledevice/ideviceinstaller.git "$_SRCDIR"
+cd "$_SRCDIR"
 git -c advice.detachedHead=false checkout 1431d42b568ee78161a41ed02df0de60dc1439d6
