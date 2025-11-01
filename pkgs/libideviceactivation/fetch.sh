9afa7e5f9b554e32cd4755833754cf37afe7a85d
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-01 13:19:29 -0400

    fix

--- pkgs/libideviceactivation/fetch.sh
+++ pkgs/libideviceactivation/fetch.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 rm -rf "$_DESTDIR" "$_SRCDIR"
 printf "Downloading source...\n"
-git clone https://github.com/libimobiledevice/libideviceactivation.git src
-cd src || exit 1
+git clone https://github.com/libimobiledevice/libideviceactivation.git "$_SRCDIR"
+cd "$_SRCDIR"
 git -c advice.detachedHead=false checkout ecc10ef8048c6591b936c5ca1b0971157087e6b2
