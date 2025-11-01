430876696d82d1275436c4aed869acb94fb935d2
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-01 17:10:02 -0400

    nit

--- pkgs/pam/fetch.sh
+++ pkgs/pam/fetch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 rm -rf "$_DESTDIR" "$_SRCDIR"
 printf "Downloading source...\n"
 curl -L -# -o src.tar.gz https://github.com/apple-oss-distributions/pam/archive/refs/tags/pam-32.1.tar.gz
@@ -6,7 +6,7 @@ printf "Unpacking source...\n"
 tar -C "$_TMP" -xf src.tar.gz
 rm src.tar.gz
 mv "$_TMP"/pam-pam-* "$_SRCDIR"
-cd "$_SRCDIR" || exit 1
+cd "$_SRCDIR"
 printf "Downloading modules source...\n"
 curl -L -# -o src.tar.gz https://github.com/apple-oss-distributions/pam_modules/archive/refs/tags/pam_modules-36.2.tar.gz
 printf "Unpacking modules source...\n"
