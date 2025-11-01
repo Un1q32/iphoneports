5a1ba6a4e028716b9d030624cd8a8d90d3839e74
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-01 13:17:52 -0400

    fix

--- pkgs/libimobiledevice/fetch.sh
+++ pkgs/libimobiledevice/fetch.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 rm -rf "$_DESTDIR" "$_SRCDIR"
 printf "Downloading source...\n"
-git clone https://github.com/libimobiledevice/libimobiledevice.git src
-cd src || exit 1
+git clone https://github.com/libimobiledevice/libimobiledevice.git "$_SRCDIR"
+cd "$_SRCDIR"
 git -c advice.detachedHead=false checkout c8cdf20fe20b0c46ed7d9a9386bed03301ddbfa5
