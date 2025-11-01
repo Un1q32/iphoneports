bada265ceaf25355c42b53cf6d1ad99b6b404fca
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-31 22:46:56 -0400

    another nit

--- pkgs/compiler-rt/build.sh
+++ pkgs/compiler-rt/build.sh
@@ -17,20 +17,20 @@ armv6srcs="$armv7srcs floatundisf.c floatundidf.c"
 cc() {
     printf " \033[1;32mCC\033[0m %s\n" "$2"
     command -v ccache >/dev/null && ccache=ccache
-    $ccache clang "$@"
+    $ccache clang -isysroot "$_PKGROOT/sysroot" "$@"
 }
 
 for src in $armv6srcs; do
-    cc -o "armv6-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target armv6-apple-ios1 "../lib/builtins/$src" -c -O3 &
+    cc -o "armv6-${src%\.c}.o" -target armv6-apple-ios1 "../lib/builtins/$src" -c -O3 &
 done
 for src in $armv7srcs; do
-    cc -o "armv7-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target armv7-apple-ios3 "../lib/builtins/$src" -c -O3 &
+    cc -o "armv7-${src%\.c}.o" -target armv7-apple-ios3 "../lib/builtins/$src" -c -O3 &
 done
 for src in $armv7ssrcs; do
-    cc -o "armv7s-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target armv7s-apple-ios6 "../lib/builtins/$src" -c -O3 &
+    cc -o "armv7s-${src%\.c}.o" -target armv7s-apple-ios6 "../lib/builtins/$src" -c -O3 &
 done
 for src in $arm64srcs; do
-    cc -o "arm64-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target arm64-apple-ios7 "../lib/builtins/$src" -c -O3 &
+    cc -o "arm64-${src%\.c}.o" -target arm64-apple-ios7 "../lib/builtins/$src" -c -O3 &
 done
 cc -o nothing.o -target arm64e-apple-ios14 -xc /dev/null -c &
 wait
@@ -39,10 +39,10 @@ wait
 rm ./*.o
 
 for src in $x32srcs; do
-    cc -o "i386-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target i386-apple-macos10.4 "../lib/builtins/$src" -c -O3 &
+    cc -o "i386-${src%\.c}.o" -target i386-apple-macos10.4 "../lib/builtins/$src" -c -O3 &
 done
 for src in $x64srcs; do
-    cc -o "x86_64-${src%\.c}.o" -isysroot "$_PKGROOT/sysroot" -target x86_64-apple-macos10.4 "../lib/builtins/$src" -c -O3 &
+    cc -o "x86_64-${src%\.c}.o" -target x86_64-apple-macos10.4 "../lib/builtins/$src" -c -O3 &
 done
 cc -o nothing.o -target arm64-apple-macos11.0 -arch arm64 -arch arm64e -xc /dev/null -c &
 wait
