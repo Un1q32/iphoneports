4268bd6d41c840708ffefc827f0f4ed7d7b68103
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-04 20:55:29 -0500

    update dav1d

--- pkgs/dav1d/fetch.sh
+++ pkgs/dav1d/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://code.videolan.org/videolan/dav1d/-/archive/1.5.1/dav1d-1.5.1.tar.bz2
+ver='1.5.2'
+if [ ! -f "$_DLCACHE/dav1d-$ver.tar.bz2" ] ||
+    [ "$(sha256sum "$_DLCACHE/dav1d-$ver.tar.bz2" | awk '{print $1}')" != "c748a3214cf02a6d23bc179a0e8caea9d6ece1e46314ef21f5508ca6b5de6262" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/dav1d-$ver.tar.bz2" "https://code.videolan.org/videolan/dav1d/-/archive/$ver/dav1d-$ver.tar.bz2" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.bz2
-rm src.tar.bz2
+tar -C "$_TMP" -xf "$_DLCACHE/dav1d-$ver.tar.bz2"
 mv "$_TMP"/dav1d-* "$_SRCDIR"
