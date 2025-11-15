349e2ae9ba321af425f3d68b432d3f4e1f3fa484
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-15 18:23:37 -0500

    update cctools

--- pkgs/cctools/fetch.sh
+++ pkgs/cctools/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='080a4b1b41d8d03ea56654d50d4cc9c344e46eb7'
+ver='12e2486bc81c3b2be975d3e117a9d3ab6ec3970c'
 if [ ! -f "$_DLCACHE/cctools-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/cctools-$ver.tar.gz" | awk '{print $1}')" != "3a283f652138ee6182ac0e895b8979c0c9e99d66f236d46d9d651d70c1470cd5" ]; then
+    [ "$(sha256sum "$_DLCACHE/cctools-$ver.tar.gz" | awk '{print $1}')" != "791714a4eeb3ff8f1017ba634936366f33634296eb37dcb1699b058bd34b44c1" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/cctools-$ver.tar.gz" "https://github.com/Un1q32/cctools-port/archive/$ver.tar.gz" || exit 1
 fi
