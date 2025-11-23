165a9211c926cdc5a86035bf757c36d80d5d3ba5
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-23 17:19:46 -0500

    update ffmpeg

--- pkgs/ffmpeg/fetch.sh
+++ pkgs/ffmpeg/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='8.0'
+ver='8.0.1'
 if [ ! -f "$_DLCACHE/ffmpeg-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/ffmpeg-$ver.tar.xz" | awk '{print $1}')" != "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e" ]; then
+    [ "$(sha256sum "$_DLCACHE/ffmpeg-$ver.tar.xz" | awk '{print $1}')" != "05ee0b03119b45c0bdb4df654b96802e909e0a752f72e4fe3794f487229e5a41" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/ffmpeg-$ver.tar.xz" "https://ffmpeg.org/releases/ffmpeg-$ver.tar.xz" || exit 1
 fi
