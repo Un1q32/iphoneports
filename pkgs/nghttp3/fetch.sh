b7527876c90ce269d3c7961603b21017612c0d69
Un1q32 <joey.t.reinhart@gmail.com>
2025-11-24 05:28:56 -0500

    update nghttp3

--- pkgs/nghttp3/fetch.sh
+++ pkgs/nghttp3/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.13.0'
+ver='1.13.1'
 if [ ! -f "$_DLCACHE/nghttp3-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/nghttp3-$ver.tar.xz" | awk '{print $1}')" != "926d358a74d6f15e5a11239e3e980dd4c66332e52e4d1b38c2cf8820458ad4d8" ]; then
+    [ "$(sha256sum "$_DLCACHE/nghttp3-$ver.tar.xz" | awk '{print $1}')" != "020836668c711d5c166969f8b165fbfd989e6967d0601947bf608f29e2158518" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/nghttp3-$ver.tar.xz" "https://github.com/ngtcp2/nghttp3/releases/download/v$ver/nghttp3-$ver.tar.xz" || exit 1
 fi
