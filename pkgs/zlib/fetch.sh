0e7aa27d06d33a595d90858d665ab9ffed934a24
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-02 18:55:42 -0500

    fix

--- pkgs/zlib/fetch.sh
+++ pkgs/zlib/fetch.sh
@@ -2,7 +2,7 @@
 rm -rf "$_DESTDIR" "$_SRCDIR"
 ver='2.3.2'
 if [ ! -f "$_DLCACHE/zlib-ng-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/zlib-ng-$ver.tar.gz" | awk '{print $1}')" != "94cfa0a53a8265c813c8369d3963bf09ac6a357824ad1f93aee13486176e152e" ]; then
+    [ "$(sha256sum "$_DLCACHE/zlib-ng-$ver.tar.gz" | awk '{print $1}')" != "6a0561b50b8f5f6434a6a9e667a67026f2b2064a1ffa959c6b2dae320161c2a8" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/zlib-ng-$ver.tar.gz" "https://github.com/zlib-ng/zlib-ng/archive/refs/tags/$ver.tar.gz" || exit 1
 fi
