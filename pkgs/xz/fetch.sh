002f73a44175dc865c306ec4dd11e03ec6f74c0f
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-17 13:05:58 -0500

    update xz

--- pkgs/xz/fetch.sh
+++ pkgs/xz/fetch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-ver=5.8.1
-curl -L -# -o src.tar.xz "https://github.com/tukaani-project/xz/releases/download/v$ver/xz-$ver.tar.xz"
+ver='5.8.2'
+if [ ! -f "$_DLCACHE/xz-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/xz-$ver.tar.xz" | awk '{print $1}')" != "890966ec3f5d5cc151077879e157c0593500a522f413ac50ba26d22a9a145214" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/xz-$ver.tar.xz" "https://github.com/tukaani-project/xz/releases/download/v$ver/xz-$ver.tar.xz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.xz
-rm src.tar.xz
+tar -C "$_TMP" -xf "$_DLCACHE/xz-$ver.tar.xz"
 mv "$_TMP"/xz-* "$_SRCDIR"
