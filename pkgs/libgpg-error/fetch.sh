79a840191f91ebe85096874b62dae1c6362c0342
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-17 23:03:55 -0500

    update libgpg-error

--- pkgs/libgpg-error/fetch.sh
+++ pkgs/libgpg-error/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.57'
+ver='1.58'
 if [ ! -f "$_DLCACHE/libgpg-error-$ver.tar.bz2" ] ||
     [ "$(sha256sum "$_DLCACHE/libgpg-error-$ver.tar.bz2" | awk '{print $1}')" != "ab807c81fbd2b8e1d6e3377383be802147c08818f87a82e87f85e5939c939def" ]; then
     printf "Downloading source...\n"
