45ba24874696177a4ca839ff5fd6da1bbcee5c51
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-21 22:21:34 -0500

    update cppcheck

--- pkgs/cppcheck/fetch.sh
+++ pkgs/cppcheck/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/danmar/cppcheck/archive/refs/tags/2.18.0.tar.gz
+ver='2.19.0'
+if [ ! -f "$_DLCACHE/cppcheck-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/cppcheck-$ver.tar.gz" | awk '{print $1}')" != "c6cff9d3bbcb3da941bf7f525ae974b6c7af3d610c4c5519fcd1be3f21f5ae09" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/cppcheck-$ver.tar.gz" "https://github.com/danmar/cppcheck/archive/refs/tags/$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
+tar -C "$_TMP" -xf "$_DLCACHE/cppcheck-$ver.tar.gz"
 mv "$_TMP"/cppcheck-* "$_SRCDIR"
