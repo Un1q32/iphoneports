8b5b4de07d5d2a86f78d079f737bec574f7db7ad
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-31 15:57:49 -0500

    update compiler-rt

--- pkgs/llvm/fetch.sh
+++ pkgs/llvm/fetch.sh
@@ -11,7 +11,7 @@ tar -C "$_TMP" -xf "$_DLCACHE/llvm-$ver.tar.gz"
 mv "$_TMP"/llvm-project-llvmorg-* "$_SRCDIR"
 if [ "$_PKGNAME" = 'compiler-rt' ]; then
     printf '%s\n' "${ver%%.*}" > "$_SRCDIR/iphoneports-llvmversion.txt"
-    ubsanver='affbc86d7f0e6fdb2452da52fd4c61c221bb4810'
+    ubsanver='731e93a71d47d291d7ec1ae0efd35df5ed6d22ed'
     curl -L -s -o "$_SRCDIR/compiler-rt/ubsan.c" "https://raw.githubusercontent.com/Un1q32/ubsan/$ubsanver/ubsan.c" &
     curl -L -s -o "$_SRCDIR/compiler-rt/UBSAN-LICENSE" "https://raw.githubusercontent.com/Un1q32/ubsan/$ubsanver/LICENSE"
     wait
