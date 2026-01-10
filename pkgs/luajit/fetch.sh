db89b1afdf39036fe200f29fca3cac0e05ecb94f
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-10 17:56:21 -0500

    update luajit

--- pkgs/luajit/fetch.sh
+++ pkgs/luajit/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='7152e15489d2077cd299ee23e3d51a4c599ab14f'
+ver='707c12bf00dafdfd3899b1a6c36435dbbf6c7022'
 if [ ! -f "$_DLCACHE/luajit-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/luajit-$ver.tar.gz" | awk '{print $1}')" != "2103f2c9526d158259a627d51799de8016e2a2c6d89f6ef25f9d89b92c597511" ]; then
+    [ "$(sha256sum "$_DLCACHE/luajit-$ver.tar.gz" | awk '{print $1}')" != "ac2defd1e9b4ab07ac0a52f2403318ffb0d54505b975dff0cfc49a927ab84d90" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/luajit-$ver.tar.gz" "https://github.com/LuaJIT/LuaJIT/archive/$ver.tar.gz" || exit 1
 fi
