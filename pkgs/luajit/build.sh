07b021e74d0e054e19b09fa3d4f34f72603b47b9
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-10 19:02:31 -0500

    luajit has a lot of assembly that uses r9
    when building for armv6 iPhone OS 2.x, increase the iOS version to 3.0
    (shouldn't cause issues with the 2.0 SDK) to also take advantage of r9
    in the C code, we might as well.
    
    when building for armv6 iPhone OS 1.x, just refuse the build, since no
    version with the 1.x ABI allows usage of the r9 register.

--- pkgs/luajit/build.sh
+++ pkgs/luajit/build.sh
@@ -1,16 +1,40 @@
 #!/bin/sh
+# shellcheck disable=2086
 . ../../files/lib.sh
 
-(
-cd "$_SRCDIR"
-"$_TARGET-cc" -dM -E - < /dev/null | grep -q __LP64__ && arg=-m64
 if [ "$_SUBSYSTEM" = "ios" ]; then
+    if [ "$_OSVER" -lt 20000 ]; then
+        printf 'luajit on arm requires the r9 register to be available, which is only the case on iPhone OS 3.0+\n'
+        mkdir "$_DESTDIR"
+        exit 0
+    elif [ "$_OSVER" -lt 30000 ] && [ "$_CPU" = "armv6" ]; then
+        cflags='TARGET_FLAGS=-mios-version-min=3.0'
+    fi
     sys='iOS'
 else
     sys='Darwin'
 fi
-make amalg TARGET_SYS="$sys" HOST_CC="clang ${arg:--m32}" CROSS="$_TARGET-" BUILDMODE=dynamic CCOPT=-O3 PREFIX=/var/usr DESTDIR="$_DESTDIR" MACOSX_DEPLOYMENT_TARGET="$_MACVER" -j"$_JOBS"
-make install PREFIX=/var/usr DESTDIR="$_DESTDIR" TARGET_SYS=Darwin
+
+(
+cd "$_SRCDIR"
+"$_TARGET-cc" -dM -E - < /dev/null | grep -q __LP64__ && arg=-m64
+make amalg \
+    TARGET_SYS="$sys" \
+    HOST_CC="clang ${arg:--m32}" \
+    CROSS="$_TARGET-" \
+    BUILDMODE=dynamic \
+    CCOPT=-O3 \
+    PREFIX=/var/usr \
+    DESTDIR="$_DESTDIR" \
+    MACOSX_DEPLOYMENT_TARGET="$_MACVER" \
+    $cflags \
+    -j"$_JOBS"
+make install \
+    TARGET_SYS="$sys" \
+    PREFIX=/var/usr \
+    DESTDIR="$_DESTDIR" \
+    $cflags \
+    -j"$_JOBS"
 )
 
 (
