829ae811bb592e4694988f75fd23131a96e6a982
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-20 09:09:50 -0500

    update ngtcp2 and nghttp3

--- pkgs/nghttp3/fetch.sh
+++ pkgs/nghttp3/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.14.0'
+ver='1.15.0'
 if [ ! -f "$_DLCACHE/nghttp3-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/nghttp3-$ver.tar.xz" | awk '{print $1}')" != "b3083dae2ff30cf00d24d5fedd432479532c7b17d993d384103527b36c1ec82d" ]; then
+    [ "$(sha256sum "$_DLCACHE/nghttp3-$ver.tar.xz" | awk '{print $1}')" != "6da0cd06b428d32a54c58137838505d9dc0371a900bb8070a46b29e1ceaf2e0f" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/nghttp3-$ver.tar.xz" "https://github.com/ngtcp2/nghttp3/releases/download/v$ver/nghttp3-$ver.tar.xz" || exit 1
 fi
--- pkgs/ngtcp2/fetch.sh
+++ pkgs/ngtcp2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.19.0'
+ver='1.20.0'
 if [ ! -f "$_DLCACHE/ngtcp2-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/ngtcp2-$ver.tar.xz" | awk '{print $1}')" != "f11f7da5065f2298f8b5f079a11f1a6f72389271b8dedd893c8eb26aba94bce9" ]; then
+    [ "$(sha256sum "$_DLCACHE/ngtcp2-$ver.tar.xz" | awk '{print $1}')" != "7fb5b46bbf73dd43efbad55c707d067fdbafdca3609fc71d96e8f3068c5a6667" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/ngtcp2-$ver.tar.xz" "https://github.com/ngtcp2/ngtcp2/releases/download/v$ver/ngtcp2-$ver.tar.xz" || exit 1
 fi
