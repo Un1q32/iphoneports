bc2241fd71f3ee373a5d0d8096f921ec39df8868
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-24 16:41:07 -0500

    update jansson

--- pkgs/jansson/fetch.sh
+++ pkgs/jansson/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.bz2 https://github.com/akheron/jansson/releases/download/v2.14.1/jansson-2.14.1.tar.bz2
+ver='2.15.0'
+if [ ! -f "$_DLCACHE/jansson-$ver.tar.bz2" ] ||
+    [ "$(sha256sum "$_DLCACHE/jansson-$ver.tar.bz2" | awk '{print $1}')" != "a7eac7765000373165f9373eb748be039c10b2efc00be9af3467ec92357d8954" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/jansson-$ver.tar.bz2" "https://github.com/akheron/jansson/releases/download/v$ver/jansson-$ver.tar.bz2" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.bz2
-rm src.tar.bz2
+tar -C "$_TMP" -xf "$_DLCACHE/jansson-$ver.tar.bz2"
 mv "$_TMP"/jansson-* "$_SRCDIR"
