247e212ad31448a20d6eab24e8b762732df9bea5
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-24 17:41:28 -0500

    update luau

--- pkgs/luau/fetch.sh
+++ pkgs/luau/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='0.703'
+ver='0.706'
 if [ ! -f "$_DLCACHE/luau-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/luau-$ver.tar.gz" | awk '{print $1}')" != "e661d85d03244cf6c00e5a17b493e19714b7b414b3bf8b9d1ae20b508ba2980f" ]; then
+    [ "$(sha256sum "$_DLCACHE/luau-$ver.tar.gz" | awk '{print $1}')" != "f4968e32947f7aaf65ea66efe803918c539239f00d95eb329de7cea16573be3d" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/luau-$ver.tar.gz" "https://github.com/luau-lang/luau/archive/refs/tags/$ver.tar.gz" || exit 1
 fi
