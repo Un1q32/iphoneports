3d6eee153b017412b8e8de6964f9024594f60bcd
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-28 22:44:18 -0500

    update tinyxxd

--- pkgs/tinyxxd/fetch.sh
+++ pkgs/tinyxxd/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://github.com/xyproto/tinyxxd/archive/refs/tags/v1.3.7.tar.gz
+ver='1.3.8'
+if [ ! -f "$_DLCACHE/tinyxxd-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/tinyxxd-$ver.tar.gz" | awk '{print $1}')" != "c1a355fd50079a594546f5265e7c66ff3a5742767c9935e1e577825099629534" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/tinyxxd-$ver.tar.gz" "https://github.com/xyproto/tinyxxd/archive/refs/tags/v$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
+tar -C "$_TMP" -xf "$_DLCACHE/tinyxxd-$ver.tar.gz"
 mv "$_TMP"/tinyxxd-* "$_SRCDIR"
