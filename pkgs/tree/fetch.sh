a46f4891763c090413cbfbf5b2f80e006dff0f2c
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-03 08:16:19 -0500

    update tree

--- pkgs/tree/fetch.sh
+++ pkgs/tree/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://oldmanprogrammer.net/tar/tree/tree-2.2.1.tgz
+ver='2.3.1'
+if [ ! -f "$_DLCACHE/tree-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/tree-$ver.tar.gz" | awk '{print $1}')" != "47ca786ed4ea4aa277cabd42b1a54635aca41b29e425e9229bd1317831f25665" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/tree-$ver.tar.gz" "https://oldmanprogrammer.net/tar/tree/tree-$ver.tgz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
+tar -C "$_TMP" -xf "$_DLCACHE/tree-$ver.tar.gz"
 mv "$_TMP"/tree-* "$_SRCDIR"
