8944e83b2d716368e2ef02ba287a1f9d99c76c04
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-04 21:51:23 -0500

    update git

--- pkgs/git/fetch.sh
+++ pkgs/git/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='2.52.0'
+ver='2.53.0'
 if [ ! -f "$_DLCACHE/git-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/git-$ver.tar.xz" | awk '{print $1}')" != "3cd8fee86f69a949cb610fee8cd9264e6873d07fa58411f6060b3d62729ed7c5" ]; then
+    [ "$(sha256sum "$_DLCACHE/git-$ver.tar.xz" | awk '{print $1}')" != "5818bd7d80b061bbbdfec8a433d609dc8818a05991f731ffc4a561e2ca18c653" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/git-$ver.tar.xz" "https://mirrors.edge.kernel.org/pub/software/scm/git/git-$ver.tar.xz" || exit 1
 fi
