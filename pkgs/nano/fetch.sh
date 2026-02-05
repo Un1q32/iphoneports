3ad948555527031a61eba94618739d27ed46a391
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-04 21:57:28 -0500

    update nano

--- pkgs/nano/fetch.sh
+++ pkgs/nano/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='8.7'
+ver='8.7.1'
 if [ ! -f "$_DLCACHE/nano-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/nano-$ver.tar.gz" | awk '{print $1}')" != "06f3237ded64dec61c8cbc531531d7a31784604e303beb3be46256bf1d212da2" ]; then
+    [ "$(sha256sum "$_DLCACHE/nano-$ver.tar.gz" | awk '{print $1}')" != "f56b612024beb5900905a0580260434cf74a4dcdace8241608ff28ae2f472118" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/nano-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/nano/nano-$ver.tar.gz" || exit 1
 fi
