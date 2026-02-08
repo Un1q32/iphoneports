37bec0cef840a53e10ee4aa7a6a4cf10ded4b234
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-08 14:05:20 -0500

    update libpng

--- pkgs/libpng/fetch.sh
+++ pkgs/libpng/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.6.53'
+ver='1.6.54'
 if [ ! -f "$_DLCACHE/libpng-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libpng-$ver.tar.xz" | awk '{print $1}')" != "1d3fb8ccc2932d04aa3663e22ef5ef490244370f4e568d7850165068778d98d4" ]; then
+    [ "$(sha256sum "$_DLCACHE/libpng-$ver.tar.xz" | awk '{print $1}')" != "01c9d8a303c941ec2c511c14312a3b1d36cedb41e2f5168ccdaa85d53b887805" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libpng-$ver.tar.xz" "https://download.sourceforge.net/libpng/libpng-$ver.tar.xz" || exit 1
 fi
