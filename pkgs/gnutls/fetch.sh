717ac74582557da993abf4031ee731e88b2fd9a9
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-16 13:33:10 -0500

    update gnutls

--- pkgs/gnutls/fetch.sh
+++ pkgs/gnutls/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.8.11'
+ver='3.8.12'
 if [ ! -f "$_DLCACHE/gnutls-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/gnutls-$ver.tar.xz" | awk '{print $1}')" != "91bd23c4a86ebc6152e81303d20cf6ceaeb97bc8f84266d0faec6e29f17baa20" ]; then
+    [ "$(sha256sum "$_DLCACHE/gnutls-$ver.tar.xz" | awk '{print $1}')" != "a7b341421bfd459acf7a374ca4af3b9e06608dcd7bd792b2bf470bea012b8e51" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/gnutls-$ver.tar.xz" "https://www.gnupg.org/ftp/gcrypt/gnutls/v${ver%.*}/gnutls-$ver.tar.xz" || exit 1
 fi
