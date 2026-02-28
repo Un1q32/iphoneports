74719a8dee59ae8177c7764a16cb59ffd6ed71ed
Un1q32 <joey.t.reinhart@gmail.com>
2026-02-28 16:52:22 -0500

    update file

--- pkgs/file/fetch.sh
+++ pkgs/file/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz ftp://ftp.astron.com/pub/file/file-5.46.tar.gz
+ver='5.47'
+if [ ! -f "$_DLCACHE/file-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/file-$ver.tar.gz" | awk '{print $1}')" != "45672fec165cb4cc1358a2d76b5d57d22876dcb97ab169427ac385cbe1d5597a" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/file-$ver.tar.gz" "ftp://ftp.astron.com/pub/file/file-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
-mv "$_TMP"/file-5.46 "$_SRCDIR"
+tar -C "$_TMP" -xf "$_DLCACHE/file-$ver.tar.gz"
+mv "$_TMP"/file-* "$_SRCDIR"
