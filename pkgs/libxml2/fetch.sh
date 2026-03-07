bc578fdd8920d507f70c79b364af9ab161f7ea38
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-06 23:10:48 -0500

    update libxml2

--- pkgs/libxml2/fetch.sh
+++ pkgs/libxml2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='2.15.1'
+ver='2.15.2'
 if [ ! -f "$_DLCACHE/libxml2-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libxml2-$ver.tar.xz" | awk '{print $1}')" != "c008bac08fd5c7b4a87f7b8a71f283fa581d80d80ff8d2efd3b26224c39bc54c" ]; then
+    [ "$(sha256sum "$_DLCACHE/libxml2-$ver.tar.xz" | awk '{print $1}')" != "c8b9bc81f8b590c33af8cc6c336dbff2f53409973588a351c95f1c621b13d09d" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libxml2-$ver.tar.xz" "https://download.gnome.org/sources/libxml2/${ver%.*}/libxml2-$ver.tar.xz" || exit 1
 fi
