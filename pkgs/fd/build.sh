afa1701fb7787bb5e9a93f59128b769b40ea24fa
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-07 11:36:21 -0500

    update fd

--- pkgs/fd/build.sh
+++ pkgs/fd/build.sh
@@ -10,6 +10,12 @@ fi
 
 (
 cd "$_SRCDIR"
+# libdispatch
+if { [ "$_SUBSYSTEM" = "ios" ] && [ "$_OSVER" -lt 40000 ]; } ||
+    { [ "$_SUBSYSTEM" = "macos" ] && [ "$_OSVER" -lt 1060 ]; }; then
+    sed -i 's|ctrlc = "3.5"|ctrlc = "3.4.7"|' Cargo.toml
+    cargo update ctrlc --precise 3.4.7
+fi
 SDKROOT="$_SDK" cargo build --target "$_RUSTTARGET" --release -j "$_JOBS"
 mkdir -p "$_DESTDIR/var/usr/bin"
 cp "target/$_RUSTTARGET/release/fd" "$_DESTDIR/var/usr/bin"
--- pkgs/fd/fetch.sh
+++ pkgs/fd/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='10.3.0'
+ver='10.4.0'
 if [ ! -f "$_DLCACHE/fd-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/fd-$ver.tar.gz" | awk '{print $1}')" != "2edbc917a533053855d5b635dff368d65756ce6f82ddefd57b6c202622d791e9" ]; then
+    [ "$(sha256sum "$_DLCACHE/fd-$ver.tar.gz" | awk '{print $1}')" != "9caf8509134fe304ce5ee4667804216d93fe61df11ff941f48a240d40495db16" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/fd-$ver.tar.gz" "https://github.com/sharkdp/fd/archive/refs/tags/v$ver.tar.gz" || exit 1
 fi
