3c24e653d2856008f1df9cc6e967721e47baa534
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-10 09:05:44 -0400

    update libarchive

--- pkgs/libarchive/fetch.sh
+++ pkgs/libarchive/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.8.5'
+ver='3.8.6'
 if [ ! -f "$_DLCACHE/libarchive-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libarchive-$ver.tar.xz" | awk '{print $1}')" != "d68068e74beee3a0ec0dd04aee9037d5757fcc651591a6dcf1b6d542fb15a703" ]; then
+    [ "$(sha256sum "$_DLCACHE/libarchive-$ver.tar.xz" | awk '{print $1}')" != "8ac57c1f5e99550948d1fe755c806d26026e71827da228f36bef24527e372e6f" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libarchive-$ver.tar.xz" "https://www.libarchive.org/downloads/libarchive-$ver.tar.xz" || exit 1
 fi
