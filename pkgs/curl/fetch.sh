fb75ba57eb4147420ab6781ed4613a0a9f362d58
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-11 20:36:23 -0400

    update curl

--- pkgs/curl/fetch.sh
+++ pkgs/curl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='8.18.0'
+ver='8.19.0'
 if [ ! -f "$_DLCACHE/curl-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/curl-$ver.tar.xz" | awk '{print $1}')" != "955f6e729ad6b3566260e8fef68620e76ba3c31acf0a18524416a185acf77992" ]; then
+    [ "$(sha256sum "$_DLCACHE/curl-$ver.tar.xz" | awk '{print $1}')" != "4eb41489790d19e190d7ac7e18e82857cdd68af8f4e66b292ced562d333f11df" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/curl-$ver.tar.xz" "https://curl.se/download/curl-$ver.tar.xz" || exit 1
 fi
