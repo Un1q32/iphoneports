1a41256da0d8761360d4c89951d0ae5bb67177ba
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-21 23:14:22 -0400

    update autoconf

--- pkgs/autoconf/fetch.sh
+++ pkgs/autoconf/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='2.72'
+ver='2.73'
 if [ ! -f "$_DLCACHE/autoconf-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/autoconf-$ver.tar.xz" | awk '{print $1}')" != "ba885c1319578d6c94d46e9b0dceb4014caafe2490e437a0dbca3f270a223f5a" ]; then
+    [ "$(sha256sum "$_DLCACHE/autoconf-$ver.tar.xz" | awk '{print $1}')" != "9fd672b1c8425fac2fa67fa0477b990987268b90ff36d5f016dae57be0d6b52e" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/autoconf-$ver.tar.xz" "https://ftp.gnu.org/gnu/autoconf/autoconf-$ver.tar.xz" || exit 1
 fi
