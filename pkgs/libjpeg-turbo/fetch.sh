49e45ed22988a4f081097accd74c3f1f0fc34e3c
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-27 16:09:45 -0400

    update libjpeg-turbo

--- pkgs/libjpeg-turbo/fetch.sh
+++ pkgs/libjpeg-turbo/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.1.3'
+ver='3.1.4'
 if [ ! -f "$_DLCACHE/libjpeg-turbo-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libjpeg-turbo-$ver.tar.gz" | awk '{print $1}')" != "3a13a5ba767dc8264bc40b185e41368a80d5d5f945944d1dbaa4b2fb0099f4e5" ]; then
+    [ "$(sha256sum "$_DLCACHE/libjpeg-turbo-$ver.tar.gz" | awk '{print $1}')" != "3a581ad5a3060cee69487aae0bc71a1f3be94b89f92215e32b43742ec4a5055c" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libjpeg-turbo-$ver.tar.gz" "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/$ver.tar.gz" || exit 1
 fi
