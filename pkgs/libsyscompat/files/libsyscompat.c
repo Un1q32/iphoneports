bd25946565170eedf89919feed66c64368af61f2
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-27 18:37:56 -0400

    update libsyscompat with a fuckton more functions

--- pkgs/libsyscompat/files/libsyscompat.c
+++ pkgs/libsyscompat/files/libsyscompat.c
@@ -1,6 +1,10 @@
 #define realpath __dont_define_realpath
 #define fstatat __dont_define_fstatat
 
+#define __dd_fd dd_fd
+#include <dirent.h>
+#undef __dd_fd
+
 #if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 40300) ||                \
     (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
@@ -176,6 +180,33 @@ int clock_gettime(int clockid, struct timespec *ts) {
   return 0;
 }
 
+int clock_getres(int clockid, struct timespec *res) {
+  switch (clockid) {
+  case CLOCK_REALTIME:
+  case CLOCK_PROCESS_CPUTIME_ID:
+#ifndef HIRES_THREAD_TIME
+  case CLOCK_THREAD_CPUTIME_ID:
+#endif
+    res->tv_sec = 0;
+    res->tv_nsec = 1000;
+    return 0;
+
+  case CLOCK_MONOTONIC_RAW:
+  case CLOCK_MONOTONIC_RAW_APPROX:
+  case CLOCK_UPTIME_RAW:
+  case CLOCK_UPTIME_RAW_APPROX:
+#ifdef HIRES_THREAD_TIME
+  case CLOCK_THREAD_CPUTIME_ID:
+#endif
+    res->tv_sec = 0;
+    res->tv_nsec = mach_time_factor;
+    return 0;
+  default:
+    errno = EINVAL;
+    return -1;
+  }
+}
+
 void arc4random_buf(void *, size_t);
 
 int getentropy(void *buf, size_t size) {
@@ -188,6 +219,8 @@ int getentropy(void *buf, size_t size) {
     (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
      __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101000)
 
+#include <limits.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
 #include <unistd.h>
@@ -201,21 +234,515 @@ int getentropy(void *buf, size_t size) {
 #ifndef AT_SYMLINK_NOFOLLOW
 #define AT_SYMLINK_NOFOLLOW 0x0020
 #endif
+#ifndef AT_SYMLINK_FOLLOW
+#define AT_SYMLINK_FOLLOW 0x0040
+#endif
+#ifndef AT_REMOVEDIR
+#define AT_REMOVEDIR 0x0080
+#endif
 
-#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
-     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||                \
-    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
-     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
-
-#include <limits.h>
-
-#else
+#if !((defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&              \
+       __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||              \
+      (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&               \
+       __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050))
 
 static inline int pthread_fchdir_np(int fd) {
   return syscall(SYS___pthread_fchdir, fd);
 }
 
 #endif
+
+int linkat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath,
+           int flags) {
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 20000) ||               \
+    !defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+
+  static bool init = false;
+  static int (*func)(int, const char *, int, const char *, int);
+
+  if (!init) {
+    func = (int (*)(int, const char *, int, const char *, int))dlsym(RTLD_NEXT,
+                                                                     "linkat");
+    init = true;
+  }
+
+  if (func)
+    return func(olddirfd, oldpath, newdirfd, newpath, flags);
+
+#endif
+
+  /*
+   * This has race conditions, they could be mitigated by using pthread_fchdir,
+   * but that would require writing a really long function and I don't wanna do
+   * that.
+   */
+  if (oldpath[0] != '/' && olddirfd != AT_FDCWD) {
+    struct stat st;
+    if (fstat(olddirfd, &st) == -1)
+      return -1;
+    if (!S_ISDIR(st.st_mode)) {
+      errno = ENOTDIR;
+      return -1;
+    }
+
+    char oldfdpath[PATH_MAX + strlen(oldpath) + 2];
+    fcntl(olddirfd, F_GETPATH, oldfdpath);
+    strcat(oldfdpath, "/");
+    strcat(oldfdpath, oldpath);
+    oldpath = oldfdpath;
+  }
+  /* I don't know any way to emulate linkat with AT_SYMLINK_FOLLOW properly */
+  if (flags & AT_SYMLINK_FOLLOW) {
+    struct stat _st;
+    if (lstat(oldpath, &_st) == -1)
+      return -1;
+    if (S_ISLNK(_st.st_mode)) {
+      errno = EINVAL;
+      return -1;
+    }
+  }
+  if (newpath[0] != '/' && newdirfd != AT_FDCWD) {
+    struct stat st;
+    if (fstat(newdirfd, &st) == -1)
+      return -1;
+    if (!S_ISDIR(st.st_mode)) {
+      errno = ENOTDIR;
+      return -1;
+    }
+
+    char newfdpath[PATH_MAX + strlen(newpath) + 2];
+    fcntl(newdirfd, F_GETPATH, newfdpath);
+    strcat(newfdpath, "/");
+    strcat(newfdpath, newpath);
+    newpath = newfdpath;
+  }
+  return link(oldpath, newpath);
+}
+
+int renameat(int olddirfd, const char *oldpath, int newdirfd,
+             const char *newpath) {
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 20000) ||               \
+    !defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+
+  static bool init = false;
+  static int (*func)(int, const char *, int, const char *);
+
+  if (!init) {
+    func = (int (*)(int, const char *, int, const char *))dlsym(RTLD_NEXT,
+                                                                "renameat");
+    init = true;
+  }
+
+  if (func)
+    return func(olddirfd, oldpath, newdirfd, newpath);
+
+#endif
+
+  /*
+   * This has race conditions, they could be mitigated by using pthread_fchdir,
+   * but that would require writing a really long function and I don't wanna do
+   * that.
+   */
+  if (oldpath[0] != '/' && olddirfd != AT_FDCWD) {
+    struct stat st;
+    if (fstat(olddirfd, &st) == -1)
+      return -1;
+    if (!S_ISDIR(st.st_mode)) {
+      errno = ENOTDIR;
+      return -1;
+    }
+
+    char oldfdpath[PATH_MAX + strlen(oldpath) + 2];
+    fcntl(olddirfd, F_GETPATH, oldfdpath);
+    strcat(oldfdpath, "/");
+    strcat(oldfdpath, oldpath);
+    oldpath = oldfdpath;
+  }
+  if (newpath[0] != '/' && newdirfd != AT_FDCWD) {
+    struct stat st;
+    if (fstat(newdirfd, &st) == -1)
+      return -1;
+    if (!S_ISDIR(st.st_mode)) {
+      errno = ENOTDIR;
+      return -1;
+    }
+
+    char newfdpath[PATH_MAX + strlen(newpath) + 2];
+    fcntl(newdirfd, F_GETPATH, newfdpath);
+    strcat(newfdpath, "/");
+    strcat(newfdpath, newpath);
+    newpath = newfdpath;
+  }
+  return rename(oldpath, newpath);
+}
+
+ssize_t readlinkat(int fd, const char *path, char *buf, size_t bufsize) {
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 20000) ||               \
+    !defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+
+  static bool init = false;
+  static ssize_t (*func)(int, const char *, char *, size_t);
+
+  if (!init) {
+    func = (ssize_t (*)(int, const char *, char *, size_t))dlsym(RTLD_NEXT,
+                                                                 "readlinkat");
+    init = true;
+  }
+
+  if (func)
+    return func(fd, path, buf, bufsize);
+
+#endif
+
+  if (fd == AT_FDCWD || path[0] == '/')
+    return readlink(path, buf, bufsize);
+
+  if (path[0] == '\0') {
+    /* there is a race condition here between the fcntl and readlink calls */
+    char fdpath[PATH_MAX + strlen(path) + 2];
+    if (fcntl(fd, F_GETPATH, fdpath) == -1)
+      return -1;
+    return readlink(fdpath, buf, bufsize);
+  }
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
+
+  /* the Mac OS X 10.4 / iPhone OS 1 version has a race condition */
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+    return -1;
+  if (!S_ISDIR(st.st_mode)) {
+    errno = ENOTDIR;
+    return -1;
+  }
+
+  char fdpath[PATH_MAX + strlen(path) + 2];
+  fcntl(fd, F_GETPATH, fdpath);
+  if (path[0] != '\0') {
+    strcat(fdpath, "/");
+    strcat(fdpath, path);
+  }
+  return readlink(fdpath, buf, bufsize);
+
+#else
+
+  int cwd = open(".", O_RDONLY);
+  if (pthread_fchdir_np(-1) < 0 && cwd != -1) {
+    close(cwd);
+    cwd = -1;
+  }
+  if (pthread_fchdir_np(fd) < 0) {
+    pthread_fchdir_np(cwd);
+    if (cwd != -1)
+      close(cwd);
+    return -1;
+  }
+
+  ssize_t ret = readlink(path, buf, bufsize);
+
+  pthread_fchdir_np(cwd);
+  if (cwd != -1)
+    close(cwd);
+
+  return ret;
+
+#endif
+}
+
+int mkdirat(int fd, const char *path, mode_t mode) {
+
+#if !(defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&               \
+      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000)
+
+  static bool init = false;
+  static int (*func)(int, const char *, mode_t);
+
+  if (!init) {
+    func = (int (*)(int, const char *, mode_t))dlsym(RTLD_NEXT, "mkdirat");
+    init = true;
+  }
+
+  if (func)
+    return func(fd, path, mode);
+
+#endif
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
+
+  /* the Mac OS X 10.4 / iPhone OS 1 version has a race condition */
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+    return -1;
+  if (!S_ISDIR(st.st_mode)) {
+    errno = ENOTDIR;
+    return -1;
+  }
+
+  char fdpath[PATH_MAX + strlen(path) + 2];
+  fcntl(fd, F_GETPATH, fdpath);
+  strcat(fdpath, "/");
+  strcat(fdpath, path);
+
+  return mkdir(fdpath, mode);
+
+#else
+
+  int cwd = open(".", O_RDONLY);
+  if (pthread_fchdir_np(-1) < 0 && cwd != -1) {
+    close(cwd);
+    cwd = -1;
+  }
+  if (pthread_fchdir_np(fd) < 0) {
+    pthread_fchdir_np(cwd);
+    if (cwd != -1)
+      close(cwd);
+    return -1;
+  }
+
+  int ret = mkdir(path, mode);
+
+  pthread_fchdir_np(cwd);
+  if (cwd != -1)
+    close(cwd);
+
+  return ret;
+
+#endif
+}
+
+int openat(int fd, const char *path, int flags, ...) {
+  int mode = mode;
+  if (flags & O_CREAT) {
+    va_list va_args;
+    va_start(va_args, flags);
+    mode = va_arg(va_args, int);
+    va_end(va_args);
+  }
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 20000) ||               \
+    !defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+
+  static bool init = false;
+  static int (*func)(int, const char *, int, ...);
+
+  if (!init) {
+    func = (int (*)(int, const char *, int, ...))dlsym(RTLD_NEXT, "openat");
+    init = true;
+  }
+
+  if (func)
+    return func(fd, path, flags, mode);
+
+#endif
+
+  if (fd == AT_FDCWD || path[0] == '/')
+    return open(path, flags, mode);
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
+
+  /* the Mac OS X 10.4 / iPhone OS 1 version has a race condition */
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+    return -1;
+  if (!S_ISDIR(st.st_mode)) {
+    errno = ENOTDIR;
+    return -1;
+  }
+
+  char fdpath[PATH_MAX + strlen(path) + 2];
+  fcntl(fd, F_GETPATH, fdpath);
+  strcat(fdpath, "/");
+  strcat(fdpath, path);
+
+  return open(fdpath, flags, mode);
+
+#else
+
+  int cwd = open(".", O_RDONLY);
+  if (pthread_fchdir_np(-1) < 0 && cwd != -1) {
+    close(cwd);
+    cwd = -1;
+  }
+  if (pthread_fchdir_np(fd) < 0) {
+    pthread_fchdir_np(cwd);
+    if (cwd != -1)
+      close(cwd);
+    return -1;
+  }
+
+  int ret = open(path, flags, mode);
+
+  pthread_fchdir_np(cwd);
+  if (cwd != -1)
+    close(cwd);
+
+  return ret;
+
+#endif
+}
+
+int unlinkat(int fd, const char *path, int flags) {
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 20000) ||               \
+    !defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+
+  static bool init = false;
+  static int (*func)(int, const char *, int);
+
+  if (!init) {
+    func = (int (*)(int, const char *, int))dlsym(RTLD_NEXT, "unlinkat");
+    init = true;
+  }
+
+  if (func)
+    return func(fd, path, flags);
+
+#endif
+
+  if (fd == AT_FDCWD || path[0] == '/') {
+    if (flags & AT_REMOVEDIR)
+      return rmdir(path);
+    return unlink(path);
+  }
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
+
+  /* the Mac OS X 10.4 / iPhone OS 1 version has a race condition */
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+    return -1;
+  if (!S_ISDIR(st.st_mode)) {
+    errno = ENOTDIR;
+    return -1;
+  }
+
+  char fdpath[PATH_MAX + strlen(path) + 2];
+  fcntl(fd, F_GETPATH, fdpath);
+  strcat(fdpath, "/");
+  strcat(fdpath, path);
+
+  if (flags & AT_REMOVEDIR)
+    return rmdir(fdpath);
+  return unlink(fdpath);
+
+#else
+
+  int cwd = open(".", O_RDONLY);
+  if (pthread_fchdir_np(-1) < 0 && cwd != -1) {
+    close(cwd);
+    cwd = -1;
+  }
+  if (pthread_fchdir_np(fd) < 0) {
+    pthread_fchdir_np(cwd);
+    if (cwd != -1)
+      close(cwd);
+    return -1;
+  }
+
+  int ret;
+  if (flags & AT_REMOVEDIR)
+    ret = rmdir(path);
+  else
+    ret = unlink(path);
+
+  pthread_fchdir_np(cwd);
+  if (cwd != -1)
+    close(cwd);
+
+  return ret;
+
+#endif
+}
+
+int symlinkat(const char *target, int fd, const char *path) {
+
+#if !(defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&               \
+      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000)
+
+  static bool init = false;
+  static int (*func)(const char *, int, const char *);
+
+  if (!init) {
+    func =
+        (int (*)(const char *, int, const char *))dlsym(RTLD_NEXT, "symlinkat");
+    init = true;
+  }
+
+  if (func)
+    return func(target, fd, path);
+
+#endif
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000) ||                \
+    (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
+     __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
+
+  /* the Mac OS X 10.4 / iPhone OS 1 version has a race condition */
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+    return -1;
+  if (!S_ISDIR(st.st_mode)) {
+    errno = ENOTDIR;
+    return -1;
+  }
+
+  char fdpath[PATH_MAX + strlen(path) + 2];
+  fcntl(fd, F_GETPATH, fdpath);
+  strcat(fdpath, "/");
+  strcat(fdpath, path);
+
+  return symlink(target, fdpath);
+
+#else
+
+  int cwd = open(".", O_RDONLY);
+  if (pthread_fchdir_np(-1) < 0 && cwd != -1) {
+    close(cwd);
+    cwd = -1;
+  }
+  if (pthread_fchdir_np(fd) < 0) {
+    pthread_fchdir_np(cwd);
+    if (cwd != -1)
+      close(cwd);
+    return -1;
+  }
+
+  int ret = symlink(target, path);
+
+  pthread_fchdir_np(cwd);
+  if (cwd != -1)
+    close(cwd);
+
+  return ret;
+
+#endif
+}
 
 #if defined(__DARWIN_64_BIT_INO_T) && __DARWIN_64_BIT_INO_T &&                 \
     (!defined(__DARWIN_ONLY_64_BIT_INO_T) || !__DARWIN_ONLY_64_BIT_INO_T)
@@ -408,6 +935,47 @@ int faccessat(int fd, const char *path, int mode, int flags) {
 #endif
 }
 
+#if defined(__DARWIN_64_BIT_INO_T) && __DARWIN_64_BIT_INO_T &&                 \
+    (!defined(__DARWIN_ONLY_64_BIT_INO_T) || !__DARWIN_ONLY_64_BIT_INO_T)
+DIR *fdopendir(int fd) __asm("_fdopendir$INODE64"
+#ifdef __i386__
+                             "$UNIX2003"
+#endif
+);
+#endif
+DIR *fdopendir(int fd) {
+
+#if !(defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&               \
+      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 20000)
+
+  static bool init = false;
+  static DIR *(*func)(int);
+
+  if (!init) {
+    func = (DIR * (*)(int)) dlsym(RTLD_NEXT, "fdopendir"
+#if defined(__DARWIN_64_BIT_INO_T) && __DARWIN_64_BIT_INO_T &&                 \
+    (!defined(__DARWIN_ONLY_64_BIT_INO_T) || !__DARWIN_ONLY_64_BIT_INO_T)
+                                             "$INODE64"
+#endif
+#ifdef __i386__
+                                             "$UNIX2003"
+#endif
+    );
+    init = true;
+  }
+
+  if (func)
+    return func(fd);
+
+#endif
+
+  char fdpath[PATH_MAX];
+  if (fcntl(fd, F_GETPATH, fdpath) == -1)
+    return NULL;
+
+  return opendir(fdpath);
+}
+
 int CCRandomGenerateBytes(void *buf, size_t size) {
   arc4random_buf(buf, size);
   return 0;
@@ -417,10 +985,6 @@ int CCRandomGenerateBytes(void *buf, size_t size) {
      __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ < 60000) ||                \
     (defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) &&                 \
      __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1080)
-
-#define __dd_fd dd_fd
-#include <dirent.h>
-#undef __dd_fd
 
 /*
  * dirfd exists on old darwins as a macro function, the symbol
@@ -673,6 +1237,7 @@ char *realpath_extsn(const char *restrict path, char *restrict resolved_path) {
     defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
 
 #include <mach/mach_host.h>
+#include <sys/attr.h>
 
 int posix_memalign(void **memptr, size_t align, size_t size) {
 
@@ -707,6 +1272,41 @@ int posix_memalign(void **memptr, size_t align, size_t size) {
     *memptr = mem;
     return 0;
   }
+}
+
+#ifdef __LP64__
+#define FLAGSTYPE int
+#else
+#define FLAGSTYPE long
+#endif
+
+int fsetattrlist(int fd, struct attrlist *attrList, void *attrBuf,
+                 size_t attrBufSize, unsigned FLAGSTYPE flags) {
+
+#if (defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) &&                \
+     __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 20000) ||               \
+    !defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+
+  static bool init = false;
+  static int (*func)(int, struct attrlist *, void *, size_t,
+                     unsigned FLAGSTYPE);
+  if (!init) {
+    func = (int (*)(int, struct attrlist *, void *, size_t,
+                    unsigned FLAGSTYPE))dlsym(RTLD_NEXT, "fsetattrlist");
+    init = true;
+  }
+
+  if (func)
+    return func(fd, attrList, attrBuf, attrBufSize, flags);
+
+#endif
+
+  char fdpath[PATH_MAX];
+  if (fcntl(fd, F_GETPATH, fdpath) == -1)
+    return -1;
+
+  return setattrlist(fdpath, attrList, attrBuf, attrBufSize,
+                     flags | FSOPT_NOFOLLOW);
 }
 
 kern_return_t host_statistics64(host_t host_priv, host_flavor_t flavor,
