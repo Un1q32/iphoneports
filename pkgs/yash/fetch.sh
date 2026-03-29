85f2aea0b956883ad6a495286718286800af94e3
Un1q32 <joey.t.reinhart@gmail.com>
2026-03-29 00:38:43 -0400

    fix

--- pkgs/yash/fetch.sh
+++ pkgs/yash/fetch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
 ver='2.61'
-if [ ! -f "$_DLCACHE/yash-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/yash-$ver.tar.xz" | awk '{print $1}')" != "06401f8abfad7ac3bdbce66ccda2c37878fa103a53a3c3ed35e386f2514dd9bd" ]; then
+if [ ! -f "$_DLCACHE/yash-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/yash-$ver.tar.gz" | awk '{print $1}')" != "06401f8abfad7ac3bdbce66ccda2c37878fa103a53a3c3ed35e386f2514dd9bd" ]; then
     printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/yash-$ver.tar.xz" "https://github.com/magicant/yash/archive/refs/tags/$ver.tar.gz" || exit 1
+    curl -L -# -o "$_DLCACHE/yash-$ver.tar.gz" "https://github.com/magicant/yash/archive/refs/tags/$ver.tar.gz" || exit 1
 fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/yash-$ver.tar.xz"
+tar -C "$_TMP" -xf "$_DLCACHE/yash-$ver.tar.gz"
 mv "$_TMP"/yash-* "$_SRCDIR"
