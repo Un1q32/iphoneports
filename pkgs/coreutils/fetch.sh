dd35d41f0775bc0dd8296b4b50a2266c7a551c26
Un1q32 <joey.t.reinhart@gmail.com>
2026-04-23 15:57:00 -0400

    update coreutils

--- pkgs/coreutils/fetch.sh
+++ pkgs/coreutils/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='9.10'
+ver='9.11'
 if [ ! -f "$_DLCACHE/coreutils-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/coreutils-$ver.tar.xz" | awk '{print $1}')" != "16535a9adf0b10037364e2d612aad3d9f4eca3a344949ced74d12faf4bd51d25" ]; then
+    [ "$(sha256sum "$_DLCACHE/coreutils-$ver.tar.xz" | awk '{print $1}')" != "394024eda0a5955217ceda9cd1201e65dc8fa3aa29c2951135a49521d57c3cc3" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/coreutils-$ver.tar.xz" "https://ftpmirror.gnu.org/gnu/coreutils/coreutils-$ver.tar.xz" || exit 1
 fi
