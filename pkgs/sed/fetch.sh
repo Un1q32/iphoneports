7cbf41218c1913248b700adfe808622025845a8b
Un1q32 <joey.t.reinhart@gmail.com>
2026-04-27 02:38:07 -0400

    update sed

--- pkgs/sed/fetch.sh
+++ pkgs/sed/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftpmirror.gnu.org/gnu/sed/sed-4.9.tar.gz
+ver='4.10'
+if [ ! -f "$_DLCACHE/sed-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/sed-$ver.tar.gz" | awk '{print $1}')" != "4d179ffaf92ec4dcec541f7c032be1c3b9a1856f4970adb95a505221702f5277" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/sed-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/sed/sed-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
-mv "$_TMP"/sed-4.9 "$_SRCDIR"
+tar -C "$_TMP" -xf "$_DLCACHE/sed-$ver.tar.gz"
+mv "$_TMP"/sed-* "$_SRCDIR"
