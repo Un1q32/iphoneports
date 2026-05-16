5b42268c2534c36f2a9ce64d00d06db4f2d996be
Un1q32 <joey.t.reinhart@gmail.com>
2026-05-16 09:35:27 -0400

    update python

--- pkgs/python/fetch.sh
+++ pkgs/python/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.14.4'
+ver='3.14.5'
 if [ ! -f "$_DLCACHE/python-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/python-$ver.tar.xz" | awk '{print $1}')" != "d923c51303e38e249136fc1bdf3568d56ecb03214efdef48516176d3d7faaef8" ]; then
+    [ "$(sha256sum "$_DLCACHE/python-$ver.tar.xz" | awk '{print $1}')" != "7e32597b99e5d9a39abed35de4693fa169df3e5850d4c334337ffd6a19a36db6" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/python-$ver.tar.xz" "https://www.python.org/ftp/python/$ver/Python-$ver.tar.xz" || exit 1
 fi
