d8f86cebf8bb9c4b25eac26759f41a8c12eab0e3
Un1q32 <joey.t.reinhart@gmail.com>
2026-05-18 15:51:48 -0400

    update dash

--- pkgs/dash/fetch.sh
+++ pkgs/dash/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='0.5.13.2'
+ver='0.5.13.4'
 if [ ! -f "$_DLCACHE/dash-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/dash-$ver.tar.gz" | awk '{print $1}')" != "ce88391bd79c71d9e182b005e69ebb11829e61ed6953276a086dad43701ac594" ]; then
+    [ "$(sha256sum "$_DLCACHE/dash-$ver.tar.gz" | awk '{print $1}')" != "652e95024b75758dcd141b64a0d3973a026cc6aaee1aec81ce03e76dc3e6a267" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/dash-$ver.tar.gz" "https://git.kernel.org/pub/scm/utils/dash/dash.git/snapshot/dash-$ver.tar.gz" || exit 1
 fi
