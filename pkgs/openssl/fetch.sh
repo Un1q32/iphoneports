43beab65025fdaf04e2b2ec89654443756e4a9ba
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-09 08:05:11 -0400

    update openssl

--- pkgs/openssl/fetch.sh
+++ pkgs/openssl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.6.2'
+ver='3.6.3'
 if [ ! -f "$_DLCACHE/openssl-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/openssl-$ver.tar.gz" | awk '{print $1}')" != "aaf51a1fe064384f811daeaeb4ec4dce7340ec8bd893027eee676af31e83a04f" ]; then
+    [ "$(sha256sum "$_DLCACHE/openssl-$ver.tar.gz" | awk '{print $1}')" != "243a86649cf6f23eeb6a2ff2456e09e5d77dd9018a54d3d96b0c6bdd6ba6c7f1" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/openssl-$ver.tar.gz" "https://github.com/openssl/openssl/releases/download/openssl-$ver/openssl-$ver.tar.gz" || exit 1
 fi
