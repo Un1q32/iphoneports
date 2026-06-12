0ed4f7e3c6771f2141d1677b4941cd55eb05bc0f
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-12 09:39:52 -0400

    update wasmi

--- pkgs/wasmi/fetch.sh
+++ pkgs/wasmi/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.0.9'
+ver='1.1.0'
 if [ ! -f "$_DLCACHE/wasmi-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/wasmi-$ver.tar.gz" | awk '{print $1}')" != "f72f76987dffd516e3a887118d4ea381f50803331f7ab5c75fc6b8b7da4d66b2" ]; then
+    [ "$(sha256sum "$_DLCACHE/wasmi-$ver.tar.gz" | awk '{print $1}')" != "08339285a8ed3302a7c90a878a24907841ebac70e00fee1cce48e97e69153d2e" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/wasmi-$ver.tar.gz" "https://github.com/wasmi-labs/wasmi/archive/refs/tags/v$ver.tar.gz" || exit 1
 fi
