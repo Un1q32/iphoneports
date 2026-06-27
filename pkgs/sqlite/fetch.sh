e9ab7dc5bcb867922a78c7fea505918f3cf0735b
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-27 17:33:56 -0400

    update sqlite

--- pkgs/sqlite/fetch.sh
+++ pkgs/sqlite/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3530200'
+ver='3530300'
 if [ ! -f "$_DLCACHE/sqlite-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/sqlite-$ver.tar.gz" | awk '{print $1}')" != "588ad51949419a56ebe81fe56193d510c559eb94c9a57748387860b5d3069316" ]; then
+    [ "$(sha256sum "$_DLCACHE/sqlite-$ver.tar.gz" | awk '{print $1}')" != "c917d7db16648ec95f714974ace5e5dcf46b7dc70e26600a0a102a3141125db0" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/sqlite-$ver.tar.gz" "https://sqlite.org/2026/sqlite-autoconf-$ver.tar.gz" || exit 1
 fi
