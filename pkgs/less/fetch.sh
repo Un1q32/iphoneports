552f441ee7c24540051739aef79a2326ddca5ee3
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-27 16:25:36 -0400

    update less

--- pkgs/less/fetch.sh
+++ pkgs/less/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='702'
+ver='704'
 if [ ! -f "$_DLCACHE/less-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/less-$ver.tar.gz" | awk '{print $1}')" != "242a64c00f02d96f8ee208cf638ae1728b727c7f5fdf82a7d4f4cae32fb084e2" ]; then
+    [ "$(sha256sum "$_DLCACHE/less-$ver.tar.gz" | awk '{print $1}')" != "20a0b0a2bb2525fa53c7eee9beb854b4c9cf172eabb209af7020743547bfe9fb" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/less-$ver.tar.gz" "https://www.greenwoodsoftware.com/less/less-$ver.tar.gz" || exit 1
 fi
