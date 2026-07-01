c3aed42bf42a74d5a4ba9e6942abd358caa05d63
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-01 15:13:17 -0400

    update jansson

--- pkgs/jansson/fetch.sh
+++ pkgs/jansson/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='2.15.0'
+ver='2.15.1'
 if [ ! -f "$_DLCACHE/jansson-$ver.tar.bz2" ] ||
-    [ "$(sha256sum "$_DLCACHE/jansson-$ver.tar.bz2" | awk '{print $1}')" != "a7eac7765000373165f9373eb748be039c10b2efc00be9af3467ec92357d8954" ]; then
+    [ "$(sha256sum "$_DLCACHE/jansson-$ver.tar.bz2" | awk '{print $1}')" != "f9aa4b3ec8496fee69db94f06f3d76fd4a26b012ee9bf1e917078c2cd2841881" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/jansson-$ver.tar.bz2" "https://github.com/akheron/jansson/releases/download/v$ver/jansson-$ver.tar.bz2" || exit 1
 fi
