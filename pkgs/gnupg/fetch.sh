a1744d9c800320f34d8a08c88b0fc243871f0726
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-02 22:59:31 -0400

    update gnupg

--- pkgs/gnupg/fetch.sh
+++ pkgs/gnupg/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='2.5.20'
+ver='2.5.21'
 if [ ! -f "$_DLCACHE/gnupg-$ver.tar.bz2" ] ||
-    [ "$(sha256sum "$_DLCACHE/gnupg-$ver.tar.bz2" | awk '{print $1}')" != "dd17ab2e9a04fd79d39d853f599cbc852062ddb9ab52a4ddeb4176fd8b302964" ]; then
+    [ "$(sha256sum "$_DLCACHE/gnupg-$ver.tar.bz2" | awk '{print $1}')" != "e3af2c8caa46a66a9329fa7c6880af260451914d819595beabc2c26597b31352" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/gnupg-$ver.tar.bz2" "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-$ver.tar.bz2" || exit 1
 fi
