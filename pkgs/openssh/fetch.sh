8563a68b0b170685626e919d3fcbe03c99050433
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-08 19:17:00 -0400

    update openssh

--- pkgs/openssh/fetch.sh
+++ pkgs/openssh/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='10.3p1'
+ver='10.4p1'
 if [ ! -f "$_DLCACHE/openssh-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/openssh-$ver.tar.gz" | awk '{print $1}')" != "56682a36bb92dcf4b4f016fd8ec8e74059b79a8de25c15d670d731e7d18e45f4" ]; then
+    [ "$(sha256sum "$_DLCACHE/openssh-$ver.tar.gz" | awk '{print $1}')" != "ef6026dd2aea8d56059638d5d3262902c892ceba9f88395835e0d06d3fb63238" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/openssh-$ver.tar.gz" "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$ver.tar.gz" || exit 1
 fi
