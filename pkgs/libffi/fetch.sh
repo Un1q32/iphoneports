097523e1833f58cc96bc203d07724082327751a5
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-12 01:05:06 -0400

    update libffi

--- pkgs/libffi/fetch.sh
+++ pkgs/libffi/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.5.2'
+ver='3.7.1'
 if [ ! -f "$_DLCACHE/libffi-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libffi-$ver.tar.gz" | awk '{print $1}')" != "f3a3082a23b37c293a4fcd1053147b371f2ff91fa7ea1b2a52e335676bac82dc" ]; then
+    [ "$(sha256sum "$_DLCACHE/libffi-$ver.tar.gz" | awk '{print $1}')" != "d5e9a6638ddbd2513ddb54518eb67e4bbe6fa707bcc01c10f6212f0a088d819d" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libffi-$ver.tar.gz" "https://github.com/libffi/libffi/releases/download/v$ver/libffi-$ver.tar.gz" || exit 1
 fi
