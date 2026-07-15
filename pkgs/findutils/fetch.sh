66ec962e83091db6c56df638aca641e27f7fa9ed
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-14 22:22:36 -0400

    update findutils

--- pkgs/findutils/fetch.sh
+++ pkgs/findutils/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='4.10.0'
+ver='4.11.0'
 if [ ! -f "$_DLCACHE/findutils-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/findutils-$ver.tar.xz" | awk '{print $1}')" != "1387e0b67ff247d2abde998f90dfbf70c1491391a59ddfecb8ae698789f0a4f5" ]; then
+    [ "$(sha256sum "$_DLCACHE/findutils-$ver.tar.xz" | awk '{print $1}')" != "bfd19cb06cc71f3352d567e90284d8cdac02ac89774bbeadf0b533b0c11432fd" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/findutils-$ver.tar.xz" "https://ftpmirror.gnu.org/gnu/findutils/findutils-$ver.tar.xz" || exit 1
 fi
