033355b1a3882c7a442b80a40608b3e168405e64
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-16 23:23:13 -0400

    update libpsl

--- pkgs/libpsl/fetch.sh
+++ pkgs/libpsl/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='0.22.0'
+ver='0.23.0'
 if [ ! -f "$_DLCACHE/libpsl-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libpsl-$ver.tar.gz" | awk '{print $1}')" != "c45c3aa17576b99873e05a9b09a44041b065bbfa390e6d474d06fbfaeb9c7722" ]; then
+    [ "$(sha256sum "$_DLCACHE/libpsl-$ver.tar.gz" | awk '{print $1}')" != "f39b9631b3d369a21259ea4654f8875c0ec6995ce9551c0eb5d423e4c011f911" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/libpsl-$ver.tar.gz" "https://github.com/rockdaboot/libpsl/releases/download/$ver/libpsl-$ver.tar.gz" || exit 1
 fi
