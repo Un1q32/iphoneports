5daedfb6876e3efc606b7a7f9f1ea35b95c85a96
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-26 12:25:38 -0400

    update ngtcp2

--- pkgs/ngtcp2/fetch.sh
+++ pkgs/ngtcp2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.24.0'
+ver='1.25.0'
 if [ ! -f "$_DLCACHE/ngtcp2-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/ngtcp2-$ver.tar.xz" | awk '{print $1}')" != "7fa5ec2be0f0cbed8bc4ec89c0787dfa9d8ce678f1ed9477c52f30eb1a591207" ]; then
+    [ "$(sha256sum "$_DLCACHE/ngtcp2-$ver.tar.xz" | awk '{print $1}')" != "2a34d2484ba17847a5d11965704e9dd0fac4c6d8efc75ffe1ec7de66d8c6b6fb" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/ngtcp2-$ver.tar.xz" "https://github.com/ngtcp2/ngtcp2/releases/download/v$ver/ngtcp2-$ver.tar.xz" || exit 1
 fi
