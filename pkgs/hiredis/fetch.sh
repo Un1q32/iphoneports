b89bd702e5d07f8442f74200e364c038eb19fc9f
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-06 13:49:48 -0400

    update hiredis

--- pkgs/hiredis/fetch.sh
+++ pkgs/hiredis/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.4.0'
+ver='1.4.1'
 if [ ! -f "$_DLCACHE/hiredis-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/hiredis-$ver.tar.gz" | awk '{print $1}')" != "5fa6e719e59cd4f8ae435c52a18ac4035d135251f9ee54e7a045bccf59107ed8" ]; then
+    [ "$(sha256sum "$_DLCACHE/hiredis-$ver.tar.gz" | awk '{print $1}')" != "ca3180359a8b1275838a45415851f8cd5c411e27bdbf18f4823012e45507d2e4" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/hiredis-$ver.tar.gz" "https://github.com/redis/hiredis/archive/refs/tags/v$ver.tar.gz" || exit 1
 fi
