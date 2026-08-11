f4d6874cb40dac519bb6a6b08d1a6486c17b46de
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-11 04:07:25 -0400

    dont need this thing anymore

--- pkgs/sdk/build.sh
+++ pkgs/sdk/build.sh
@@ -3,6 +3,6 @@
 
 mkdir -p "$_DESTDIR/var/usr/etc/profile.d"
 cp files/sdkroot.sh "$_DESTDIR/var/usr/etc/profile.d"
-cp -a "$("$_TARGET-sdkpath")" "$_DESTDIR/var/usr/sdk"
+cp -a "$_SDK" "$_DESTDIR/var/usr/sdk"
 
 builddeb
