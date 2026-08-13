d6097d9ca54791284c2a637ce8285059f5855acb
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 02:16:44 -0400

    update screen

--- pkgs/screen/fetch.sh
+++ pkgs/screen/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftpmirror.gnu.org/gnu/screen/screen-5.0.0.tar.gz
+ver='5.0.2'
+if [ ! -f "$_DLCACHE/screen-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/screen-$ver.tar.gz" | awk '{print $1}')" != "ca9a2c7e240919bc7ac12124593ae4529bb4eb5f7349d8857829b7e3f0b3b332" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/screen-$ver.tar.gz" "https://ftp.gnu.org/gnu/screen/screen-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
+tar -C "$_TMP" -xf "$_DLCACHE/screen-$ver.tar.gz"
 mv "$_TMP"/screen-* "$_SRCDIR"
