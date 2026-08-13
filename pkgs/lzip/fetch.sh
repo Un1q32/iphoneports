8b2220a708866a0ab34eb71f107a7c11287c9a7f
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 16:55:10 -0400

    update lzip

--- pkgs/lzip/fetch.sh
+++ pkgs/lzip/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://download.savannah.gnu.org/releases/lzip/lzip-1.25.tar.gz
+ver='1.26'
+if [ ! -f "$_DLCACHE/lzip-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/lzip-$ver.tar.gz" | awk '{print $1}')" != "641cf30961525cbe3b340cc883436c8854e9f5032f459f444de4782b621e6572" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/lzip-$ver.tar.gz" "https://download.savannah.nongnu.org/releases/lzip/lzip-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
+tar -C "$_TMP" -xf "$_DLCACHE/lzip-$ver.tar.gz"
 mv "$_TMP"/lzip-* "$_SRCDIR"
