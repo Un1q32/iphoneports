0809ccf17969a49c4c2db676b2b9363ed92e6233
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 01:55:43 -0400

    fix npth download

--- pkgs/npth/build.sh
+++ pkgs/npth/build.sh
@@ -3,6 +3,7 @@
 
 (
 cd "$_SRCDIR"
+./autogen.sh
 ./configure --host="$_TARGET" --prefix=/var/usr --disable-static --disable-tests
 make
 make DESTDIR="$_DESTDIR" install
--- pkgs/npth/fetch.sh
+++ pkgs/npth/fetch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
 ver='1.8'
-if [ ! -f "$_DLCACHE/npth-$ver.tar.bz2" ] ||
-    [ "$(sha256sum "$_DLCACHE/npth-$ver.tar.bz2" | awk '{print $1}')" != "8bd24b4f23a3065d6e5b26e98aba9ce783ea4fd781069c1b35d149694e90ca3e" ]; then
+if [ ! -f "$_DLCACHE/npth-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/npth-$ver.tar.gz" | awk '{print $1}')" != "39baf403b0d63271b31a7546a480b4570a422465823f82d93bcdd9c1491df5af" ]; then
     printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/npth-$ver.tar.bz2" "https://gnupg.org/ftp/gcrypt/npth/npth-$ver.tar.bz2" || exit 1
+    curl -L -# -o "$_DLCACHE/npth-$ver.tar.gz" "https://github.com/gpg/npth/archive/refs/tags/npth-$ver.tar.gz" || exit 1
 fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/npth-$ver.tar.bz2"
+tar -C "$_TMP" -xf "$_DLCACHE/npth-$ver.tar.gz"
 mv "$_TMP"/npth-* "$_SRCDIR"
