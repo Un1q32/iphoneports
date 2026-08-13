a687e6afc61f8826f45bd149ce94f64ea77b9295
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 17:06:28 -0400

    cache patch

--- pkgs/make/fetch.sh
+++ pkgs/make/fetch.sh
@@ -4,7 +4,7 @@ ver='4.4.1'
 if [ ! -f "$_DLCACHE/make-$ver.tar.gz" ] ||
     [ "$(sha256sum "$_DLCACHE/make-$ver.tar.gz" | awk '{print $1}')" != "dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3" ]; then
     printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/make-$ver.tar.gz" "https://ftp.gnu.org/gnu/make/make-$ver.tar.gz" || exit 1
+    curl -L -# -o "$_DLCACHE/make-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/make/make-$ver.tar.gz" || exit 1
 fi
 printf "Unpacking source...\n"
 tar -C "$_TMP" -xf "$_DLCACHE/make-$ver.tar.gz"
--- pkgs/patch/fetch.sh
+++ pkgs/patch/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.xz https://ftpmirror.gnu.org/gnu/patch/patch-2.8.tar.xz
+ver='2.8'
+if [ ! -f "$_DLCACHE/patch-$ver.tar.xz" ] ||
+    [ "$(sha256sum "$_DLCACHE/patch-$ver.tar.xz" | awk '{print $1}')" != "f87cee69eec2b4fcbf60a396b030ad6aa3415f192aa5f7ee84cad5e11f7f5ae3" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/patch-$ver.tar.xz" "https://ftpmirror.gnu.org/gnu/patch/patch-$ver.tar.xz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.xz
-rm src.tar.xz
+tar -C "$_TMP" -xf "$_DLCACHE/patch-$ver.tar.xz"
 mv "$_TMP"/patch-* "$_SRCDIR"
