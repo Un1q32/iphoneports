db5e31d3fa65c31d75ec67f78fc85f4640c9ca99
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 01:14:00 -0400

    update imagemagick

--- pkgs/imagemagick/fetch.sh
+++ pkgs/imagemagick/fetch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='7.1.2-24'
-if [ ! -f "$_DLCACHE/imagemagick-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/imagemagick-$ver.tar.xz" | awk '{print $1}')" != "e44e70aa49e97ceb8b4f68e97829d0bde69b79387d69660b8886b1f6e06ddb19" ]; then
+ver='7.1.2-29'
+if [ ! -f "$_DLCACHE/imagemagick-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/imagemagick-$ver.tar.gz" | awk '{print $1}')" != "b05924ad73c6932ba62c9b32f338f0619b90b767162c8b767f3566556187a284" ]; then
     printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/imagemagick-$ver.tar.xz" "https://imagemagick.org/archive/releases/ImageMagick-$ver.tar.xz" || exit 1
+    curl -L -# -o "$_DLCACHE/imagemagick-$ver.tar.gz" "https://github.com/ImageMagick/ImageMagick/archive/refs/tags/$ver.tar.gz" || exit 1
 fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/imagemagick-$ver.tar.xz"
+tar -C "$_TMP" -xf "$_DLCACHE/imagemagick-$ver.tar.gz"
 mv "$_TMP"/ImageMagick-* "$_SRCDIR"
