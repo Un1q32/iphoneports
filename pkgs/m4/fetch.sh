94c1a1da90b7b745d1ad5bfdc3ff6b5441c3da54
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 01:42:49 -0400

    update m4

--- pkgs/m4/fetch.sh
+++ pkgs/m4/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftpmirror.gnu.org/gnu/m4/m4-1.4.20.tar.gz
+ver='1.4.21'
+if [ ! -f "$_DLCACHE/m4-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/m4-$ver.tar.gz" | awk '{print $1}')" != "38ae59f7a30bf9c108193cc5c25fbb06014f21e230c7ede2eff614f7b7c37ed8" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/m4-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/m4/m4-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
+tar -C "$_TMP" -xf "$_DLCACHE/m4-$ver.tar.gz"
 mv "$_TMP"/m4-* "$_SRCDIR"
