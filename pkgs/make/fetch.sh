68e25316e64036d66e82bb00966f64f82b09b2f5
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 16:59:20 -0400

    cache make

--- pkgs/make/fetch.sh
+++ pkgs/make/fetch.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-printf "Downloading source...\n"
-curl -L -# -o src.tar.gz https://ftpmirror.gnu.org/gnu/make/make-4.4.1.tar.gz
+ver='4.4.1'
+if [ ! -f "$_DLCACHE/make-$ver.tar.gz" ] ||
+    [ "$(sha256sum "$_DLCACHE/make-$ver.tar.gz" | awk '{print $1}')" != "dd16fb1d67bfab79a72f5e8390735c49e3e8e70b4945a15ab1f81ddb78658fb3" ]; then
+    printf "Downloading source...\n"
+    curl -L -# -o "$_DLCACHE/make-$ver.tar.gz" "https://ftp.gnu.org/gnu/make/make-$ver.tar.gz" || exit 1
+fi
 printf "Unpacking source...\n"
-tar -C "$_TMP" -xf src.tar.gz
-rm src.tar.gz
-mv "$_TMP"/make-4.4.1 "$_SRCDIR"
+tar -C "$_TMP" -xf "$_DLCACHE/make-$ver.tar.gz"
+mv "$_TMP"/make-* "$_SRCDIR"
