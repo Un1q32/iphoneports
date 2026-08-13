c16fd471bf24f5d17d80fd7f3895787b8a0d8597
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-13 02:16:57 -0400

    bru

--- pkgs/screen/fetch.sh
+++ pkgs/screen/fetch.sh
@@ -4,7 +4,7 @@ ver='5.0.2'
 if [ ! -f "$_DLCACHE/screen-$ver.tar.gz" ] ||
     [ "$(sha256sum "$_DLCACHE/screen-$ver.tar.gz" | awk '{print $1}')" != "ca9a2c7e240919bc7ac12124593ae4529bb4eb5f7349d8857829b7e3f0b3b332" ]; then
     printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/screen-$ver.tar.gz" "https://ftp.gnu.org/gnu/screen/screen-$ver.tar.gz" || exit 1
+    curl -L -# -o "$_DLCACHE/screen-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/screen/screen-$ver.tar.gz" || exit 1
 fi
 printf "Unpacking source...\n"
 tar -C "$_TMP" -xf "$_DLCACHE/screen-$ver.tar.gz"
