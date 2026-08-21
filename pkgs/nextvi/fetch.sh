39069155c23983f00410eef4b544e61f379c7af8
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-20 20:29:34 -0400

    update nextvi

--- pkgs/nextvi/fetch.sh
+++ pkgs/nextvi/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='a3259775afdb54419557994a5803c9b05a11450d'
+ver='6e686474c52132bdbd3a2343ccbb615870e7a9bc'
 if [ ! -f "$_DLCACHE/nextvi-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/nextvi-$ver.tar.gz" | awk '{print $1}')" != "ae435691010c39a21e7dc5ac389adbbb29dd2bf18d9c17d6db4a30b37e169a6b" ]; then
+    [ "$(sha256sum "$_DLCACHE/nextvi-$ver.tar.gz" | awk '{print $1}')" != "e39286f1341eccca03459a15953b491e2e582380f04759e582e66a8a17db74fb" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/nextvi-$ver.tar.gz" "https://github.com/Un1q32/nextvi/archive/$ver.tar.gz" || exit 1
 fi
