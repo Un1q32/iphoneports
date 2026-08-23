b33c014755d39cf0434dddb56f39e351b4cf29de
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-23 05:32:43 -0400

    update ccache

--- pkgs/ccache/fetch.sh
+++ pkgs/ccache/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='4.13.6'
+ver='4.14'
 if [ ! -f "$_DLCACHE/ccache-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/ccache-$ver.tar.gz" | awk '{print $1}')" != "b0688da07593d481ed1901ec39739a770469090c8feff32522256d48077aaf9b" ]; then
+    [ "$(sha256sum "$_DLCACHE/ccache-$ver.tar.gz" | awk '{print $1}')" != "ee23b7526eb50bd8c276b6cba6e046f745753ba0174ea5b39e0ae2fa005c7989" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/ccache-$ver.tar.gz" "https://github.com/ccache/ccache/archive/refs/tags/v$ver.tar.gz" || exit 1
 fi
