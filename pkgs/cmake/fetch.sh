10a11080a32ccf4fcb21e8a1fdc33e4b75a342aa
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-25 18:29:05 -0400

    update cmake

--- pkgs/cmake/fetch.sh
+++ pkgs/cmake/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='4.4.2'
+ver='4.4.3'
 if [ ! -f "$_DLCACHE/cmake-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/cmake-$ver.tar.gz" | awk '{print $1}')" != "e821bfb902a4f5e47b6e45bdae0a781918043d4b2c5517a023bc99596ae6abaf" ]; then
+    [ "$(sha256sum "$_DLCACHE/cmake-$ver.tar.gz" | awk '{print $1}')" != "bfd14c62cf4bfb2fd10525449ea59007d3325e7641349e5b7e69cee1127f2e7d" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/cmake-$ver.tar.gz" "https://github.com/Kitware/CMake/archive/refs/tags/v$ver.tar.gz" || exit 1
 fi
