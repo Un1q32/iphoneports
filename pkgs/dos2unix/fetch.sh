7457fed2bcf2395ebfe4b28c7c33b8435ba1d365
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-27 21:13:04 -0400

    update dos2unix

--- pkgs/dos2unix/fetch.sh
+++ pkgs/dos2unix/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='7.5.6'
+ver='7.5.7'
 if [ ! -f "$_DLCACHE/dos2unix-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/dos2unix-$ver.tar.gz" | awk '{print $1}')" != "75f692b8484c8c24579a2ffd87df16b9c9428ed95497e3393a21d1ba0697ac33" ]; then
+    [ "$(sha256sum "$_DLCACHE/dos2unix-$ver.tar.gz" | awk '{print $1}')" != "669ee27120ae71589f638fe3a167d6ea54f8633f5ab1b282551bd7a7c9510dfa" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/dos2unix-$ver.tar.gz" "https://downloads.sourceforge.net/project/dos2unix/dos2unix/$ver/dos2unix-$ver.tar.gz" || exit 1
 fi
