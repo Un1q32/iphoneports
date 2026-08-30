261b60d7fdd8a91e7f0ec2d6d7eb1e7d9f43d73d
Un1q32 <joey.t.reinhart@gmail.com>
2026-08-30 07:05:25 -0400

    update onefetch

--- pkgs/onefetch/fetch.sh
+++ pkgs/onefetch/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='2.27.1'
+ver='2.28.0'
 if [ ! -f "$_DLCACHE/onefetch-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/onefetch-$ver.tar.gz" | awk '{print $1}')" != "3a6f82d3da4da62b2e5406bbe307b0afc73cd8fcc4855534886d80ea0121cc03" ]; then
+    [ "$(sha256sum "$_DLCACHE/onefetch-$ver.tar.gz" | awk '{print $1}')" != "b55047d224cbf42dbb06e140da4125269141c5a74eae76b712a8455c558ca8e1" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/onefetch-$ver.tar.gz" "https://github.com/o2sh/onefetch/archive/refs/tags/$ver.tar.gz" || exit 1
 fi
