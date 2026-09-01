a3fc166f188b164aff35f453b59d0adc03bcdac9
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-01 00:54:02 -0400

    update pcre2

--- pkgs/pcre2/fetch.sh
+++ pkgs/pcre2/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='10.47'
+ver='10.48'
 if [ ! -f "$_DLCACHE/pcre2-$ver.tar.bz2" ] ||
-    [ "$(sha256sum "$_DLCACHE/pcre2-$ver.tar.bz2" | awk '{print $1}')" != "47fe8c99461250d42f89e6e8fdaeba9da057855d06eb7fc08d9ca03fd08d7bc7" ]; then
+    [ "$(sha256sum "$_DLCACHE/pcre2-$ver.tar.bz2" | awk '{print $1}')" != "b6c68fdf6f3ac31388b50aa89ff0fc49c00c987c16e7b5146491d12003f2c8ed" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/pcre2-$ver.tar.bz2" "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-$ver/pcre2-$ver.tar.bz2" || exit 1
 fi
