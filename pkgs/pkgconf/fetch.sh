e44874dbe7eba691893020bd96f4fe0efa0c7a93
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-05 14:50:31 -0400

    update pkgconf

--- pkgs/pkgconf/fetch.sh
+++ pkgs/pkgconf/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='3.0.6'
+ver='3.0.7'
 if [ ! -f "$_DLCACHE/pkgconf-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/pkgconf-$ver.tar.gz" | awk '{print $1}')" != "a32d9dd8fc3d1179d0755df471a561842a0f2475013da9c5cf86e38215a61380" ]; then
+    [ "$(sha256sum "$_DLCACHE/pkgconf-$ver.tar.gz" | awk '{print $1}')" != "a9ae678879771fcf15247ac2435e7ad8308be8a5921898e6720b3b8949410f73" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/pkgconf-$ver.tar.gz" "https://github.com/pkgconf/pkgconf/archive/refs/tags/pkgconf-$ver.tar.gz" || exit 1
 fi
