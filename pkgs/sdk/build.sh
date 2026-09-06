48f4a415461e97402dbed0596b1bd799c549f67b
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-05 20:11:55 -0400

    fix clang default target for arm64 iOS and tvOS

--- pkgs/sdk/build.sh
+++ pkgs/sdk/build.sh
@@ -5,4 +5,17 @@ mkdir -p "$_DESTDIR/var/usr/etc/profile.d"
 cp files/sdkroot.sh "$_DESTDIR/var/usr/etc/profile.d"
 cp -a "$_SDK" "$_DESTDIR/var/usr/sdk"
 
+if ! [ -f "$_DESTDIR/var/usr/sdk/SDKSettings.json" ]; then
+    case $_SUBSYSTEM in
+        (ios)     subsystem=iphoneos  ;;
+        (tvos)    subsystem=appletvos ;;
+        (macos)   subsystem=macosx    ;;
+        (watchos) subsystem=watchos   ;;
+    esac
+
+    sed -e "s/@VERSION@/$_SUBSYSTEMVER/g" \
+        -e "s/@SUBSYSTEM@/$subsystem/g" \
+        files/SDKSettings.json > "$_DESTDIR/var/usr/sdk/SDKSettings.json"
+fi
+
 builddeb
