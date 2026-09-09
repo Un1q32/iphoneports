04203c46d073536ff49ff2beed3c50d6536c2666
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-09 12:56:39 -0400

    bruh

--- pkgs/sdk/build.sh
+++ pkgs/sdk/build.sh
@@ -5,7 +5,7 @@ mkdir -p "$_DESTDIR/var/usr/etc/profile.d"
 cp files/sdkroot.sh "$_DESTDIR/var/usr/etc/profile.d"
 cp -a "$_SDK" "$_DESTDIR/var/usr/sdk"
 
-if ! [ -f "$_DESTDIR/var/usr/sdk/SDKSettings.json" ] && ! { [ "$_SUBSYSTEM" = 'ios' ] && [ "$_OSVER" -lt 20000 ]; }; then
+if ! [ -f "$_DESTDIR/var/usr/sdk/SDKSettings.json" ] && [ "$_CPU" != 'armv6' ]; then
     case $_SUBSYSTEM in
         (ios)     subsystem=iphoneos  ;;
         (tvos)    subsystem=appletvos ;;
