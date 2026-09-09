4a7ba6696719e496a175aa859745cacd64bfb92f
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-08 21:09:54 -0400

    update vim

--- pkgs/vim/fetch.sh
+++ pkgs/vim/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='c2734dc03cd9ee437d25a6940e8c37ac7873b01d'
+ver='5d934b1bdb803c652a521f04481f92b2c2fa029e'
 if [ ! -f "$_DLCACHE/vim-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/vim-$ver.tar.gz" | awk '{print $1}')" != "ac992c44ecd00ea4d62b56ef41efafc9bcd5f83dfb6925b7a6d92bc943d855a9" ]; then
+    [ "$(sha256sum "$_DLCACHE/vim-$ver.tar.gz" | awk '{print $1}')" != "80998c84f92ae8860fc3bdd625ff74a58c6cf63461973aae69a53dcd24400067" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/vim-$ver.tar.gz" "https://github.com/vim/vim/archive/$ver.tar.gz" || exit 1
 fi
