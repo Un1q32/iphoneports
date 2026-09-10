91fbe232b3df238868b970137ea7b5f9fe46548a
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-10 11:25:13 -0400

    update unrar

--- pkgs/unrar/fetch.sh
+++ pkgs/unrar/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='7.1.10'
+ver='7.3.1'
 if [ ! -f "$_DLCACHE/unrar-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/unrar-$ver.tar.gz" | awk '{print $1}')" != "72a9ccca146174f41876e8b21ab27e973f039c6d10b13aabcb320e7055b9bb98" ]; then
+    [ "$(sha256sum "$_DLCACHE/unrar-$ver.tar.gz" | awk '{print $1}')" != "634900842a3737d9cc15bbcc71d4c74cc713437e0bca296a573424fe5f2660ab" ]; then
     printf "Downloading source...\n"
     curl -L -# -o "$_DLCACHE/unrar-$ver.tar.gz" "https://www.rarlab.com/rar/unrarsrc-$ver.tar.gz" || exit 1
 fi
