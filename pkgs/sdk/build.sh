8fc225fda43b731f1778b588cde352a3a92dcbd3
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-09 23:01:11 -0400

    arm64e too

--- pkgs/sdk/build.sh
+++ pkgs/sdk/build.sh
@@ -5,7 +5,7 @@ mkdir -p "$_DESTDIR/var/usr/etc/profile.d"
 cp files/sdkroot.sh "$_DESTDIR/var/usr/etc/profile.d"
 cp -a "$_SDK" "$_DESTDIR/var/usr/sdk"
 
-if ! [ -f "$_DESTDIR/var/usr/sdk/SDKSettings.json" ] && [ "$_CPU" = 'arm64' ]; then
+if ! [ -f "$_DESTDIR/var/usr/sdk/SDKSettings.json" ] && { [ "$_CPU" = 'arm64' ] || [ "$_CPU" = 'arm64e' ]; }; then
     case $_SUBSYSTEM in
         (ios)     subsystem=iphoneos  ;;
         (tvos)    subsystem=appletvos ;;
