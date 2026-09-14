e346b98924cb6db55340f5c6175a6d5a76f7dedf
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-14 15:06:08 -0400

    update automake

--- pkgs/automake/fetch.sh
+++ pkgs/automake/fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='1.18.1'
-if [ ! -f "$_DLCACHE/automake-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/automake-$ver.tar.xz" | awk '{print $1}')" != "168aa363278351b89af56684448f525a5bce5079d0b6842bd910fdd3f1646887" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/automake-$ver.tar.xz" "https://ftp.gnu.org/gnu/automake/automake-$ver.tar.xz" || exit 1
-fi
-printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/automake-$ver.tar.xz"
-mv "$_TMP"/automake-* "$_SRCDIR"
+. ../../files/dllib.sh
+ver='1.19'
+dlsrc \
+    "https://ftpmirror.gnu.org/gnu/automake/automake-$ver.tar.xz" \
+    "automake-$ver.tar.xz" \
+    e3e2c2e3abf37898138db5b6c1d1dc35c9160c5978be7947d2c741705251d445 \
+    "automake-$ver"
