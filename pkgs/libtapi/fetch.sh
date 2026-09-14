c978daa9f005a28a2d586f287f08805c986550e1
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-14 15:13:54 -0400

    update

--- pkgs/libtapi/fetch.sh
+++ pkgs/libtapi/fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-rm -rf "$_DESTDIR" "$_SRCDIR"
+. ../../files/dllib.sh
 ver='593d29141bf176d24021208c75af54a2ef23c38b'
-if [ ! -f "$_DLCACHE/libtapi-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/libtapi-$ver.tar.gz" | awk '{print $1}')" != "dbad2a41f2351b052367db1fe251bebe43ee9ca5815e46b725c4bc71c339186f" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/libtapi-$ver.tar.gz" "https://github.com/tpoechtrager/apple-libtapi/archive/$ver.tar.gz" || exit 1
-fi
-printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/libtapi-$ver.tar.gz"
-mv "$_TMP"/apple-libtapi-* "$_SRCDIR"
+dlsrc \
+    "https://github.com/tpoechtrager/apple-libtapi/archive/$ver.tar.gz" \
+    "libtapi-$ver.tar.gz" \
+    dbad2a41f2351b052367db1fe251bebe43ee9ca5815e46b725c4bc71c339186f \
+    "apple-libtapi-$ver"
