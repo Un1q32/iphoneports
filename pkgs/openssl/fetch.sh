64a868899ba7125ec7a52f7ddac6aa2fe377ad6b
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-14 15:12:11 -0400

    update fetch script

--- pkgs/openssl/fetch.sh
+++ pkgs/openssl/fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-rm -rf "$_DESTDIR" "$_SRCDIR"
+. ../../files/dllib.sh
 ver='3.6.4'
-if [ ! -f "$_DLCACHE/openssl-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/openssl-$ver.tar.gz" | awk '{print $1}')" != "9bffaa1ad1e07b354c21bd3324ec02fa15579f45a7d0494b3e74bc449b7333ef" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/openssl-$ver.tar.gz" "https://github.com/openssl/openssl/releases/download/openssl-$ver/openssl-$ver.tar.gz" || exit 1
-fi
-printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/openssl-$ver.tar.gz"
-mv "$_TMP"/openssl-* "$_SRCDIR"
+dlsrc \
+    "https://github.com/openssl/openssl/releases/download/openssl-$ver/openssl-$ver.tar.gz" \
+    "openssl-$ver.tar.gz" \
+    9bffaa1ad1e07b354c21bd3324ec02fa15579f45a7d0494b3e74bc449b7333ef \
+    "openssl-$ver"
