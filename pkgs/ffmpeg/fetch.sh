4594265afb9b11ff72f3435a15b29bab4d86cfd6
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-19 15:57:11 -0400

    update ffmpeg

--- pkgs/ffmpeg/fetch.sh
+++ pkgs/ffmpeg/fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-rm -rf "$_DESTDIR" "$_SRCDIR"
-ver='9.0'
-if [ ! -f "$_DLCACHE/ffmpeg-$ver.tar.xz" ] ||
-    [ "$(sha256sum "$_DLCACHE/ffmpeg-$ver.tar.xz" | awk '{print $1}')" != "7f607a00dd0d28a729d5a4811205812eef01cf6ef6155025febb6f36a9062d52" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/ffmpeg-$ver.tar.xz" "https://ffmpeg.org/releases/ffmpeg-$ver.tar.xz" || exit 1
-fi
-printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/ffmpeg-$ver.tar.xz"
-mv "$_TMP"/ffmpeg-* "$_SRCDIR"
+. ../../files/dllib.sh
+ver='9.0.2'
+dlsrc \
+    "https://ffmpeg.org/releases/ffmpeg-$ver.tar.xz" \
+    "ffmpeg-$ver.tar.gz" \
+    8c3850283eb25fa026482078a04051e0be17347b09ef81a0849bec15a96e002e \
+    "ffmpeg-$ver"
