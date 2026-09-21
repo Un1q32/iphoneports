6fb89ff0ef8d421567bc8e0011c7191eac9e2018
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-20 23:24:14 -0400

    update readline fetch

--- pkgs/readline/fetch.sh
+++ pkgs/readline/fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-rm -rf "$_DESTDIR" "$_SRCDIR"
+. ../../files/dllib.sh
 ver='8.3'
-if [ ! -f "$_DLCACHE/readline-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/readline-$ver.tar.gz" | awk '{print $1}')" != "fe5383204467828cd495ee8d1d3c037a7eba1389c22bc6a041f627976f9061cc" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/readline-$ver.tar.gz" "https://ftpmirror.gnu.org/gnu/readline/readline-$ver.tar.gz" || exit 1
-fi
-printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/readline-$ver.tar.gz"
-mv "$_TMP"/readline-* "$_SRCDIR"
+dlsrc \
+    "https://ftpmirror.gnu.org/gnu/readline/readline-$ver.tar.gz" \
+    "readline-$ver.tar.gz" \
+    fe5383204467828cd495ee8d1d3c037a7eba1389c22bc6a041f627976f9061cc \
+    "readline-$ver"
