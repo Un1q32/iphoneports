2296f87cf2fc18327a08a6aadbd68b8fae577044
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-21 17:49:20 -0400

    update rsync fetch script

--- pkgs/rsync/fetch.sh
+++ pkgs/rsync/fetch.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-rm -rf "$_DESTDIR" "$_SRCDIR"
+. ../../files/dllib.sh
 ver='3.4.3'
-if [ ! -f "$_DLCACHE/rsync-$ver.tar.gz" ] ||
-    [ "$(sha256sum "$_DLCACHE/rsync-$ver.tar.gz" | awk '{print $1}')" != "c72e63ca3021cbc80ba86ec30102773f4c5631fbc492b52e773b3958f82a53d3" ]; then
-    printf "Downloading source...\n"
-    curl -L -# -o "$_DLCACHE/rsync-$ver.tar.gz" "https://download.samba.org/pub/rsync/src/rsync-$ver.tar.gz" || exit 1
-fi
-printf "Unpacking source...\n"
-tar -C "$_TMP" -xf "$_DLCACHE/rsync-$ver.tar.gz"
-mv "$_TMP"/rsync-* "$_SRCDIR"
+dlsrc \
+    "https://download.samba.org/pub/rsync/src/rsync-$ver.tar.gz" \
+    "rsync-$ver.tar.gz" \
+    c72e63ca3021cbc80ba86ec30102773f4c5631fbc492b52e773b3958f82a53d3 \
+    "rsync-$ver"
